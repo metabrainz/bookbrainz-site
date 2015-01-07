e10b7f9c7e564ef3e0c3c2ba3ea4d8ca0e413bed
Ben Ockmore <ben.sput@gmail.com>
2015-01-07 13:25:25 +0000

    Create form now functional, but edit/revision linking still needs doing.

--- routes/edit.js
+++ routes/edit.js
@@ -15,4 +15,43 @@ router.get('/publication/create', function(req, res) {
   });
 });
 
+
+router.post('/publication/create/handler', function(req, res) {
+  var ws = req.app.get('webservice');
+
+  console.log(req.body);
+
+  if (!req.body.edit) {
+    console.log('Make new edit!');
+  }
+  // If 'new edit' in form, create a new edit.
+  //var editPromise = request.post(ws + '/edits')
+  //.set('Authorization', 'Bearer ' + req.session.oauth.access_token).promise();
+  var changes = {
+    'entity_gid': [],
+    'publication_data': {
+      'publication_type_id': parseInt(req.body.type)
+    }
+  };
+
+  if (req.body.disambiguation) {
+    changes.disambiguation = req.body.disambiguation;
+  }
+
+  if (req.body.name && req.body.sort_name) {
+    changes.aliases = [{
+      'name': req.body.name,
+      'sort_name': req.body.sort_name,
+      'language_id': 1
+    }];
+  }
+
+  request.post(ws + '/revisions')
+  .set('Authorization', 'Bearer ' + req.session.oauth.access_token)
+  .send(changes).promise()
+  .then(function(revision) {
+    res.redirect(303, '/publication/' + revision.body.entity.gid);
+  })
+});
+
 module.exports = router;
