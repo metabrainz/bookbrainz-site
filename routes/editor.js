22a1d6527857528b6979e4ec4ec0cb7316743f49
Ben Ockmore <ben.sput@gmail.com>
2015-01-26 22:47:06 +0000

    Added basic edits page, which gets revision IDs and latest edits for the user.

--- routes/editor.js
+++ routes/editor.js
@@ -31,4 +31,36 @@ router.get('/editor/:id', function(req, res) {
   });
 });
 
+router.get('/editor/:id/edits', function(req, res) {
+  // Get the list of publication types
+  var ws = req.app.get('webservice');
+
+  var userPromise = request.get(ws + '/user/' + req.params.id).promise();
+  var userEditsPromise = request.get(ws + '/user/' +
+                                     req.params.id + '/edits').promise();
+
+  var userRevisionsPromise = userEditsPromise.then(function(edits) {
+    return Promise.all(
+      edits.body.objects.map(function(edit) {
+        return request.get(edit.revisions_uri).promise();
+      })
+    );
+  });
+
+
+  Promise.join(userPromise, userEditsPromise, userRevisionsPromise, function(user, edits, revisions) {
+    var editObjects = edits.body.objects;
+    editObjects.forEach(function (e, i) {
+      editObjects[i].revisions = revisions[i].body;
+    });
+    console.log(editObjects);
+
+    res.render('editor/edits', {
+      session: req.session,
+      user: user.body,
+      edits: edits.body
+    });
+  });
+});
+
 module.exports = router;
