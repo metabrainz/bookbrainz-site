f35f977c67db6808f5b3399b38c77a4c549f5232
Ben Ockmore <ben.sput@gmail.com>
2015-01-28 20:57:08 +0000

    Added the register form handler, which send ws requests and redirects to the homepage.

--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,20 @@ router.get('/register', function(req, res) {
   res.render('register', {session: req.session, error: req.query.error});
 });
 
+router.post('/register/handler', function(req, res) {
+  // This function should post a new user to the /user endpoint of the ws.
+  var ws = req.app.get('webservice');
+
+  // FIXME: This hardcodes the user type, assuming editor will be #1 - not
+  // very good
+  request.post(ws + '/user').send({
+    'name': req.body.username,
+    'email': req.body.email,
+    'user_type_id': 1
+  }).promise()
+  .then(function() {
+    res.redirect(303, '/');
+  });
+});
+
 module.exports = router;
