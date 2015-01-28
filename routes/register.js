355d024524a3ca1d7bc85188e2dd654d6b08ef29
Ben Ockmore <ben.sput@gmail.com>
2015-01-28 20:43:22 +0000

    Fixed bug: missing module export.

--- routes/register.js
+++ routes/register.js
@@ -4,3 +4,5 @@ var router = express.Router();
 router.get('/register', function(req, res) {
   res.render('register', {session: req.session, error: req.query.error});
 });
+
+module.exports = router;
