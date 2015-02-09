c1188713e7080d15af15c558960c1c8605182892
Ben Ockmore <ben.sput@gmail.com>
2015-02-09 22:16:54 +0000

    Switched from Grunt to Gulp build system.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+
+function bundle() {
+  var srcFiles = [
+    './views/editor/editor.js',
+    './views/entity/create/edit.js'
+  ];
+
+  var dstFiles = [
+    'public/javascripts/editor.js',
+    'public/javascripts/edit.js'
+  ];
+
+  return browserify(srcFiles)
+  .plugin('factor-bundle', {outputs: dstFiles})
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest('public/javascripts'));
+}
+
+function compress() {
+  return gulp.src('public/javascripts/*.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('public/javascripts'));
+}
+
+gulp.task('default', bundle);
+gulp.task('bundle', bundle);
+gulp.task('compress', ['bundle'], compress);
+gulp.task('watch', function() {
+  var watcher = gulp.watch('./views/**/*.js', ['bundle']);
+  watcher.on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
