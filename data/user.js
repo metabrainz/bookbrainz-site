38055e9c37a1e4e7a8c4da234ccaf1729a1b8263
Seán de Búrca <leftmostcat@gmail.com>
2015-03-22 22:12:46 -0600

    Add user and user type models

---
+++ data/user.js
@@ -0,0 +1,26 @@
+var bbws = rootRequire('helpers/bbws'),
+    Model = rootRequire('helpers/model'),
+    UserType = rootRequire('data/properties/user-type');
+
+var User = new Model({
+	endpoint: 'user'
+});
+
+User.extend({
+	id: { type: 'number', map: 'user_id' },
+	name: { type: 'string' },
+	user_type: { type: 'object', model: UserType },
+	email: { type: 'string' },
+	reputation: { type: 'number' },
+	bio: { type: 'string' },
+	created_at: { type: 'date' },
+	active_at: { type: 'date' }
+});
+
+User.getCurrent = function(accessToken) {
+	return bbws.post('/account', null, {
+		accessToken: accessToken
+	});
+}
+
+module.exports = User;
