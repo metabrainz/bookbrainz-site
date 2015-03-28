81fa17ddf1ad9f4aba1af4d4cd9b875c1df5577b
Seán de Búrca <leftmostcat@gmail.com>
2015-03-28 15:20:41 -0600

    Clean up single-file entity routes

--- routes/entity/creator.js
+++ routes/entity/creator.js
@@ -10,111 +10,110 @@ var express = require('express'),
 // Viewing
 
 router.get('/:id', function(req, res, next) {
-  var render = function(creator) {
-    res.render('entity/view/creator', {
-      title: 'BookBrainz',
-      entity: creator
-    });
-  };
+	var render = function(creator) {
+		res.render('entity/view/creator', {
+			title: 'BookBrainz',
+			entity: creator
+		});
+	};
 
-  Creator.findOne(req.params.id, {
-    populate: [
-      'annotation',
-      'disambiguation'
-    ]
-  })
-    .then(render)
-    .catch(function(err) {
-      console.log(err.stack);
-      next(err);
-    });
+	Creator.findOne(req.params.id, {
+		populate: [
+			'annotation',
+			'disambiguation'
+		]
+	})
+		.then(render)
+		.catch(function(err) {
+			console.log(err.stack);
+			next(err);
+		});
 });
 
 // Creation
 
-router.get('/', auth.isAuthenticated, function(req, res) {
-  var gendersPromise = Gender.find();
-  var creatorTypesPromise = CreatorType.find();
-  var languagesPromise = Language.find();
+router.get('/create', auth.isAuthenticated, function(req, res) {
+	var gendersPromise = Gender.find();
+	var creatorTypesPromise = CreatorType.find();
+	var languagesPromise = Language.find();
 
-  Promise.join(gendersPromise, creatorTypesPromise, languagesPromise,
-    function(genders, creatorTypes, languages) {
-      var genderList = genders.sort(function(a, b) {
-        return a.id > b.id;
-      });
+	Promise.join(gendersPromise, creatorTypesPromise, languagesPromise,
+		function(genders, creatorTypes, languages) {
+			var genderList = genders.sort(function(a, b) {
+				return a.id > b.id;
+			});
 
-      var alphabeticLanguagesList = languages.sort(function(a, b) {
-        if (a.frequency != b.frequency)
-          return a.frequency < b.frequency;
+			var alphabeticLanguagesList = languages.sort(function(a, b) {
+				if (a.frequency != b.frequency)
+					return a.frequency < b.frequency;
 
-        return a.name.localeCompare(b.name);
-      });
+				return a.name.localeCompare(b.name);
+			});
 
-      res.render('entity/create/creator', {
-        genders: genderList,
-        languages: alphabeticLanguagesList,
-        creatorTypes: creatorTypes
-      });
-    });
+			res.render('entity/create/creator', {
+				genders: genderList,
+				languages: alphabeticLanguagesList,
+				creatorTypes: creatorTypes
+			});
+		});
 });
 
+router.post('/create/handler', auth.isAuthenticated, function(req, res) {
+	var changes = {
+		'bbid': null,
+		'ended': req.body.ended
+	};
 
-router.post('/handler', auth.isAuthenticated, function(req, res) {
-  var changes = {
-    'bbid': null,
-    'ended': req.body.ended
-  };
+	if (req.body.creatorTypeId) {
+		changes.creator_type = {
+			creator_type_id: req.body.creatorTypeId
+		};
+	}
 
-  if (req.body.creatorTypeId) {
-    changes.creator_type = {
-      creator_type_id: req.body.creatorTypeId
-    };
-  }
+	if (req.body.genderId) {
+		changes.gender = {
+			gender_id: req.body.genderId
+		};
+	}
 
-  if (req.body.genderId) {
-    changes.gender = {
-      gender_id: req.body.genderId
-    };
-  }
+	if (req.body.beginDate) {
+		changes.begin_date = req.body.beginDate;
+	}
 
-  if (req.body.beginDate) {
-    changes.begin_date = req.body.beginDate;
-  }
+	if (req.body.endDate) {
+		changes.end_date = req.body.endDate;
+	}
 
-  if (req.body.endDate) {
-    changes.end_date = req.body.endDate;
-  }
+	if (req.body.disambiguation)
+		changes.disambiguation = req.body.disambiguation;
 
-  if (req.body.disambiguation)
-    changes.disambiguation = req.body.disambiguation;
+	if (req.body.annotation)
+		changes.annotation = req.body.annotation;
 
-  if (req.body.annotation)
-    changes.annotation = req.body.annotation;
+	if (req.body.note) {
+		changes.revision = {
+			note: req.body.note
+		};
+	}
 
-  if (req.body.note) {
-    changes.revision = {
-      note: req.body.note
-    };
-  }
+	if (req.body.aliases.length) {
+		var default_alias = req.body.aliases[0];
 
-  if (req.body.aliases.length) {
-    var default_alias = req.body.aliases[0];
+		changes.aliases = [{
+			name: default_alias.name,
+			sort_name: default_alias.sortName,
+			language_id: default_alias.languageId,
+			primary: default_alias.primary,
+			default: true
+		}];
+	}
 
-    changes.aliases = [{
-      name: default_alias.name,
-      sort_name: default_alias.sortName,
-      language_id: default_alias.languageId,
-      primary: default_alias.primary,
-      default: true
-    }];
-  }
-
-  Creator.create(changes, {
-    session: req.session
-  })
-    .then(function(revision) {
-      res.send(revision);
-    });
+	Creator.create(changes, {
+		session: req.session
+	})
+		.then(function(revision) {
+			res.send(revision);
+		});
 });
 
 module.exports = router;
--- routes/entity/publication.js
+++ routes/entity/publication.js
@@ -9,84 +9,81 @@ var express = require('express'),
 // Viewing
 
 router.get('/:id', function(req, res, next) {
-  var render = function(publication) {
-    res.render('entity/view/publication', {
-      title: 'BookBrainz',
-      entity: publication
-    });
-  };
+	var render = function(publication) {
+		res.render('entity/view/publication', {
+			title: 'BookBrainz',
+			entity: publication
+		});
+	};
 
-  Publication.findOne(req.params.id, {
-    populate: [
-      'annotation',
-      'disambiguation'
-    ]
-  })
-    .then(render)
-    .catch(function(err) {
-      console.log(err.stack);
-      next(err);
-    });
+	Publication.findOne(req.params.id, {
+		populate: [
+			'annotation',
+			'disambiguation'
+		]
+	})
+		.then(render)
+		.catch(function(err) {
+			console.log(err.stack);
+			next(err);
+		});
 });
-
-module.exports = router;
-
 
 // Creation
 
-router.get('/', auth.isAuthenticated, function(req, res) {
-  // Get the list of publication types
-  var publicationTypesPromise = PublicationType.find();
-  var languagesPromise = Language.find();
+router.get('/create', auth.isAuthenticated, function(req, res) {
+	// Get the list of publication types
+	var publicationTypesPromise = PublicationType.find();
+	var languagesPromise = Language.find();
 
-  Promise.join(publicationTypesPromise, languagesPromise,
-    function(publicationTypes, languages) {
-      var alphabeticLanguagesList = languages.sort(function(a, b) {
-        if (a.frequency != b.frequency)
-          return a.frequency < b.frequency;
+	Promise.join(publicationTypesPromise, languagesPromise,
+		function(publicationTypes, languages) {
+			var alphabeticLanguagesList = languages.sort(function(a, b) {
+				if (a.frequency != b.frequency)
+					return a.frequency < b.frequency;
 
-        return a.name.localeCompare(b.name);
-      });
+				return a.name.localeCompare(b.name);
+			});
 
-      res.render('entity/create/publication', {
-        languages: alphabeticLanguagesList,
-        publicationTypes: publicationTypes
-      });
-    });
+			res.render('entity/create/publication', {
+				languages: alphabeticLanguagesList,
+				publicationTypes: publicationTypes
+			});
+		});
 });
 
-router.post('/handler', auth.isAuthenticated, function(req, res) {
-  var changes = {
-    'bbid': null,
-    'publication_type': {
-      publication_type_id: req.body.publicationTypeId
-    }
-  };
+router.post('/create/handler', auth.isAuthenticated, function(req, res) {
+	var changes = {
+		'bbid': null,
+		'publication_type': {
+			publication_type_id: req.body.publicationTypeId
+		}
+	};
 
-  if (req.body.disambiguation)
-    changes.disambiguation = req.body.disambiguation;
+	if (req.body.disambiguation)
+		changes.disambiguation = req.body.disambiguation;
 
-  if (req.body.annotation)
-    changes.annotation = req.body.annotation;
+	if (req.body.annotation)
+		changes.annotation = req.body.annotation;
 
-  if (req.body.aliases.length) {
-    var default_alias = req.body.aliases[0];
+	if (req.body.aliases.length) {
+		var default_alias = req.body.aliases[0];
 
-    changes.aliases = [{
-      name: default_alias.name,
-      sort_name: default_alias.sortName,
-      language_id: default_alias.languageId,
-      primary: default_alias.primary,
-      default: true
-    }];
-  }
+		changes.aliases = [{
+			name: default_alias.name,
+			sort_name: default_alias.sortName,
+			language_id: default_alias.languageId,
+			primary: default_alias.primary,
+			default: true
+		}];
+	}
 
-  Publication.create(changes, {
-    session: req.session
-  })
-    .then(function(revision) {
-      res.send(revision);
-    });
+	Publication.create(changes, {
+		session: req.session
+	})
+		.then(function(revision) {
+			res.send(revision);
+		});
 });
 
 module.exports = router;
