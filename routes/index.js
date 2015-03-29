b26076fdd115d8499e811475d65a0f4afeaa3e77
Ben Ockmore <ben.sput@gmail.com>
2015-03-29 12:43:27 +0100

    Added search functionality to site.

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var _ = require('underscore');
 var Revision = rootRequire('data/properties/revision');
+var bbws = rootRequire('helpers/bbws');
+var Promise = require('bluebird');
+var Publication = rootRequire('data/entities/publication');
+var Creator = rootRequire('data/entities/creator');
+var Edition = rootRequire('data/entities/edition');
+var Work = rootRequire('data/entities/work');
+var Publisher = rootRequire('data/entities/publisher');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -49,5 +56,43 @@ router.get('/getStarted', function(req, res) {
 		title: 'BookBrainz :: Get Started',
 	});
 });
+
+router.get('/search', function(req, res) {
+	query = req.query.q;
+	var resultsPromise = bbws.get('/search?q=' + query);
+
+	var entitiesPromise = resultsPromise
+	.then(function(results) {
+		var entities = results.objects.map(function(entity_stub) {
+			if (entity_stub._type == 'Publication') {
+				return Publication.findOne(entity_stub.entity_gid);
+			} else if (entity_stub._type == 'Creator') {
+				return Creator.findOne(entity_stub.entity_gid);
+			} else if (entity_stub._type == 'Edition') {
+				return Edition.findOne(entity_stub.entity_gid);
+			} else if (entity_stub._type == 'Work') {
+				return Work.findOne(entity_stub.entity_gid);
+			} else if (entity_stub._type == 'Publisher') {
+				return Publisher.findOne(entity_stub.entity_gid);
+			}
+		});
+
+		return Promise.all(entities);
+	});
+
+	Promise.join(resultsPromise, entitiesPromise, function(results, entities) {
+		entities.forEach(function(entity, i) {
+			entity.type = results.objects[i]._type;
+		});
+
+		console.log(results);
+		res.render('search', {
+			title: 'BookBrainz :: Search "' + query + '"',
+			query: query,
+			results: entities
+		});
+	});
+});
+
 
 module.exports = router;
