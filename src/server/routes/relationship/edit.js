68a08eb00a5060c07c23c1c0aa39b1c15af07c34
Seán de Búrca <leftmostcat@gmail.com>
2015-04-11 01:52:29 -0600

    Use :bbid parameter name for relationship editor

--- src/server/routes/relationship/edit.js
+++ src/server/routes/relationship/edit.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var auth = require('../../helpers/auth');
 var Relationship = require('../../data/relationship');
 
-router.get('/:id/relationships', auth.isAuthenticated, function relationshipEditor(req, res) {
+router.get('/:bbid/relationships', auth.isAuthenticated, function relationshipEditor(req, res) {
 	res.render('relationship/edit', {
-		entityGid: req.params.id
+		entityGid: req.params.bbid
 	});
 });
 
-router.post('/:id/relationships/handler', auth.isAuthenticated, function(req, res) {
+router.post('/:bbid/relationships/handler', auth.isAuthenticated, function(req, res) {
 	req.body.forEach(function(relationship) {
 		// Send a relationship revision for each of the relationships
 		var changes = relationship;
