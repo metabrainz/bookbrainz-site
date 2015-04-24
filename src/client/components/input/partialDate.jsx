fa6e09d16db0fa3370dd7c3ca471b818a11a1d94
Seán de Búrca <leftmostcat@gmail.com>
2015-04-24 14:38:05 -0600

    Whitespace cleanup

--- src/client/components/input/partialDate.jsx
+++ src/client/components/input/partialDate.jsx
@@ -6,62 +6,64 @@ var ym_re = /^\d{4}-\d{2}$/;
 var y_re = /^\d{4}$/;
 
 var PartialDate = React.createClass({
-  getValue: function() {
-    return this.state.value;
-  },
-  getInitialState: function() {
-    return {
-      value: this.props.defaultValue,
-      valid: this.validate(this.props.defaultValue),
-    };
-  },
-  validate: function(value) {
-    if(!value) {
-      return true;
-    } else {
-      return Boolean(ymd_re.test(value) || ym_re.test(value) ||
-                     y_re.test(value));
-    }
-  },
-  handleChange: function() {
-    var input = this.refs.input.getValue().trim();
-    if(input.length > 10) {
-      return;
-    }
+	getValue: function() {
+		return this.state.value;
+	},
+	getInitialState: function() {
+		return {
+			value: this.props.defaultValue,
+			valid: this.validate(this.props.defaultValue),
+		};
+	},
+	validate: function(value) {
+		if (!value) {
+			return true;
+		}
+		else {
+			return Boolean(ymd_re.test(value) || ym_re.test(value) ||
+                           y_re.test(value));
+		}
+	},
+	handleChange: function() {
+		var input = this.refs.input.getValue().trim();
 
-    this.setState({
-        value: input,
-        valid: this.validate(input),
-    });
-  },
-  valid: function() {
-    return this.state.valid;
-  },
-  validationState: function() {
-    var validationClass = null;
+		if (input.length > 10) {
+			return;
+		}
 
-    if(this.state.value) {
-      validationClass = this.state.valid ? 'success' : 'error';
-    }
+		this.setState({
+			value: input,
+			valid: this.validate(input),
+		});
+	},
+	valid: function() {
+		return this.state.valid;
+	},
+	validationState: function() {
+		var validationClass = null;
 
-    return validationClass;
-  },
-  render: function() {
-    return (
-      <Input
-        type='text'
-        value={this.state.value}
-        placeholder={this.props.placeholder}
-        label={this.props.label}
-        help={this.props.help}
-        bsStyle={this.validationState()}
-        ref='input'
-        groupClassName={this.props.groupClassName}
-        wrapperClassName={this.props.wrapperClassName}
-        labelClassName={this.props.labelClassName}
-        onChange={this.handleChange} />
-    );
-  }
+		if (this.state.value) {
+			validationClass = this.state.valid ? 'success' : 'error';
+		}
+
+		return validationClass;
+	},
+	render: function() {
+		return (
+			<Input
+				type='text'
+				value={this.state.value}
+				placeholder={this.props.placeholder}
+				label={this.props.label}
+				help={this.props.help}
+				bsStyle={this.validationState()}
+				ref='input'
+				groupClassName={this.props.groupClassName}
+				wrapperClassName={this.props.wrapperClassName}
+				labelClassName={this.props.labelClassName}
+				onChange={this.handleChange} />
+		);
+	}
 });
 
 
--- src/client/components/input/uuid.jsx
+++ src/client/components/input/uuid.jsx
@@ -2,64 +2,64 @@ var React = require('react');
 var Input = require('react-bootstrap').Input;
 
 var uuid_re =
-  /[a-f0-9]{8}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{12}/;
+	/[a-f0-9]{8}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{12}/;
 
 var UUIDInput = React.createClass({
-  getInitialState: function() {
-    return {
-      value: this.props.defaultValue,
-      valid: true,
-    };
-  },
-  getValue: function() {
-    return this.state.value;
-  },
-  valid: function() {
-    return this.state.valid;
-  },
-  validationState: function() {
-    if (this.state.valid) {
-      return 'success';
-    } else {
-      return 'error';
-    }
-  },
-  handleChange: function(e) {
-    // This could also be done using ReactLink:
-    // http://facebook.github.io/react/docs/two-way-binding-helpers.html
+	getInitialState: function() {
+		return {
+			value: this.props.defaultValue,
+			valid: true,
+		};
+	},
+	getValue: function() {
+		return this.state.value;
+	},
+	valid: function() {
+		return this.state.valid;
+	},
+	validationState: function() {
+		if (this.state.valid) {
+			return 'success';
+		} else {
+			return 'error';
+		}
+	},
+	handleChange: function(e) {
+		// This could also be done using ReactLink:
+		// http://facebook.github.io/react/docs/two-way-binding-helpers.html
 
-    var result = uuid_re.exec(this.refs.input.getValue());
+		var result = uuid_re.exec(this.refs.input.getValue());
 
-    var valid = Boolean(result);
-    if (valid) {
-      result = result[0];
-    } else {
-      result = this.refs.input.getValue();
-    }
+		var valid = Boolean(result);
+		if (valid) {
+			result = result[0];
+		} else {
+			result = this.refs.input.getValue();
+		}
 
-    this.setState({
-        value: result,
-        valid: valid,
-    }, this.props.onChange ? this.props.onChange.bind(this, e) : null);
-  },
-  render: function() {
-    return (
-      <Input
-        type='text'
-        value={this.state.value}
-        placeholder={this.props.placeholder}
-        label={this.props.label}
-        help={this.props.help}
-        bsStyle={this.validationState()}
-        ref='input'
-        groupClassName={this.props.groupClassName}
-        wrapperClassName={this.props.wrapperClassName}
-        labelClassName={this.props.labelClassName}
-        onChange={this.handleChange}
-        disabled={this.props.disabled}
-        standalone={this.props.standalone}/>
-    );
-  }
+		this.setState({
+				value: result,
+				valid: valid,
+		}, this.props.onChange ? this.props.onChange.bind(this, e) : null);
+	},
+	render: function() {
+		return (
+			<Input
+				type='text'
+				value={this.state.value}
+				placeholder={this.props.placeholder}
+				label={this.props.label}
+				help={this.props.help}
+				bsStyle={this.validationState()}
+				ref='input'
+				groupClassName={this.props.groupClassName}
+				wrapperClassName={this.props.wrapperClassName}
+				labelClassName={this.props.labelClassName}
+				onChange={this.handleChange}
+				disabled={this.props.disabled}
+				standalone={this.props.standalone}/>
+		);
+	}
 });
 
 module.exports = UUIDInput;
