222d032f5cf5e6693c3ebd68e919b7a1bfa0a19e
Seán de Búrca <leftmostcat@gmail.com>
2015-11-19 00:57:36 -0700

    Further FK cleanup

--- scripts/create_foreign_keys.sql
+++ scripts/create_foreign_keys.sql
@@ -1,14 +1,12 @@
-
-
 ALTER TABLE bookbrainz.editor ADD FOREIGN KEY (editor_type_id) REFERENCES bookbrainz.editor_type (id);
 ALTER TABLE bookbrainz.editor ADD FOREIGN KEY (gender_id) REFERENCES musicbrainz.gender (id);
 
 ALTER TABLE bookbrainz.editor_language ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
-ALTER TABLE bookbrainz.editor_language ADD FOREIGN KEY (language_id) REFERENCES musicbrainz.language (language_id);
+ALTER TABLE bookbrainz.editor_language ADD FOREIGN KEY (language_id) REFERENCES musicbrainz.language (id);
 
 ALTER TABLE bookbrainz.message ADD FOREIGN KEY (sender_id) REFERENCES bookbrainz.editor (id);
 
-ALTER TABLE bookbrainz.message_receipt ADD FOREIGN KEY (message_id) REFERENCES bookbrainz.message (message_id);
+ALTER TABLE bookbrainz.message_receipt ADD FOREIGN KEY (message_id) REFERENCES bookbrainz.message (id);
 ALTER TABLE bookbrainz.message_receipt ADD FOREIGN KEY (recipient_id) REFERENCES bookbrainz.editor (id);
 
 ALTER TABLE bookbrainz.oauth_client ADD FOREIGN KEY (owner_id) REFERENCES bookbrainz.editor (id);
