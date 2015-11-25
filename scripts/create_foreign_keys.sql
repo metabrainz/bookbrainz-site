5ff422fabdd0d094e3f6b86546011e0377371279
Seán de Búrca <leftmostcat@gmail.com>
2015-11-25 00:14:10 -0700

    Add foreign keys for entity data tables

--- scripts/create_foreign_keys.sql
+++ scripts/create_foreign_keys.sql
@@ -12,3 +12,28 @@ ALTER TABLE bookbrainz.message_receipt ADD FOREIGN KEY (recipient_id) REFERENCES
 ALTER TABLE bookbrainz.oauth_client ADD FOREIGN KEY (owner_id) REFERENCES bookbrainz.editor (id);
 
 ALTER TABLE bookbrainz.revision ADD FOREIGN KEY (author_id) REFERENCES bookbrainz.editor (id);
+
+-------------
+-- Entities
+-------------
+
+ALTER TABLE bookbrainz.creator_data ADD FOREIGN KEY (entity_data_id) REFERENCES bookbrainz.entity_data (id);
+ALTER TABLE bookbrainz.creator_data ADD FOREIGN KEY (gender_id) REFERENCES musicbrainz.gender (id);
+ALTER TABLE bookbrainz.creator_data ADD FOREIGN KEY (creator_type_id) REFERENCES bookbrainz.creator_type (id);
+
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (entity_data_id) REFERENCES bookbrainz.entity_data (id);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (publication_bbid) REFERENCES bookbrainz.entity (bbid);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (creator_credit_id) REFERENCES bookbrainz.creator_credit (id);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (language_id) REFERENCES musicbrainz.language (id);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (edition_format_id) REFERENCES bookbrainz.edition_format (id);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (edition_status_id) REFERENCES bookbrainz.edition_status (id);
+ALTER TABLE bookbrainz.edition_data ADD FOREIGN KEY (publisher_bbid) REFERENCES bookbrainz.entity (bbid);
+
+ALTER TABLE bookbrainz.publication_data ADD FOREIGN KEY (entity_data_id) REFERENCES bookbrainz.entity_data (id);
+ALTER TABLE bookbrainz.publication_data ADD FOREIGN KEY (publication_type_id) REFERENCES bookbrainz.publication_type (id);
+
+ALTER TABLE bookbrainz.publisher_data ADD FOREIGN KEY (entity_data_id) REFERENCES bookbrainz.entity_data (id);
+ALTER TABLE bookbrainz.publisher_data ADD FOREIGN KEY (publisher_type_id) REFERENCES bookbrainz.publisher_type (id);
+
+ALTER TABLE bookbrainz.work_data ADD FOREIGN KEY (entity_data_id) REFERENCES bookbrainz.entity_data (id);
+ALTER TABLE bookbrainz.work_data ADD FOREIGN KEY (work_type_id) REFERENCES bookbrainz.work_type (id);
