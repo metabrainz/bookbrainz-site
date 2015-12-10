217bf11bf49300a67bfebd8a9969e85d4e59f49b
Annie <anniezhou301@gmail.com>
2015-12-09 22:53:04 -0500

    React

--- src/client/components/pages/register.jsx
+++ src/client/components/pages/register.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
-const PageHeader = require('react-bootstrap').PageHeader;
+const Input = require('react-bootstrap').Input;
+const Button = require('react-bootstrap').Button;
 
 module.exports = React.createClass({
 	displayName: 'RegistrationPage',
@@ -8,36 +9,30 @@ module.exports = React.createClass({
 
 		return (
 			<div className="row">
-        <div className="col-md-6 col-md-offset-3">
-          <form action="/register/handler" method="post" className="whole-page-form form-horizontal">
-            <div className="form-group">
-              <label htmlFor="registerUsername" className="col-md-2">Username</label>
-              <div className="col-md-10">
-                <input id="registerUsername" type="text" placeholder="Username" name="username" className="form-control" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="registerEmail" className="col-md-2">Email</label>
-              <div className="col-md-10">
-                <input id="registerEmail" type="email" placeholder="email@example.com" name="email" className="form-control" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="registerPassword" className="col-md-2">Password</label>
-              <div className="col-md-10">
-                <input id="registerPassword" type="password" placeholder="Password" name="password" className="form-control" />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="registerPassword2" className="col-md-2">Repeat Password</label>
-              <div className="col-md-10">
-                <input id="registerPassword2" type="password" placeholder="Password" name="password2" className="form-control" />
-              </div>
-            </div>
-            <button type="submit" className="btn btn-success btn-lg btn-block">Register</button>
-          </form>
-        </div>
-      </div>
-    );
+        		<div className="col-md-6 col-md-offset-3">
+          			<form action="/register/handler" method="post" className="whole-page-form form-horizontal">
+            			<div className="form-group">
+              
+                		<Input id="registerUsername" label="Username" labelClassName="col-md-2" wrapperClassName="col-md-10" type="text" placeholder="Username" name="username" className="form-control" />
+              
+            			</div>
+            		<div className="form-group">
+                		<Input id="registerEmail" label="Email" labelClassName="col-md-2" wrapperClassName="col-md-10" type="email" placeholder="email@example.com" name="email" className="form-control" />
+          
+            		</div>
+            		<div className="form-group">
+                		<Input id="registerPassword" label="Password" labelClassName="col-md-2" wrapperClassName="col-md-10" type="password" placeholder="Password" name="password" className="form-control" />
+
+            		</div>
+            		<div className="form-group">
+
+                		<Input id="registerPassword2" label="Repeat Password" labelClassName="col-md-2" wrapperClassName="col-md-10" type="password" placeholder="Password" name="password2" className="form-control" />
+              
+            		</div>	
+            	<Button type="submit" bsStyle="success" bsSize="large" block>Register</Button>
+          		</form>
+        	</div>
+      	</div>
+    	);
 	}
 });
