cf12c0db896cc9cd57a5e8479ce9b487d0f9479d
Ben Ockmore <ben.sput@gmail.com>
2015-12-13 23:20:28 +0000

    Added MB areas to the musicbrainz schema

--- schemas/musicbrainz.sql
+++ schemas/musicbrainz.sql
@@ -18,4 +18,48 @@ CREATE TABLE musicbrainz.language (
     iso_code_3          CHAR(3)  -- ISO 639-3
 );
 
+CREATE TABLE musicbrainz.area_type ( -- replicate
+    id                  SERIAL, -- PK
+    name                VARCHAR(255) NOT NULL,
+    parent              INTEGER, -- references area_type.id
+    child_order         INTEGER NOT NULL DEFAULT 0,
+    description         TEXT
+);
+
+
+CREATE TABLE musicbrainz.area ( -- replicate (verbose)
+    id                  SERIAL, -- PK
+    gid                 uuid NOT NULL,
+    name                VARCHAR NOT NULL,
+    type                INTEGER, -- references area_type.id
+    edits_pending       INTEGER NOT NULL DEFAULT 0 CHECK (edits_pending >=0),
+    last_updated        TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    begin_date_year     SMALLINT,
+    begin_date_month    SMALLINT,
+    begin_date_day      SMALLINT,
+    end_date_year       SMALLINT,
+    end_date_month      SMALLINT,
+    end_date_day        SMALLINT,
+    ended               BOOLEAN NOT NULL DEFAULT FALSE
+      CHECK (
+        (
+          -- If any end date fields are not null, then ended must be true
+          (end_date_year IS NOT NULL OR
+           end_date_month IS NOT NULL OR
+           end_date_day IS NOT NULL) AND
+          ended = TRUE
+        ) OR (
+          -- Otherwise, all end date fields must be null
+          (end_date_year IS NULL AND
+           end_date_month IS NULL AND
+           end_date_day IS NULL)
+        )
+      ),
+    comment             VARCHAR(255) NOT NULL DEFAULT ''
+);
+
+CREATE TABLE musicbrainz.country_area ( -- replicate (verbose)
+    area                INTEGER -- PK, references area.id
+);
+
 COMMIT;
