7174205ca64be8e10d5824908de19d41b3e1134a
Ben Ockmore <ben.sput@gmail.com>
2016-02-14 00:41:27 +0000

    Added trigger for processing edition inserts, updates and deletions

--- scripts/create_triggers.sql
+++ scripts/create_triggers.sql
@@ -48,10 +48,60 @@ CREATE OR REPLACE FUNCTION bookbrainz.process_creator() RETURNS TRIGGER
 	END;
 $process_creator$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION bookbrainz.process_edition() RETURNS TRIGGER
+	AS $process_edition$
+	DECLARE
+		edition_data_id INT;
+	BEGIN
+		IF (TG_OP = 'INSERT') THEN
+			IF (NEW.bbid IS NULL) THEN
+				INSERT INTO bookbrainz.entity(type) VALUES('Edition') RETURNING bookbrainz.entity.bbid INTO NEW.bbid;
+			ELSE
+				INSERT INTO bookbrainz.entity(bbid, type) VALUES(NEW.bbid, 'Edition');
+			END IF;
+			INSERT INTO bookbrainz.edition_header(bbid) VALUES(NEW.bbid);
+		END IF;
+
+		-- If we're not deleting, create new entity data rows as necessary
+		IF (TG_OP <> 'DELETE') THEN
+			INSERT INTO bookbrainz.edition_data(
+				alias_set_id, identifier_set_id, relationship_set_id, annotation_id,
+				disambiguation_id, publication_bbid, creator_credit_id, width, height,
+				depth, weight, pages, format_id, status_id
+			) VALUES (
+				NEW.alias_set_id, NEW.identifier_set_id, NEW.relationship_set_id,
+				NEW.annotation_id, NEW.disambiguation_id, NEW.publication_bbid,
+				NEW.creator_credit_id, NEW.width, NEW.height, NEW.depth,
+				NEW.weight, NEW.pages, NEW.format_id, NEW.status_id
+			) RETURNING bookbrainz.edition_data.id INTO edition_data_id;
+		END IF;
+
+		INSERT INTO bookbrainz.edition_revision VALUES(NEW.revision_id, NEW.bbid, edition_data_id);
+
+		UPDATE bookbrainz.edition_header
+			SET master_revision_id = NEW.revision_id
+			WHERE bbid = NEW.bbid;
+
+		IF (TG_OP = 'DELETE') THEN
+			RETURN OLD;
+		ELSE
+			RETURN NEW;
+		END IF;
+	END;
+$process_edition$ LANGUAGE plpgsql;
+
 BEGIN;
 
 CREATE TRIGGER process_creator
 	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.creator
 	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_creator();
+
+COMMIT;
+
+BEGIN;
+
+CREATE TRIGGER process_edition
+	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.edition
+	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_edition();
 
 COMMIT;
