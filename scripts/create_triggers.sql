2312006dde7c9b5a92917545709a6ac1cf293b82
Ben Ockmore <ben.sput@gmail.com>
2016-03-10 01:27:18 +0000

    Added triggers for work, publisher and publication views

--- scripts/create_triggers.sql
+++ scripts/create_triggers.sql
@@ -90,6 +90,132 @@ CREATE OR REPLACE FUNCTION bookbrainz.process_edition() RETURNS TRIGGER
 	END;
 $process_edition$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION bookbrainz.process_work() RETURNS TRIGGER
+	AS $process_work$
+	DECLARE
+		work_data_id INT;
+	BEGIN
+		IF (TG_OP = 'INSERT') THEN
+			IF (NEW.bbid IS NULL) THEN
+				INSERT INTO bookbrainz.entity(type) VALUES('Work') RETURNING bookbrainz.entity.bbid INTO NEW.bbid;
+			ELSE
+				INSERT INTO bookbrainz.entity(bbid, type) VALUES(NEW.bbid, 'Work');
+			END IF;
+			INSERT INTO bookbrainz.work_header(bbid) VALUES(NEW.bbid);
+		END IF;
+
+		-- If we're not deleting, create new entity data rows as necessary
+		IF (TG_OP <> 'DELETE') THEN
+			INSERT INTO bookbrainz.work_data(
+				alias_set_id, identifier_set_id, relationship_set_id, annotation_id,
+				disambiguation_id, type_id
+			) VALUES (
+				NEW.alias_set_id, NEW.identifier_set_id, NEW.relationship_set_id,
+				NEW.annotation_id, NEW.disambiguation_id, NEW.type_id
+			) RETURNING bookbrainz.work_data.id INTO work_data_id;
+		END IF;
+
+		INSERT INTO bookbrainz.work_revision VALUES(NEW.revision_id, NEW.bbid, work_data_id);
+
+		UPDATE bookbrainz.work_header
+			SET master_revision_id = NEW.revision_id
+			WHERE bbid = NEW.bbid;
+
+		IF (TG_OP = 'DELETE') THEN
+			RETURN OLD;
+		ELSE
+			RETURN NEW;
+		END IF;
+	END;
+$process_work$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION bookbrainz.process_publisher() RETURNS TRIGGER
+	AS $process_publisher$
+	DECLARE
+		publisher_data_id INT;
+	BEGIN
+		IF (TG_OP = 'INSERT') THEN
+			IF (NEW.bbid IS NULL) THEN
+				INSERT INTO bookbrainz.entity(type) VALUES('Publisher') RETURNING bookbrainz.entity.bbid INTO NEW.bbid;
+			ELSE
+				INSERT INTO bookbrainz.entity(bbid, type) VALUES(NEW.bbid, 'Publisher');
+			END IF;
+			INSERT INTO bookbrainz.publisher_header(bbid) VALUES(NEW.bbid);
+		END IF;
+
+		IF (NEW.ended IS NULL) THEN
+			NEW.ended = false;
+		END IF;
+
+		-- If we're not deleting, create new entity data rows as necessary
+		IF (TG_OP <> 'DELETE') THEN
+			INSERT INTO bookbrainz.publisher_data(
+				alias_set_id, identifier_set_id, relationship_set_id, annotation_id,
+				disambiguation_id, type_id, begin_year, begin_month, begin_day, end_year,
+				end_month, end_day, ended, area_id
+			) VALUES (
+				NEW.alias_set_id, NEW.identifier_set_id, NEW.relationship_set_id,
+				NEW.annotation_id, NEW.disambiguation_id, NEW.type_id, NEW.begin_year,
+				NEW.begin_month, NEW.begin_day, NEW.end_year, NEW.end_month, NEW.end_day,
+				NEW.ended, NEW.area_id
+			) RETURNING bookbrainz.publisher_data.id INTO publisher_data_id;
+		END IF;
+
+		INSERT INTO bookbrainz.publisher_revision VALUES(NEW.revision_id, NEW.bbid, publisher_data_id);
+
+		UPDATE bookbrainz.publisher_header
+			SET master_revision_id = NEW.revision_id
+			WHERE bbid = NEW.bbid;
+
+		IF (TG_OP = 'DELETE') THEN
+			RETURN OLD;
+		ELSE
+			RETURN NEW;
+		END IF;
+	END;
+$process_publisher$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION bookbrainz.process_publication() RETURNS TRIGGER
+	AS $process_publication$
+	DECLARE
+		publication_data_id INT;
+	BEGIN
+		IF (TG_OP = 'INSERT') THEN
+			IF (NEW.bbid IS NULL) THEN
+				INSERT INTO bookbrainz.entity(type) VALUES('Publication') RETURNING bookbrainz.entity.bbid INTO NEW.bbid;
+			ELSE
+				INSERT INTO bookbrainz.entity(bbid, type) VALUES(NEW.bbid, 'Publication');
+			END IF;
+			INSERT INTO bookbrainz.publication_header(bbid) VALUES(NEW.bbid);
+		END IF;
+
+		-- If we're not deleting, create new entity data rows as necessary
+		IF (TG_OP <> 'DELETE') THEN
+			INSERT INTO bookbrainz.publication_data(
+				alias_set_id, identifier_set_id, relationship_set_id, annotation_id,
+				disambiguation_id, type_id
+			) VALUES (
+				NEW.alias_set_id, NEW.identifier_set_id, NEW.relationship_set_id,
+				NEW.annotation_id, NEW.disambiguation_id, NEW.type_id
+			) RETURNING bookbrainz.publication_data.id INTO publication_data_id;
+		END IF;
+
+		INSERT INTO bookbrainz.publication_revision VALUES(NEW.revision_id, NEW.bbid, publication_data_id);
+
+		UPDATE bookbrainz.publication_header
+			SET master_revision_id = NEW.revision_id
+			WHERE bbid = NEW.bbid;
+
+		IF (TG_OP = 'DELETE') THEN
+			RETURN OLD;
+		ELSE
+			RETURN NEW;
+		END IF;
+	END;
+$process_publication$ LANGUAGE plpgsql;
+
+
 BEGIN;
 
 CREATE TRIGGER process_creator
@@ -103,5 +229,29 @@ BEGIN;
 CREATE TRIGGER process_edition
 	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.edition
 	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_edition();
+
+COMMIT;
+
+BEGIN;
+
+CREATE TRIGGER process_work
+	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.work
+	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_work();
+
+COMMIT;
+
+BEGIN;
+
+CREATE TRIGGER process_publisher
+	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.publisher
+	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_publisher();
+
+COMMIT;
+
+BEGIN;
+
+CREATE TRIGGER process_publication
+	INSTEAD OF INSERT OR UPDATE OR DELETE ON bookbrainz.publication
+	FOR EACH ROW EXECUTE PROCEDURE bookbrainz.process_publication();
 
 COMMIT;
