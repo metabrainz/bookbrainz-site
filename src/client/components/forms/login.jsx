c49eb705ed1982210e3a38ea830c35d3a99b8c52
Seán de Búrca <leftmostcat@gmail.com>
2016-05-01 02:27:13 -0600

    BB-190: Login and registration to ES6

--- src/client/components/forms/login.jsx
+++ src/client/components/forms/login.jsx
@@ -16,6 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 const React = require('react');
 const request = require('superagent-bluebird-promise');
 
@@ -25,121 +26,131 @@ const Input = require('react-bootstrap').Input;
 
 const LoadingSpinner = require('../loading_spinner.jsx');
 
-module.exports = React.createClass({
-	displayName: 'LoginForm',
-	getInitialState() {
-		'use strict';
+(() => {
+	'use strict';
 
-		return {
-			error: null,
-			password: '',
-			waiting: false
-		};
-	},
-	handleSubmit(event) {
-		'use strict';
+	class LoginForm extends React.Component {
+		constructor() {
+			super();
 
-		event.preventDefault();
+			this.state = {
+				error: null,
+				password: '',
+				waiting: false
+			};
 
-		const data = {
-			username: this.username.getValue(),
-			password: this.password.getValue()
-		};
-
-		this.setState({
-			error: null,
-			password: '',
-			waiting: true
-		});
-
-		request.post('/login/handler')
-			.send(data)
-			.then((res) => {
-				let redirectTo = '/';
-				if (res.body && res.body.redirectTo) {
-					redirectTo = res.body.redirectTo;
-				}
-
-				window.location.href = redirectTo;
-			})
-			.catch((res) => {
-				const error = res.body.error;
-				this.setState({
-					error,
-					waiting: false
-				});
-			});
-	},
-	handlePasswordChange(event) {
-		'use strict';
-
-		this.setState({
-			password: event.target.value
-		});
-	},
-	render() {
-		'use strict';
-
-		let errorComponent = null;
-		if (this.state.error) {
-			errorComponent =
-				(<Alert bsStyle="danger">{this.state.error}</Alert>);
+			// React does not autobind non-React class methods
+			this.handlePasswordChange = this.handlePasswordChange.bind(this);
+			this.handleSubmit = this.handleSubmit.bind(this);
 		}
 
-		const loadingComponent = this.state.waiting ? <LoadingSpinner/> : null;
+		handleSubmit(event) {
+			event.preventDefault();
 
-		return (
-			<div className="row">
-				{loadingComponent}
-				<div className="col-md-6 col-md-offset-3">
-					<form
-						className="whole-page-form form-horizontal"
-						onSubmit={this.handleSubmit}
-					>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Username"
-								labelClassName="col-md-2"
-								placeholder="Username"
-								ref={(ref) => this.username = ref}
-								type="text"
-								wrapperClassName="col-md-10"
-							/>
-						</div>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Password"
-								labelClassName="col-md-2"
-								placeholder="Password"
-								ref={(ref) => this.password = ref}
-								type="password"
-								value={this.state.password}
-								wrapperClassName="col-md-10"
-								onChange={this.handlePasswordChange}
-							/>
-						</div>
-						{errorComponent}
-						<Button
-							block
-							bsSize="large"
-							bsStyle="primary"
-							type="submit"
+			const data = {
+				username: this.username.getValue(),
+				password: this.password.getValue()
+			};
+
+			this.setState({
+				error: null,
+				password: '',
+				waiting: true
+			});
+
+			request.post('/login/handler')
+				.send(data)
+				.then((res) => {
+					let redirectTo = '/';
+					if (res.body && res.body.redirectTo) {
+						redirectTo = res.body.redirectTo;
+					}
+
+					window.location.href = redirectTo;
+				})
+				.catch((res) => {
+					const error = res.body.error;
+
+					this.setState({
+						error,
+						waiting: false
+					});
+				});
+		}
+
+		handlePasswordChange(event) {
+			this.setState({
+				password: event.target.value
+			});
+		}
+
+		render() {
+			let errorComponent = null;
+			if (this.state.error) {
+				errorComponent =
+					(<Alert bsStyle="danger">{this.state.error}</Alert>);
+			}
+
+			const loadingComponent = this.state.waiting ?
+				<LoadingSpinner/> : null;
+
+			return (
+				<div className="row">
+					{loadingComponent}
+					<div className="col-md-6 col-md-offset-3">
+						<form
+							className="whole-page-form form-horizontal"
+							onSubmit={this.handleSubmit}
 						>
-							Log In
-						</Button>
-						<hr/>
-						<Button
-							block
-							bsSize="large"
-							href="/register"
-						>
-							Register
-						</Button>
-					</form>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Username"
+									labelClassName="col-md-2"
+									placeholder="Username"
+									ref={(ref) => this.username = ref}
+									type="text"
+									wrapperClassName="col-md-10"
+								/>
+							</div>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Password"
+									labelClassName="col-md-2"
+									placeholder="Password"
+									ref={(ref) => this.password = ref}
+									type="password"
+									value={this.state.password}
+									wrapperClassName="col-md-10"
+									onChange={this.handlePasswordChange}
+								/>
+							</div>
+							{errorComponent}
+							<Button
+								block
+								bsSize="large"
+								bsStyle="primary"
+								type="submit"
+							>
+								Log In
+							</Button>
+							<hr/>
+							<Button
+								block
+								bsSize="large"
+								href="/register"
+							>
+								Register
+							</Button>
+						</form>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
-});
+
+	LoginForm.displayName = 'LoginForm';
+
+	module.exports = LoginForm;
+})();
--- src/client/components/forms/registration.jsx
+++ src/client/components/forms/registration.jsx
@@ -16,6 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 const React = require('react');
 const request = require('superagent-bluebird-promise');
 
@@ -25,143 +26,152 @@ const Input = require('react-bootstrap').Input;
 
 const LoadingSpinner = require('../loading_spinner.jsx');
 
-module.exports = React.createClass({
-	displayName: 'RegistrationForm',
-	getInitialState() {
-		'use strict';
+(() => {
+	'use strict';
 
-		return {
-			error: null,
-			password: '',
-			passwordRepeat: '',
-			waiting: false
-		};
-	},
-	handleSubmit(event) {
-		'use strict';
+	class RegistrationForm extends React.Component {
+		constructor() {
+			super();
 
-		event.preventDefault();
+			this.state = {
+				error: null,
+				password: '',
+				passwordRepeat: '',
+				waiting: false
+			};
 
-		const data = {
-			username: this.username.getValue(),
-			email: this.email.getValue(),
-			password: this.password.getValue(),
-			passwordRepeat: this.passwordRepeat.getValue()
-		};
-
-		this.setState({
-			error: null,
-			password: '',
-			passwordRepeat: '',
-			waiting: true
-		});
-
-		request.post('/register/handler')
-			.send(data)
-			.then(() => {
-				window.location.href = '/';
-			})
-			.catch((res) => {
-				const error = res.body.error;
-				this.setState({
-					error,
-					waiting: false
-				});
-			});
-	},
-	handlePasswordChange(event) {
-		'use strict';
-
-		this.setState({
-			password: event.target.value
-		});
-	},
-	handlePasswordRepeatChange(event) {
-		'use strict';
-
-		this.setState({
-			passwordRepeat: event.target.value
-		});
-	},
-	render() {
-		'use strict';
-
-		let errorComponent = null;
-		if (this.state.error) {
-			errorComponent =
-				(<Alert bsStyle="danger">{this.state.error}</Alert>);
+			this.handleSubmit = this.handleSubmit.bind(this);
+			this.handlePasswordChange = this.handlePasswordChange.bind(this);
+			this.handlePasswordRepeatChange =
+				this.handlePasswordRepeatChange.bind(this);
 		}
 
-		const loadingComponent = this.state.waiting ? <LoadingSpinner/> : null;
+		handleSubmit(event) {
+			event.preventDefault();
 
-		return (
-			<div className="row">
-				{loadingComponent}
-				<div className="col-md-6 col-md-offset-3">
-					<form
-						className="whole-page-form form-horizontal"
-						onSubmit={this.handleSubmit}
-					>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Username"
-								labelClassName="col-md-2"
-								placeholder="Username"
-								ref={(ref) => this.username = ref}
-								type="text"
-								wrapperClassName="col-md-10"
-							/>
-						</div>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Email"
-								labelClassName="col-md-2"
-								placeholder="email@example.com"
-								ref={(ref) => this.email = ref}
-								type="email"
-								wrapperClassName="col-md-10"
-							/>
-						</div>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Password"
-								labelClassName="col-md-2"
-								placeholder="Password"
-								ref={(ref) => this.password = ref}
-								type="password"
-								value={this.state.password}
-								wrapperClassName="col-md-10"
-								onChange={this.handlePasswordChange}
-							/>
-						</div>
-						<div className="form-group">
-							<Input
-								className="form-control"
-								label="Repeat Password"
-								labelClassName="col-md-2"
-								placeholder="Password"
-								ref={(ref) => this.passwordRepeat = ref}
-								type="password"
-								value={this.state.passwordRepeat}
-								wrapperClassName="col-md-10"
-								onChange={this.handlePasswordRepeatChange}
-							/>
-						</div>
-						{errorComponent}
-						<Button
-							block
-							bsSize="large"
-							bsStyle="primary"
-							type="submit"
+			const data = {
+				username: this.username.getValue(),
+				email: this.email.getValue(),
+				password: this.password.getValue(),
+				passwordRepeat: this.passwordRepeat.getValue()
+			};
+
+			this.setState({
+				error: null,
+				password: '',
+				passwordRepeat: '',
+				waiting: true
+			});
+
+			request.post('/register/handler')
+				.send(data)
+				.then(() => {
+					window.location.href = '/';
+				})
+				.catch((res) => {
+					const error = res.body.error;
+					this.setState({
+						error,
+						waiting: false
+					});
+				});
+		}
+
+		handlePasswordChange(event) {
+			this.setState({
+				password: event.target.value
+			});
+		}
+
+		handlePasswordRepeatChange(event) {
+			this.setState({
+				passwordRepeat: event.target.value
+			});
+		}
+
+		render() {
+			let errorComponent = null;
+			if (this.state.error) {
+				errorComponent =
+					(<Alert bsStyle="danger">{this.state.error}</Alert>);
+			}
+
+			const loadingComponent = this.state.waiting ?
+				<LoadingSpinner/> : null;
+
+			return (
+				<div className="row">
+					{loadingComponent}
+					<div className="col-md-6 col-md-offset-3">
+						<form
+							className="whole-page-form form-horizontal"
+							onSubmit={this.handleSubmit}
 						>
-							Register
-						</Button>
-					</form>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Username"
+									labelClassName="col-md-2"
+									placeholder="Username"
+									ref={(ref) => this.username = ref}
+									type="text"
+									wrapperClassName="col-md-10"
+								/>
+							</div>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Email"
+									labelClassName="col-md-2"
+									placeholder="email@example.com"
+									ref={(ref) => this.email = ref}
+									type="email"
+									wrapperClassName="col-md-10"
+								/>
+							</div>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Password"
+									labelClassName="col-md-2"
+									placeholder="Password"
+									ref={(ref) => this.password = ref}
+									type="password"
+									value={this.state.password}
+									wrapperClassName="col-md-10"
+									onChange={this.handlePasswordChange}
+								/>
+							</div>
+							<div className="form-group">
+								<Input
+									className="form-control"
+									label="Repeat Password"
+									labelClassName="col-md-2"
+									placeholder="Password"
+									ref={(ref) => this.passwordRepeat = ref}
+									type="password"
+									value={this.state.passwordRepeat}
+									wrapperClassName="col-md-10"
+									onChange={this.handlePasswordRepeatChange}
+								/>
+							</div>
+							{errorComponent}
+							<Button
+								block
+								bsSize="large"
+								bsStyle="primary"
+								type="submit"
+							>
+								Register
+							</Button>
+						</form>
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
-});
+
+	RegistrationForm.displayName = 'RegistrationForm';
+
+	module.exports = RegistrationForm;
+})();
