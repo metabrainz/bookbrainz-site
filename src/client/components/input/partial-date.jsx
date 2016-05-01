748b40692b0ecd10d69fcb9492c59e4161d3809c
Seán de Búrca <leftmostcat@gmail.com>
2016-05-01 11:55:15 -0600

    BB-190: All input and page components to classes

--- src/client/components/input/partial-date.jsx
+++ src/client/components/input/partial-date.jsx
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2015  Ben Ockmore
- *               2015  Sean Burke
+ * Copyright (C) 2015       Ben Ockmore
+ *               2015-2016  Sean Burke
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,9 +25,89 @@ const ymdRegex = /^\d{4}-\d{2}-\d{2}$/;
 const ymRegex = /^\d{4}-\d{2}$/;
 const yRegex = /^\d{4}$/;
 
-const PartialDate = React.createClass({
-	displayName: 'partialDateInput',
-	propTypes: {
+(() => {
+	'use strict';
+
+	class PartialDate extends React.Component {
+		static validate(value) {
+			if (!value) {
+				return true;
+			}
+
+			const validSyntax = Boolean(
+				ymdRegex.test(value) ||
+				ymRegex.test(value) ||
+				yRegex.test(value)
+			);
+			const validValue = !isNaN(Date.parse(value));
+
+			return validSyntax && validValue;
+		}
+
+		constructor(props) {
+			super(props);
+
+			this.state = {
+				value: this.props.defaultValue,
+				valid: PartialDate.validate(this.props.defaultValue)
+			};
+
+			// React does not autobind non-React class methods
+			this.handleChange = this.handleChange.bind(this);
+		}
+
+		getValue() {
+			return this.state.value;
+		}
+
+		handleChange() {
+			const input = this.input.getValue().trim();
+
+			if (input.length > 10) {
+				return;
+			}
+
+			this.setState({
+				value: input,
+				valid: PartialDate.validate(input)
+			});
+		}
+
+		valid() {
+			return this.state.valid;
+		}
+
+		validationState() {
+			let validationClass = null;
+
+			if (this.state.value) {
+				validationClass = this.state.valid ? 'success' : 'error';
+			}
+
+			return validationClass;
+		}
+
+		render() {
+			return (
+				<Input
+					bsStyle={this.validationState()}
+					groupClassName={this.props.groupClassName}
+					help={this.props.help}
+					label={this.props.label}
+					labelClassName={this.props.labelClassName}
+					placeholder={this.props.placeholder}
+					ref={(ref) => this.input = ref}
+					type="text"
+					value={this.state.value}
+					wrapperClassName={this.props.wrapperClassName}
+					onChange={this.handleChange}
+				/>
+			);
+		}
+	}
+
+	PartialDate.displayName = 'PartialDate';
+	PartialDate.propTypes = {
 		defaultValue: React.PropTypes.string,
 		groupClassName: React.PropTypes.string,
 		help: React.PropTypes.string,
@@ -35,83 +115,7 @@ const PartialDate = React.createClass({
 		labelClassName: React.PropTypes.string,
 		placeholder: React.PropTypes.string,
 		wrapperClassName: React.PropTypes.string
-	},
-	getInitialState() {
-		'use strict';
+	};
 
-		return {
-			value: this.props.defaultValue,
-			valid: this.validate(this.props.defaultValue)
-		};
-	},
-	getValue() {
-		'use strict';
-
-		return this.state.value;
-	},
-	handleChange() {
-		'use strict';
-
-		const input = this.input.getValue().trim();
-
-		if (input.length > 10) {
-			return;
-		}
-
-		this.setState({
-			value: input,
-			valid: this.validate(input)
-		});
-	},
-	valid() {
-		'use strict';
-
-		return this.state.valid;
-	},
-	validate(value) {
-		'use strict';
-
-		if (!value) {
-			return true;
-		}
-
-		const validSyntax = Boolean(
-			ymdRegex.test(value) || ymRegex.test(value) || yRegex.test(value)
-		);
-		const validValue = !isNaN(Date.parse(value));
-		return validSyntax && validValue;
-	},
-	validationState() {
-		'use strict';
-
-		let validationClass = null;
-
-		if (this.state.value) {
-			validationClass = this.state.valid ? 'success' : 'error';
-		}
-
-		return validationClass;
-	},
-	render() {
-		'use strict';
-
-		return (
-			<Input
-				bsStyle={this.validationState()}
-				groupClassName={this.props.groupClassName}
-				help={this.props.help}
-				label={this.props.label}
-				labelClassName={this.props.labelClassName}
-				placeholder={this.props.placeholder}
-				ref={(ref) => this.input = ref}
-				type="text"
-				value={this.state.value}
-				wrapperClassName={this.props.wrapperClassName}
-				onChange={this.handleChange}
-			/>
-		);
-	}
-});
-
-
-module.exports = PartialDate;
+	module.exports = PartialDate;
+})();
--- src/client/components/pages/revision.jsx
+++ src/client/components/pages/revision.jsx
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2015-2016  Stanisław Szcześniak
- *                          Ben Ockmore
+ *               2015-2016  Ben Ockmore
+ *               2016       Sean Burke
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,154 +17,163 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
 const React = require('react');
 const _compact = require('lodash.compact');
 
 const EntityLink = require('../entity-link.jsx');
 
-function formatValueList(list) {
+(() => {
 	'use strict';
 
-	if (!list) {
-		return null;
-	}
+	class RevisionPage extends React.Component {
+		static formatValueList(list) {
+			if (!list) {
+				return null;
+			}
 
-	return list.map((val, idx) => (
-		<div key={idx}>{val.toString()}</div>
-	));
-}
-
-module.exports = React.createClass({
-	displayName: 'RevisionPage',
-	propTypes: {
-		diffs: React.PropTypes.any.isRequired,
-		revision: React.PropTypes.any.isRequired
-	},
-	formatChange(change) {
-		'use strict';
-		if (change.kind === 'N') {
-			return (
-				<tr className="success"
-					key={change.key}
-				>
-					<th scope="row">{change.key}</th>
-					<td> — </td>
-					<td>
-						{formatValueList(change.rhs)}
-					</td>
-				</tr>
-			);
+			return list.map((val, idx) => (
+				<div key={idx}>{val.toString()}</div>
+			));
 		}
 
-		if (change.kind === 'E') {
-			return (
-				<tr
-					className="warning"
-					key={change.key}
-				>
-					<th scope="row">{change.key}</th>
-					<td>
-						{formatValueList(change.lhs)}
-					</td>
-					<td>
-						{formatValueList(change.rhs)}
-					</td>
-				</tr>
-			);
+		static formatChange(change) {
+			if (change.kind === 'N') {
+				return (
+					<tr className="success"
+						key={change.key}
+					>
+						<th scope="row">{change.key}</th>
+						<td> — </td>
+						<td>
+							{RevisionPage.formatValueList(change.rhs)}
+						</td>
+					</tr>
+				);
+			}
+
+			if (change.kind === 'E') {
+				return (
+					<tr
+						className="warning"
+						key={change.key}
+					>
+						<th scope="row">{change.key}</th>
+						<td>
+							{RevisionPage.formatValueList(change.lhs)}
+						</td>
+						<td>
+							{RevisionPage.formatValueList(change.rhs)}
+						</td>
+					</tr>
+				);
+			}
+
+			if (change.kind === 'D') {
+				return (
+					<tr
+						className="danger"
+						key={change.key}
+					>
+						<th scope="row">{change.key}</th>
+						<td>
+							{RevisionPage.formatValueList(change.lhs)}
+						</td>
+						<td> — </td>
+					</tr>
+				);
+			}
+
+			return null;
 		}
 
-		if (change.kind === 'D') {
-			return (
-				<tr
-					className="danger"
-					key={change.key}
-				>
-					<th scope="row">{change.key}</th>
-					<td>
-						{formatValueList(change.lhs)}
-					</td>
-					<td> — </td>
-				</tr>
+		static formatDiff(diff) {
+			const result = diff.changes.map(
+				(change) =>
+					RevisionPage.formatChange(change)
 			);
+
+			return _compact(result);
 		}
 
-		return null;
-	},
-	formatDiff(diff) {
-		'use strict';
-		const result = diff.changes.map((change) => this.formatChange(change));
-		return _compact(result);
-	},
-	render() {
-		'use strict';
-		const revision = this.props.revision;
-		const diffs = this.props.diffs;
+		render() {
+			const revision = this.props.revision;
+			const diffs = this.props.diffs;
 
-		const diffDivs = diffs.map((diff) => (
-			<div key="{diff.entity.bbid}">
-				<h3>
-					<EntityLink
-						bbid={diff.entity.bbid}
-						text={`${diff.entity.type} ${diff.entity.bbid}`}
-						type={diff.entity.type}
-					/>
-				</h3>
-				<table className="table table-bordered text-center">
-					<tbody>
-						{this.formatDiff(diff)}
-					</tbody>
-				</table>
-			</div>
-		));
+			const diffDivs = diffs.map((diff) => (
+				<div key="{diff.entity.bbid}">
+					<h3>
+						<EntityLink
+							bbid={diff.entity.bbid}
+							text={`${diff.entity.type} ${diff.entity.bbid}`}
+							type={diff.entity.type}
+						/>
+					</h3>
+					<table className="table table-bordered text-center">
+						<tbody>
+						{RevisionPage.formatDiff(diff)}
+						</tbody>
+					</table>
+				</div>
+			));
 
-		let revisionNotes = revision.notes.map((note) => {
-			const timeCreated =
-				new Date(note.postedAt).toTimeString();
-			const dateCreated =
-				new Date(note.postedAt).toDateString();
+			let revisionNotes = revision.notes.map((note) => {
+				const timeCreated =
+					new Date(note.postedAt).toTimeString();
+				const dateCreated =
+					new Date(note.postedAt).toDateString();
 
-			return (
-				<div
-					className="panel panel-default"
-					key={note.id}
-				>
-					<div className="panel-body">
-						<p>{note.content}</p>
-						<p className="text-right">
-							—&nbsp;
-							<a href={`/editor/${note.author.id}`}>
-								{note.author.name}
-							</a>
-							, {`${timeCreated}, ${dateCreated}`}
-						</p>
+				return (
+					<div
+						className="panel panel-default"
+						key={note.id}
+					>
+						<div className="panel-body">
+							<p>{note.content}</p>
+							<p className="text-right">
+								—&nbsp;
+								<a href={`/editor/${note.author.id}`}>
+									{note.author.name}
+								</a>
+								, {`${timeCreated}, ${dateCreated}`}
+							</p>
+						</div>
 					</div>
+				);
+			});
+
+			if (revisionNotes.length === 0) {
+				revisionNotes = (<p> No revision notes present </p>);
+			}
+
+			const dateRevisionCreated =
+				new Date(revision.createdAt).toDateString();
+
+			return (
+				<div>
+					<h1>Revision #{revision.id}</h1>
+					{diffDivs}
+					<p className="text-right">
+						Created by&nbsp;
+						<a href={`/editor/${revision.author.id}`}>
+							{revision.author.name}
+						</a>
+						, {dateRevisionCreated}
+					</p>
+
+					<h3>Revision Notes</h3>
+					{revisionNotes}
+
 				</div>
 			);
-		});
-
-		if (revisionNotes.length === 0) {
-			revisionNotes = (<p> No revision notes present </p>);
 		}
-
-		const dateRevisionCreated =
-			new Date(revision.createdAt).toDateString();
-
-		return (
-			<div>
-				<h1>Revision #{revision.id}</h1>
-				{diffDivs}
-				<p className="text-right">
-					Created by&nbsp;
-					<a href={`/editor/${revision.author.id}`}>
-						{revision.author.name}
-					</a>
-					, {dateRevisionCreated}
-				</p>
-
-				<h3>Revision Notes</h3>
-				{revisionNotes}
-
-			</div>
-		);
 	}
-});
+
+	RevisionPage.displayName = 'RevisionPage';
+	RevisionPage.propTypes = {
+		diffs: React.PropTypes.any.isRequired,
+		revision: React.PropTypes.any.isRequired
+	};
+
+	module.exports = RevisionPage;
+})();
