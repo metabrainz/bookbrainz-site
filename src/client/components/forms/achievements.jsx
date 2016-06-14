aa55a5ca6ef4bc8c38137dc80b75cf4d256443bf
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-14 22:08:36 +0100

    Add display for achievements that are not unlocked

--- src/client/components/forms/achievements.jsx
+++ src/client/components/forms/achievements.jsx
@@ -21,11 +21,31 @@ module.exports = React.createClass({
 				);
 			}
 		});
+		const locked = this.props.achievement.model.map(function(achievement) {
+			if (!achievement.unlocked) {
+				return (
+					<div className="row well">
+						<div className="col-md-2">
+							<img height="100px" src={achievement.badgeUrl}></img>
+						</div>
+						<div className="col-md-8">
+							<div className="h2">
+								{achievement.name}
+							</div>
+							<p>{achievement.description}</p>
+						</div>
+					</div>
+				);
+			}
+		});
+			
 
 		return (
 			<div>
 				<div className="h1">Unlocked Achievements</div>
 				{achievements}
+				<div className="h1">Locked Achievements</div>
+				{locked}
 			</div>
 		);
 	}
