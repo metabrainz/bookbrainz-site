100f844a714e823567a653a9187de0be2f8b6752
Max Prettyjohns <heshoots9999@gmail.com>
2016-06-18 19:16:11 +0100

    Add title unlocks to achievement module

--- src/server/helpers/achievement.js
+++ src/server/helpers/achievement.js
@@ -21,6 +21,9 @@
 const AchievementType = require('bookbrainz-data').AchievementType;
 const AchievementUnlock = require('bookbrainz-data').AchievementUnlock;
 const Editor = require('bookbrainz-data').Editor;
+const TitleType = require('bookbrainz-data').TitleType;
+const TitleUnlock = require('bookbrainz-data').TitleUnlock;
+
 const Promise = require('bluebird');
 const achievement = {};
 const Bookshelf = require('bookbrainz-data').bookshelf;
@@ -47,7 +50,27 @@ function awardAchievement(editorId, achievementId) {
 	});
 }
 
-// tiers = [{threshold, name}]
+function awardTitle(editorId, titleId) {
+	const titleAttribs = {
+		editorId,
+		titleId
+	};
+	return new TitleUnlock(titleAttribs)
+		.fetch()
+		.then((unlock) => {
+			let awardPromise;
+			if (unlock === null) {
+				awardPromise = new TitleUnlock(titleAttribs)
+					.save(null, {method: 'insert'});
+			}
+			else {
+				awardPromise = Promise.resolve();
+			}
+			return awardPromise;
+		});
+}
+
+// tiers = [{threshold, name, (titleName)}] (optional)
 function testTiers(signal, editorId, tiers) {
 	const promiseList = [];
 	let achievementPromise;
@@ -55,6 +78,14 @@ function testTiers(signal, editorId, tiers) {
 	for (let i = 0; i < tiers.length; i++) {
 		if (signal > tiers[i].threshold) {
 			achievementAwarded = true;
+			if (tiers[i].titleName) {
+				promiseList.push(
+					new TitleType({title: tiers[i].titleName})
+						.fetch({require: true})
+						.then((title) =>
+							awardTitle(editorId, title.id))
+				);
+			}
 			promiseList.push(
 				new AchievementType({name: tiers[i].name})
 					.fetch({require: true})
@@ -93,9 +124,11 @@ function processRevisionist(editorId) {
 		.then((editor) => {
 			const revisions = editor.attributes.revisionsApplied;
 			const tiers = [
-				{threshold: 250, name: 'Revisionist III'},
+				{threshold: 250, name: 'Revisionist III',
+					titleName: 'Revisionist'},
 				{threshold: 50, name: 'Revisionist II'},
-				{threshold: 1, name: 'Revisionist I'}
+				{threshold: 1, name: 'Revisionist I',
+					titleName: 'Revisionist'},
 			];
 			return testTiers(revisions, editorId, tiers);
 		});
@@ -106,7 +139,8 @@ function processCreatorCreator(editorId) {
 		.then((rowCount) => {
 			let creatorPromise;
 			const tiers = [
-				{threshold: 100, name: 'Creator Creator III'},
+				{threshold: 100, name: 'Creator Creator III',
+					titleName: 'Creator Creator'},
 				{threshold: 10, name: 'Creator Creator II'},
 				{threshold: 1, name: 'Creator Creator I'}
 			];
@@ -118,10 +152,25 @@ function processLimitedEdition(editorId) {
 	getTypeRevisions('editionRevision', editorId)
 		.then((rowCount) => {
 			const tiers = [
-				{threshold: 100, name: 'Limited Edition III'},
+				{threshold: 100, name: 'Limited Edition III',
+					titleName: 'Limited Edition'},
 				{threshold: 10, name: 'Limited Edition II'},
 				{threshold: 1, name: 'Limited Edition I'}
 			];
+			return testTiers(rowCount, editorId, tiers);
+		});
+}
+
+function processPublisher(editorId) {
+	getTypeRevisions('publisherRevision', editorId)
+		.then((rowCount) => {
+			const tiers = [
+				{threshold: 100, name: 'Publisher III',
+					titleName: 'Publisher'},
+				{threshold: 10, name: 'Publisher II'},
+				{threshold: 1, name: 'Publisher I'}
+			];
+			return testTiers(rowCount, editorId, tiers);
 		});
 }
 
@@ -133,7 +182,9 @@ achievement.processPageVisit = () => {
 achievement.processEdit = (userid) =>
 	Promise.join(
 		processRevisionist(userid),
-		processCreatorCreator(userid)
+		processCreatorCreator(userid),
+		processLimitedEdition(userid),
+		processPublisher(userid)
 	);
 
 
