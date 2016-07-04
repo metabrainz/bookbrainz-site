0e728683fa6c32bde7e0b1bc24f487a1197e2630
Max Prettyjohns <heshoots9999@gmail.com>
2016-07-04 14:15:00 +0100

    Drop foreign keys in down script

--- migrations/2016-06/down.sql
+++ migrations/2016-06/down.sql
@@ -1,5 +1,20 @@
 BEGIN;
 
+ALTER TABLE bookbrainz.title_unlock
+	DROP CONSTRAINT title_unlock_editor_id_fkey;
+
+ALTER TABLE bookbrainz.title_unlock
+	DROP CONSTRAINT title_unlock_title_id_fkey;
+
+ALTER TABLE bookbrainz.achievement_unlock
+	DROP CONSTRAINT achievement_unlock_achievement_id_fkey;
+
+ALTER TABLE bookbrainz.achievement_unlock
+	DROP CONSTRAINT achievement_unlock_editor_id_fkey;
+
+ALTER TABLE bookbrainz.editor
+	DROP CONSTRAINT editor_title_unlock_id_fkey;
+
 ALTER TABLE bookbrainz.editor
 	DROP COLUMN title_unlock_id;
 
