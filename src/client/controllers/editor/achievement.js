cc85580352f4d3d6ec41c3a19a625af22b3f8bbe
Max Prettyjohns <heshoots9999@gmail.com>
2016-07-04 20:25:23 +0100

    Add controller for client side js

---
+++ src/client/controllers/editor/achievement.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015  Ben Ockmore
+ *               2015  Sean Burke
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const AchievementForm = React.createFactory(
+	require('../../components/forms/achievements.jsx')
+);
+const props = JSON.parse(document.getElementById('props').innerHTML);
+
+ReactDOM.render(AchievementForm(props), document.getElementById('achievementsForm'));
