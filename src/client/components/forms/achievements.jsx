d27d17827f92d7a3c862de7f93660d88a5fee3ad
Max Prettyjohns <heshoots9999@gmail.com>
2016-07-19 15:31:08 +0100

    Convert AchievementForm to es6 class

--- src/client/components/forms/achievements.jsx
+++ src/client/components/forms/achievements.jsx
@@ -2,134 +2,146 @@ const React = require('react');
 const request = require('superagent-bluebird-promise');
 const Achievement = require('./parts/achievement.jsx');
 
-module.exports = React.createClass({
-	displayName: 'AchievementForm',
-	propTypes: {
+(() => {
+	'use strict';
+
+	class AchievementForm extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				editor: props.editor,
+				achievement: props.achievement
+			};
+		}
+		handleSubmit(event) {
+			event.preventDefault();
+
+			const data = {
+				id: this.state.editor.id,
+				rank1: this.rank1,
+				rank2: this.rank2,
+				rank3: this.rank3
+			};
+
+			request.post('/editor/:id/achievements')
+				.send(data)
+				.then(() => {
+					window.location.href = '/editor/:id';
+				})
+				.catch((res) => {
+					const error = res.body.error;
+					this.setState({
+						error,
+						waiting: false
+					});
+				});
+		}
+		render() {
+			const achievements = this.state.achievement.model
+					.map((achievement) => {
+						let achievementHTML;
+						if (achievement.unlocked) {
+							achievementHTML = (
+								<Achievement achievement={achievement}/>
+							);
+						}
+						return achievementHTML;
+					});
+			const locked = this.state.achievement.model.map((achievement) => {
+				let achievementHTML = null;
+				if (!achievement.unlocked) {
+					achievementHTML = (
+						<Achievement achievement={achievement}/>
+					);
+				}
+				return achievementHTML;
+			});
+
+			const rankName = this.state.achievement.model.map((achievement) => {
+				let optionHTML = null;
+				if (achievement.unlocked) {
+					optionHTML = (<option value={achievement.id}>
+						{achievement.name}
+					</option>);
+				}
+				return optionHTML;
+			});
+
+			const nullOption = (<option value="none"> </option>);
+
+			const rankUpdate = (
+				<form
+					className="form-horizontal"
+					id="rankSelectForm"
+					method="post"
+				>
+					<div className="form-group">
+						<label>Rank 1</label>
+						<div className="selectContainer">
+							<select
+								className="form-control"
+								name="rank1"
+								value={this.rank1}
+							>
+								{nullOption}
+								{rankName}
+							</select>
+						</div>
+					</div>
+
+					<div className="form-group">
+						<label>Rank 2</label>
+						<div className="selectContainer">
+							<select
+								className="form-control"
+								name="rank2"
+							>
+								{nullOption}
+								{rankName}
+							</select>
+						</div>
+					</div>
+
+					<div className="form-group">
+						<label>Rank 3</label>
+						<div className="selectContainer">
+							<select
+								className="form-control"
+								name="rank3"
+							>
+								{nullOption}
+								{rankName}
+							</select>
+						</div>
+					</div>
+
+					<div className="form-group">
+						<button
+							className="btn btn-default"
+							type="submit"
+						>
+							update
+						</button>
+					</div>
+				</form>
+			);
+			return (
+				<div>
+					<div className="h1">Unlocked Achievements</div>
+					{achievements}
+					{rankUpdate}
+					<div className="h1">Locked Achievements</div>
+					{locked}
+				</div>
+			);
+		}
+	}
+
+	AchievementForm.displayName = 'AchievementForm';
+	AchievementForm.propTypes = {
 		achievement: React.PropTypes.object,
 		editor: React.PropTypes.object
-	},
-	handleSubmit(event) {
-		'use strict';
+	};
 
-		event.preventDefault();
-
-		const data = {
-			id: this.props.editor.id,
-			rank1: this.rank1,
-			rank2: this.rank2,
-			rank3: this.rank3
-		};
-
-		request.post('/editor/:id/achievements')
-			.send(data)
-			.then(() => {
-				window.location.href = '/editor/:id';
-			})
-			.catch((res) => {
-				const error = res.body.error;
-				this.setState({
-					error,
-					waiting: false
-				});
-			});
-	},
-	render() {
-		'use strict';
-		const achievements = this.props.achievement.model.map((achievement) => {
-			let achievementHTML;
-			if (achievement.unlocked) {
-				achievementHTML = (
-					<Achievement achievement={achievement}/>
-				);
-			}
-			return achievementHTML;
-		});
-		const locked = this.props.achievement.model.map((achievement) => {
-			let achievementHTML = null;
-			if (!achievement.unlocked) {
-				achievementHTML = (
-					<Achievement achievement={achievement}/>
-				);
-			}
-			return achievementHTML;
-		});
-
-		const rankName = this.props.achievement.model.map((achievement) => {
-			let optionHTML = null;
-			if (achievement.unlocked) {
-				optionHTML = (<option value={achievement.id}>
-					{achievement.name}
-				</option>);
-			}
-			return optionHTML;
-		});
-
-		const nullOption = (<option value="none"> </option>);
-
-		const rankUpdate = (
-			<form
-				className="form-horizontal"
-				id="rankSelectForm"
-				method="post"
-			>
-				<div className="form-group">
-					<label>Rank 1</label>
-					<div className="selectContainer">
-						<select
-							className="form-control"
-							name="rank1"
-							value={this.rank1}
-						>
-							{nullOption}
-							{rankName}
-						</select>
-					</div>
-				</div>
-
-				<div className="form-group">
-					<label>Rank 2</label>
-					<div className="selectContainer">
-						<select
-							className="form-control"
-							name="rank2"
-						>
-							{nullOption}
-							{rankName}
-						</select>
-					</div>
-				</div>
-
-				<div className="form-group">
-					<label>Rank 3</label>
-					<div className="selectContainer">
-						<select
-							className="form-control"
-							name="rank3"
-						>
-							{nullOption}
-							{rankName}
-						</select>
-					</div>
-				</div>
-
-				<div className="form-group">
-					<button
-						className="btn btn-default"
-						type="submit"
-					>
-						update
-					</button>
-				</div>
-			</form>
-		);
-		return (
-			<div>
-				<div className="h1">Unlocked Achievements</div>
-				{achievements}
-				{rankUpdate}
-				<div className="h1">Locked Achievements</div>
-				{locked}
-			</div>
-		);
-	}
-});
+	module.exports = AchievementForm;
+})();
