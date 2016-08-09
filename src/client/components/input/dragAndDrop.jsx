5a1ae257dcac2df39cc56d5717ad2ff8fd90da39
Max Prettyjohns <heshoots9999@gmail.com>
2016-08-09 18:13:24 +0100

    Convert drag and drop component to es6 class

--- src/client/components/input/dragAndDrop.jsx
+++ src/client/components/input/dragAndDrop.jsx
@@ -1,61 +1,63 @@
 const React = require('react');
 const Input = require('react-bootstrap').Input;
 
-module.exports = React.createClass({
-	displayName: 'DragAndDrop',
-	propTypes: {
-		name: React.PropTypes.string
-	},
-	getInitialState() {
-		'use strict';
-		return {achievement: {}};
-	},
-	handleDragOver(ev) {
-		'use strict';
-		ev.preventDefault();
-	},
-	addChild(data) {
-		'use strict';
-		this.setState({achievement: data});
-	},
-	handleDrop(ev) {
-		'use strict';
-		ev.preventDefault();
-		let data;
+(() => {
+	'use strict';
+	class DragAndDrop extends React.Component {
+		constructor() {
+			super();
+			this.handleDragOver = this.handleDragOver.bind(this);
+			this.handleDrop = this.handleDrop.bind(this);
+			this.state = {achievement: {}};
+		}
+		handleDragOver(ev) {
+			ev.preventDefault();
+		}
+		addChild(data) {
+			this.setState({achievement: data});
+		}
+		handleDrop(ev) {
+			ev.preventDefault();
+			let data;
 
-		try {
-			data = JSON.parse(ev.dataTransfer.getData('text'));
+			try {
+				data = JSON.parse(ev.dataTransfer.getData('text'));
+			}
+			catch (err) {
+				return;
+			}
+			this.addChild(data);
 		}
-		catch (err) {
-			return;
+		getValue() {
+			return this.target.getValue();
 		}
-		this.addChild(data);
-	},
-	getValue() {
-		'use strict';
-		return this.target.getValue();
-	},
-	render() {
-		'use strict';
-		return (
-			<div className="well col-sm-4"
-				onDragOver={this.handleDragOver}
-				onDrop={this.handleDrop}
-			>
-			<Input
-				name={this.props.name}
-				type="hidden"
-				value={this.state.achievement.id}
-			/>
-			<img
-				className="center-block"
-				height="100px"
-				src={this.state.achievement.src}
-			/>
-				<div className="center-block h2">
-					{this.state.achievement.name}
+		render() {
+			return (
+				<div className="well col-sm-4"
+					onDragOver={this.handleDragOver}
+					onDrop={this.handleDrop}
+				>
+				<Input
+					name={this.props.name}
+					type="hidden"
+					value={this.state.achievement.id}
+				/>
+				<img
+					className="center-block"
+					height="100px"
+					src={this.state.achievement.src}
+				/>
+					<div className="center-block h2">
+						{this.state.achievement.name}
+					</div>
 				</div>
-			</div>
-		);
+			);
+		}
 	}
-});
+	DragAndDrop.displayName = 'DragAndDrop';
+	DragAndDrop.propTypes = {
+		name: React.PropTypes.string
+	};
+
+	module.exports = DragAndDrop;
+})();
