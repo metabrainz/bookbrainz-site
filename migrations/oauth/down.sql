ed37f37b6da37de107234fe7687f9832e1d16070
Ben Ockmore <ben.sput@gmail.com>
2016-09-05 15:06:02 +0100

    Merged WIP migrations into single set of migration files for next release

--- migrations/oauth/down.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE bookbrainz.editor
-	DROP CONSTRAINT password_or_metabrainz_account_check;
-ALTER TABLE bookbrainz.editor
-	DROP COLUMN metabrainz_user_id;
-ALTER TABLE bookbrainz.editor
-	DROP COLUMN cached_metabrainz_name;
-ALTER TABLE bookbrainz.editor
-	ADD COLUMN email VARCHAR(255) CHECK (email <> '');
