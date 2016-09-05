dc9d5fd988ca0a21d1a782d581f7b93830e1a591
Ben Ockmore <ben.sput@gmail.com>
2016-09-05 15:08:16 +0100

    Cleaned up formatting for next/up.sql migration

--- migrations/next/up.sql
+++ migrations/next/up.sql
@@ -19,19 +19,22 @@ ALTER TABLE bookbrainz.editor
 
 -- Add core achievement system
 CREATE TABLE bookbrainz.title_type (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(40) NOT NULL CHECK (title <> ''),
-    description TEXT NOT NULL CHECK (description <> '')
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(40) NOT NULL CHECK (title <> ''),
+	description TEXT NOT NULL CHECK (description <> '')
 );
 
 CREATE TABLE bookbrainz.title_unlock (
-    id SERIAL PRIMARY KEY,
-    editor_id INT NOT NULL,
-    title_id INT NOT NULL,
-    unlocked_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now())
+	id SERIAL PRIMARY KEY,
+	editor_id INT NOT NULL,
+	title_id INT NOT NULL,
+	unlocked_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+		DEFAULT timezone('UTC'::TEXT, now())
 );
-ALTER TABLE bookbrainz.title_unlock ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
-ALTER TABLE bookbrainz.title_unlock ADD FOREIGN KEY (title_id) REFERENCES bookbrainz.title_type (id);
+ALTER TABLE bookbrainz.title_unlock
+	ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+ALTER TABLE bookbrainz.title_unlock
+	ADD FOREIGN KEY (title_id) REFERENCES bookbrainz.title_type (id);
 
 CREATE TABLE bookbrainz.achievement_type (
     id SERIAL PRIMARY KEY,
@@ -40,19 +43,23 @@ CREATE TABLE bookbrainz.achievement_type (
     badge_url VARCHAR(2000)
 );
 CREATE TABLE bookbrainz.achievement_unlock (
-    id SERIAL PRIMARY KEY,
-    editor_id INT NOT NULL,
-    achievement_id INT NOT NULL,
-    unlocked_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now()),
+	id SERIAL PRIMARY KEY,
+	editor_id INT NOT NULL,
+	achievement_id INT NOT NULL,
+	unlocked_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+		DEFAULT timezone('UTC'::TEXT, now()),
 	profile_rank SMALLINT DEFAULT NULL
 );
-ALTER TABLE bookbrainz.achievement_unlock ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
-ALTER TABLE bookbrainz.achievement_unlock ADD FOREIGN KEY (achievement_id) REFERENCES bookbrainz.achievement_type (id);
+ALTER TABLE bookbrainz.achievement_unlock
+	ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+ALTER TABLE bookbrainz.achievement_unlock
+	ADD FOREIGN KEY (achievement_id) REFERENCES bookbrainz.achievement_type (id);
 
 ALTER TABLE bookbrainz.editor
 	ADD COLUMN title_unlock_id INT DEFAULT NULL;
 
-ALTER TABLE bookbrainz.editor ADD FOREIGN KEY (title_unlock_id) REFERENCES bookbrainz.title_unlock (id);
+ALTER TABLE bookbrainz.editor
+	ADD FOREIGN KEY (title_unlock_id) REFERENCES bookbrainz.title_unlock (id);
 
 
 -- Explorer achievement
@@ -63,8 +70,10 @@ CREATE TABLE bookbrainz._editor_entity_visits (
 	UNIQUE (editor_id, bbid)
 );
 
-ALTER TABLE bookbrainz._editor_entity_visits ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
-ALTER TABLE bookbrainz._editor_entity_visits ADD FOREIGN KEY (bbid) REFERENCES bookbrainz.entity (bbid);
+ALTER TABLE bookbrainz._editor_entity_visits
+	ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+ALTER TABLE bookbrainz._editor_entity_visits
+	ADD FOREIGN KEY (bbid) REFERENCES bookbrainz.entity (bbid);
 
 
 -- Remove passwords
