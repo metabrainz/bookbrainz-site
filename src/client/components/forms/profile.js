c67a186705c1d36722c43f4935b4c735b3e7558c
Daniel Hsing <hsing.daniel@gmail.com>
2016-12-31 13:07:16 +0800

    Removed extraneous todo comment
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- src/client/components/forms/profile.js
+++ src/client/components/forms/profile.js
@@ -16,14 +16,6 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
-
-/**
- * TODO:
- * - Select: Gender
- * - Input: Birth Date
- * - Input: Display name
- */
 const React = require('react');
 const request = require('superagent-bluebird-promise');
 
