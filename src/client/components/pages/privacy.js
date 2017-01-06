8b3025d9e84472a4f42a5809293cb0d9e5030016
Srijan Jha <srijanjha09@gmail.com>
2017-01-06 17:07:08 +0530

    Update privacy.js

--- src/client/components/pages/privacy.js
+++ src/client/components/pages/privacy.js
@@ -17,11 +17,13 @@
  */
 
 const React = require('react');
+
 const PageHeader = require('react-bootstrap').PageHeader;
 
 /**
-* Renders a page containing a link to the current MusicBrainz privacy policy, 
-* which will form the basis of a future privacy policy for BookBrainz.
+* Renders a page containing a link to the current MusicBrainz privacy 
+* policy , which will form the basis of a future privacy policy 
+* for BookBrainz.
 * @returns {ReactElement} an element containing the rendered output
 */
 function PrivacyPage() {
