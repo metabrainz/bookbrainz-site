df74dc7acf47eced8531587222ffbe5dd8cb3852
Arthelon <hsing.daniel@gmail.com>
2017-01-09 20:24:38 +0800

    Updated edition component license header to include other authors
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/client/components/pages/entity/edition.js
+++ src/client/components/pages/entity/edition.js
@@ -1,5 +1,7 @@
 /*
  * Copyright (C) 2016  Daniel Hsing
+ * 				 2016  Sean Burke
+ * 				 2016  Ben Ockmore
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
