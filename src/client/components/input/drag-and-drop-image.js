67a5daccac0ff1488701d620f758ee9a39385d43
Divya Prakash Mittal <divyaprakashmittal@gmail.com>
2017-01-11 05:23:12 +0530

    Add JSdoc comments to Input Component files (#132)
    Add documentation to partial-date.js and drag-and-drop-image.js

--- src/client/components/input/drag-and-drop-image.js
+++ src/client/components/input/drag-and-drop-image.js
@@ -1,11 +1,27 @@
 const React = require('react');
 const Input = require('react-bootstrap').Input;
 
+/**
+* This class is derived from the React Component base class and renders
+* an image which supports drag and drop functionality.
+*/
 class DragAndDropImage extends React.Component {
+	/**
+	* Binds the class methods to their respective data.
+	* @constructor
+	*/
 	constructor() {
 		super();
 		this.handleDragStart = this.handleDragStart.bind(this);
 	}
+	/**
+	* Transfers the data of the achievement badge component properties to the
+	* DragAndDrop event, which in turn transfers the data on handleDrop to that
+	* of the achievement badge which will be showcased on editor's
+	* public profile.
+	* @param {object} ev - Passed in the function to be initialized with data
+	* onDragStart.
+	*/
 	handleDragStart(ev) {
 		const data = {
 			id: this.props.achievementId,
@@ -14,6 +30,11 @@ class DragAndDropImage extends React.Component {
 		};
 		ev.dataTransfer.setData('text', JSON.stringify(data));
 	}
+	/**
+	* Renders an image of a particular achievement badge, which can be dragged
+	* to set the user's publicly showcased achievements
+	* @returns {ReactElement} - The rendered image element.
+	*/
 	render() {
 		return (
 			<img
