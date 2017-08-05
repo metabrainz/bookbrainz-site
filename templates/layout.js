d5f6e9490346811857975c0e91ef0a1165ed283d
Ben Ockmore <ben.sput@gmail.com>
2017-08-05 19:38:06 +0100

    chore(templates): remove unused Jade/Pug templates

--- templates/layout.js
+++
@@ -1,5 +0,0 @@
-/* eslint import/no-unassigned-import: 0, import/no-commonjs: 0 */
-/* eslint import/newline-after-import: 0 */
-import $ from 'jquery';
-window.$ = window.jQuery = $;
-require('bootstrap');
