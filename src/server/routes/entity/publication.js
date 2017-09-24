537bfbe3f337d101d82ffa4ff9902c8aa9e7cbbe
Ben Ockmore <ben.sput@gmail.com>
2017-09-24 01:24:36 +0100

    feat(entity-editor): switch Publication routes to use new entity editor

--- src/server/routes/entity/publication.js
+++ src/server/routes/entity/publication.js
@@ -18,17 +18,23 @@
  */
 
 import * as auth from '../../helpers/auth';
+import * as entityEditorHelpers from '../../../client/entity-editor/helpers';
 import * as entityRoutes from './entity';
 import * as middleware from '../../helpers/middleware';
 import * as propHelpers from '../../helpers/props';
 import * as utils from '../../helpers/utils';
-import EditForm from '../../../client/components/forms/publication';
+import EntityEditor from '../../../client/entity-editor/entity-editor';
+import Immutable from 'immutable';
 import Layout from '../../../client/containers/layout';
+import {Provider} from 'react-redux';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import _ from 'lodash';
+import {createStore} from 'redux';
 import express from 'express';
 
+
+const {createRootReducer, getEntitySection} = entityEditorHelpers;
 
 const router = express.Router();
 
@@ -85,71 +91,203 @@ router.get('/:bbid/revisions', (req, res, next) => {
 
 router.get('/create', auth.isAuthenticated, middleware.loadIdentifierTypes,
 	middleware.loadLanguages, middleware.loadPublicationTypes, (req, res) => {
-		const props = {
+		const props = propHelpers.generateProps(req, res, {
+			entityType: 'publication',
 			heading: 'Create Publication',
 			identifierTypes: res.locals.identifierTypes,
-			languages: res.locals.languages,
+			initialState: {},
+			languageOptions: res.locals.languages,
 			publicationTypes: res.locals.publicationTypes,
 			requiresJS: true,
 			subheading: 'Add a new Publication to BookBrainz',
 			submissionUrl: '/publication/create/handler'
-		};
+		});
+
+		const {initialState, ...rest} = props;
+
+		const rootReducer = createRootReducer(props.entityType);
+
+		const store = createStore(
+			rootReducer,
+			Immutable.fromJS(initialState)
+		);
+
+		const EntitySection = getEntitySection(props.entityType);
 
 		const markup = ReactDOMServer.renderToString(
-			<Layout {...propHelpers.extractLayoutProps(props)}>
-				<EditForm {...propHelpers.extractChildProps(props)}/>
+			<Layout {...propHelpers.extractLayoutProps(rest)}>
+				<Provider store={store}>
+					<EntityEditor {...propHelpers.extractChildProps(rest)}>
+						<EntitySection/>
+					</EntityEditor>
+				</Provider>
 			</Layout>
 		);
 
-		res.render('target', {
+		props.initialState = store.getState();
+
+		return res.render('target', {
 			markup,
 			props,
-			script: '/js/entity/publication.js',
-			title: 'Add Publication'
+			script: '/js/entity-editor.js',
+			title: 'Add Publisher'
 		});
 	}
 );
+
+
+function getDefaultAliasIndex(aliases) {
+	const index = aliases.findIndex((alias) => alias.default);
+	return index > 0 ? index : 0;
+}
+
+function publicationToFormState(publication) {
+	const aliases = publication.aliasSet ?
+		publication.aliasSet.aliases.map(({language, ...rest}) => ({
+			language: language.id,
+			...rest
+		})) : [];
+
+	const defaultAliasIndex = getDefaultAliasIndex(aliases);
+	const defaultAliasList = aliases.splice(defaultAliasIndex, 1);
+
+	const aliasEditor = {};
+	aliases.forEach((alias) => { aliasEditor[alias.id] = alias; });
+
+	const buttonBar = {
+		aliasEditorVisible: false,
+		disambiguationVisible: Boolean(publication.disambiguation),
+		identifierEditorVisible: false
+	};
+
+	const nameSection = _.isEmpty(defaultAliasList) ? {
+		language: null,
+		name: '',
+		sortName: ''
+	} : defaultAliasList[0];
+	nameSection.disambiguation =
+		publication.disambiguation && publication.disambiguation.comment;
+
+	const identifiers = publication.identifierSet ?
+		publication.identifierSet.identifiers.map(({type, ...rest}) => ({
+			type: type.id,
+			...rest
+		})) : [];
+
+	const identifierEditor = {};
+	identifiers.forEach(
+		(identifier) => { identifierEditor[identifier.id] = identifier; }
+	);
+
+	const publicationSection = {
+		type: publication.publicationType && {
+			label: publication.publicationType.label,
+			value: publication.publicationType.id
+		}
+	};
+
+	return {
+		aliasEditor,
+		buttonBar,
+		identifierEditor,
+		nameSection,
+		publicationSection
+	};
+}
 
 router.get('/:bbid/edit', auth.isAuthenticated, middleware.loadIdentifierTypes,
 	middleware.loadPublicationTypes, middleware.loadLanguages, (req, res) => {
 		const publication = res.locals.entity;
 
-		const props = {
+		const props = propHelpers.generateProps(req, res, {
+			entityType: 'publication',
 			heading: 'Edit Publication',
 			identifierTypes: res.locals.identifierTypes,
-			languages: res.locals.languages,
-			publication,
+			initialState: publicationToFormState(publication),
+			languageOptions: res.locals.languages,
 			publicationTypes: res.locals.publicationTypes,
 			requiresJS: true,
 			subheading: 'Edit an existing Publication in BookBrainz',
 			submissionUrl: `/publication/${publication.bbid}/edit/handler`
-		};
+		});
+
+		const {initialState, ...rest} = props;
+
+		const rootReducer = createRootReducer(props.entityType);
+
+		const store = createStore(
+			rootReducer,
+			Immutable.fromJS(initialState)
+		);
+
+		const EntitySection = getEntitySection(props.entityType);
 
 		const markup = ReactDOMServer.renderToString(
-			<Layout {...propHelpers.extractLayoutProps(props)}>
-				<EditForm {...propHelpers.extractChildProps(props)}/>
+			<Layout {...propHelpers.extractLayoutProps(rest)}>
+				<Provider store={store}>
+					<EntityEditor {...propHelpers.extractChildProps(rest)}>
+						<EntitySection/>
+					</EntityEditor>
+				</Provider>
 			</Layout>
 		);
 
-		res.render('target', {
+		props.initialState = store.getState();
+
+		return res.render('target', {
 			markup,
 			props,
-			script: '/js/entity/publication.js',
+			script: '/js/entity-editor.js',
 			title: 'Edit Publication'
 		});
 	}
 );
 
-router.post('/create/handler', auth.isAuthenticatedForHandler, (req, res) =>
-	entityRoutes.createEntity(
-		req, res, 'Publication', _.pick(req.body, 'typeId')
-	)
-);
+function transformNewForm(data) {
+	let aliases = _.map(data.aliasEditor, ({language, name, sortName}) => ({
+		default: false,
+		languageId: language,
+		name,
+		sortName
+	}));
 
-router.post('/:bbid/edit/handler', auth.isAuthenticatedForHandler, (req, res) =>
-	entityRoutes.editEntity(
+	aliases = [{
+		default: true,
+		languageId: data.nameSection.language,
+		name: data.nameSection.name,
+		primary: true,
+		sortName: data.nameSection.sortName
+	}, ...aliases];
+
+	const identifiers = _.map(data.identifierEditor, ({type, ...rest}) => ({
+		typeId: type,
+		...rest
+	}));
+
+	return {
+		aliases,
+		disambiguation: data.publicationSection.disambiguation,
+		identifiers,
+		note: data.submissionSection.note,
+		typeId: data.publicationSection.type &&
+			data.publicationSection.type.value
+	};
+}
+
+router.post('/create/handler', auth.isAuthenticatedForHandler, (req, res) => {
+	req.body = transformNewForm(req.body);
+	return entityRoutes.createEntity(
 		req, res, 'Publication', _.pick(req.body, 'typeId')
-	)
+	);
+});
+
+router.post('/:bbid/edit/handler', auth.isAuthenticatedForHandler,
+	(req, res) => {
+		req.body = transformNewForm(req.body);
+		return entityRoutes.editEntity(
+			req, res, 'Publication', _.pick(req.body, 'typeId')
+		);
+	}
 );
 
 export default router;
