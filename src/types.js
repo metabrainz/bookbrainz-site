20244afdebb63abe0138175a374239eb5c0046e8
Ben Ockmore <ben.sput@gmail.com>
2017-11-11 12:16:24 +0000

    feat: add type.js file for storing custom Flow types

---
+++ src/types.js
@@ -0,0 +1,23 @@
+// @flow
+
+export type _Creator = {
+
+};
+
+export type _CreatorType = {
+
+};
+
+export type _Gender = {
+
+};
+
+export type _IdentifierType = {
+	id: number,
+	label: string,
+	validationRegex: string
+};
+
+export type _Language = {
+
+};
