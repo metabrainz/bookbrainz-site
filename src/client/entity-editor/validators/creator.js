a2ff3f989ff19c013913fa3af5aaff10086b7bb7
Ben Ockmore <ben.sput@gmail.com>
2017-11-22 22:26:26 +0000

    test(entity-editor): add CreatorSection validator tests and dummy functions

---
+++ src/client/entity-editor/validators/creator.js
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+// @flow
+
+import type {_IdentifierType} from '../../../types';
+
+
+export function validateCreatorSectionBeginArea(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSectionBeginDate(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSectionEndArea(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSectionEndDate(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSectionEnded(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSectionType(value: any): boolean {
+	return true;
+}
+
+export function validateCreatorSection(data: any): boolean {
+	return true;
+}
+
+export function validateForm(
+	formData: any, identifierTypes?: ?Array<_IdentifierType>
+): boolean {
+	return true;
+}
---
+++ test/src/client/entity-editor/validators/test-creator.js
@@ -0,0 +1,293 @@
+/*
+ * Copyright (C) 2017  Ben Ockmore
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import * as Immutable from 'immutable';
+import {
+	IDENTIFIER_TYPES, INVALID_ALIASES, INVALID_IDENTIFIERS,
+	INVALID_NAME_SECTION, INVALID_SUBMISSION_SECTION, VALID_ALIASES,
+	VALID_IDENTIFIERS, VALID_NAME_SECTION, VALID_SUBMISSION_SECTION
+} from './data';
+import {
+	testValidateAreaFunc, testValidateBooleanFunc, testValidateDateFunc,
+	testValidatePositiveIntegerFunc
+} from './helpers';
+import {
+	validateCreatorSection, validateCreatorSectionBeginArea,
+	validateCreatorSectionBeginDate, validateCreatorSectionEndArea,
+	validateCreatorSectionEndDate, validateCreatorSectionEnded,
+	validateCreatorSectionType, validateForm
+} from '../../../../../lib/client/entity-editor/validators/creator';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+
+chai.use(chaiAsPromised);
+const {expect} = chai;
+
+
+function describeValidateCreatorSectionBeginArea() {
+	testValidateAreaFunc(validateCreatorSectionBeginArea, false);
+}
+
+function describeValidateCreatorSectionEndArea() {
+	testValidateAreaFunc(validateCreatorSectionEndArea, false);
+}
+
+function describeValidateCreatorSectionBeginDate() {
+	testValidateDateFunc(validateCreatorSectionBeginDate, false);
+}
+
+function describeValidateCreatorSectionEndDate() {
+	testValidateDateFunc(validateCreatorSectionEndDate, false);
+}
+
+function describeValidateCreatorSectionEnded() {
+	testValidateBooleanFunc(validateCreatorSectionEnded);
+}
+
+function describeValidateCreatorSectionType() {
+	testValidatePositiveIntegerFunc(validateCreatorSectionType, false);
+}
+
+const VALID_CREATOR_SECTION = {
+	beginArea: null,
+	beginDate: null,
+	endArea: null,
+	endDate: null,
+	ended: true,
+	type: 1
+};
+const INVALID_CREATOR_SECTION = {...VALID_CREATOR_SECTION, type: {}};
+
+function describeValidateCreatorSection() {
+	it('should pass a valid Object', () => {
+		const result = validateCreatorSection(VALID_CREATOR_SECTION);
+		expect(result).to.be.true;
+	});
+
+	it('should pass a valid Immutable.Map', () => {
+		const result = validateCreatorSection(
+			Immutable.fromJS(INVALID_CREATOR_SECTION)
+		);
+		expect(result).to.be.true;
+	});
+
+	it('should reject an Object with an invalid area', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			beginArea: {id: null}
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid begin date', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			beginDate: '201'
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid area', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			endArea: {id: null}
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid end date', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			endDate: '201'
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid ended flag', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			ended: null
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid type', () => {
+		const result = validateCreatorSection({
+			...VALID_CREATOR_SECTION,
+			type: {}
+		});
+		expect(result).to.be.false;
+	});
+
+	it('should reject an invalid Immutable.Map', () => {
+		const result = validateCreatorSection(
+			Immutable.fromJS(INVALID_CREATOR_SECTION)
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should pass any other non-null data type', () => {
+		const result = validateCreatorSection(1);
+		expect(result).to.be.true;
+	});
+
+	it('should pass a null value', () => {
+		const result = validateCreatorSection(null);
+		expect(result).to.be.true;
+	});
+}
+
+
+function describeValidateForm() {
+	const validForm = {
+		aliasEditor: VALID_ALIASES,
+		identifierEditor: VALID_IDENTIFIERS,
+		nameSection: VALID_NAME_SECTION,
+		creatorSection: VALID_CREATOR_SECTION,
+		submissionSection: VALID_SUBMISSION_SECTION
+	};
+
+	it('should pass a valid Object', () => {
+		const result = validateForm(validForm, IDENTIFIER_TYPES);
+		expect(result).to.be.true;
+	});
+
+	it('should pass a valid Immutable.Map', () => {
+		const result = validateForm(
+			Immutable.fromJS(validForm),
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.true;
+	});
+
+	it('should reject an Object with an invalid alias editor', () => {
+		const result = validateForm(
+			{
+				...validForm,
+				aliasEditor: INVALID_ALIASES
+			},
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid identifier editor', () => {
+		const result = validateForm(
+			{
+				...validForm,
+				identifierEditor: INVALID_IDENTIFIERS
+			}, IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid name section', () => {
+		const result = validateForm(
+			{
+				...validForm,
+				nameSection: INVALID_NAME_SECTION
+			},
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid publisher section', () => {
+		const result = validateForm(
+			{
+				...validForm,
+				creatorSection: INVALID_CREATOR_SECTION
+			},
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should reject an Object with an invalid submission section', () => {
+		const result = validateForm(
+			{
+				...validForm,
+				submissionSection: INVALID_SUBMISSION_SECTION
+			},
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	const invalidForm = {
+		...validForm,
+		submissionSection: INVALID_SUBMISSION_SECTION
+	};
+
+	it('should reject an invalid Immutable.Map', () => {
+		const result = validateForm(
+			Immutable.fromJS(invalidForm),
+			IDENTIFIER_TYPES
+		);
+		expect(result).to.be.false;
+	});
+
+	it('should reject any other non-null data type', () => {
+		const result = validateForm(1, IDENTIFIER_TYPES);
+		expect(result).to.be.false;
+	});
+
+	it('should reject a null value', () => {
+		const result = validateForm(null, IDENTIFIER_TYPES);
+		expect(result).to.be.false;
+	});
+}
+
+
+function tests() {
+	describe(
+		'validateCreatorSectionBeginArea',
+		describeValidateCreatorSectionBeginArea
+	);
+	describe(
+		'validateCreatorSectionBeginDate',
+		describeValidateCreatorSectionBeginDate
+	);
+	describe(
+		'validateCreatorSectionEndArea',
+		describeValidateCreatorSectionEndArea
+	);
+	describe(
+		'validateCreatorSectionEndDate',
+		describeValidateCreatorSectionEndDate
+	);
+	describe(
+		'validateCreatorSectionEnded',
+		describeValidateCreatorSectionEnded
+	);
+	describe(
+		'validateCreatorSectionType',
+		describeValidateCreatorSectionType
+	);
+	describe(
+		'validateCreatorSection',
+		describeValidateCreatorSection
+	);
+	describe(
+		'validateForm',
+		describeValidateForm
+	);
+}
+
+describe('validatePublisherSection* functions', tests);
