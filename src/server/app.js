ea4e9487ce7b5a383b83bf054a8ad62ab79d367d
Falafel72 <ksh717@student.cis.edu.hk>
2017-12-01 17:05:26 +0800

    fix: imports in alphabetical order

--- src/server/app.js
+++ src/server/app.js
@@ -25,6 +25,7 @@ import BookBrainzData from 'bookbrainz-data';
 import Debug from 'debug';
 import Promise from 'bluebird';
 import bodyParser from 'body-parser';
+import compression from 'compression';
 import config from './helpers/config';
 import express from 'express';
 import favicon from 'serve-favicon';
@@ -36,7 +37,6 @@ import redis from 'connect-redis';
 import routes from './routes';
 import session from 'express-session';
 import staticCache from 'express-static-cache';
-import compression from 'compression';
 
 Promise.config({
 	longStackTraces: true,
