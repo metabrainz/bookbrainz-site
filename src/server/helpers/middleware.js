e3d567dc89ab7db0fe84bf69666a377ee8640e24
agbilotia1998 <iit2016508@iiita.ac.in>
2017-12-09 16:48:08 +0530

    Fix:Adds return statement for promise functions
    Added return null to remove warning on promise functions
    which doesnot return anything.

--- src/server/helpers/middleware.js
+++ src/server/helpers/middleware.js
@@ -34,6 +34,7 @@ function makeLoader(modelName, propName, sortFunc) {
 					sortFunc ? resultsSerial.sort(sortFunc) : resultsSerial;
 
 				next();
+				return null;
 			})
 			.catch(next);
 	};
@@ -146,6 +147,7 @@ export function makeEntityLoader(modelName, additionalRels, errMessage) {
 					res.locals.entity = entity.toJSON();
 
 					next();
+					return null;
 				})
 				.catch(model.NotFoundError, () => {
 					throw new error.NotFoundError(errMessage, req);
