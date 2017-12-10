67b6a75dfc53715b4b97f03ed468832383c285a4
Ben Ockmore <ben.sput@gmail.com>
2017-12-10 09:33:46 +0000

    chore: convert other .eslintrc.* files to .eslintrc.js

---
+++ src/client/.eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/unambiguous, import/no-commonjs, no-magic-numbers */
+/* eslint-disable no-inline-comments */
+
+
+module.exports = {
+	env: {
+		browser: true
+	},
+	rules: {
+		'no-return-assign': 0
+	}
+};
---
+++ test/.eslintrc.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/unambiguous, import/no-commonjs, no-magic-numbers */
+/* eslint-disable no-inline-comments */
+
+
+module.exports = {
+	env: {
+		mocha: true
+	}
+};
