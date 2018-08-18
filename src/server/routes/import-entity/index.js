e450dec463fd50c64eaf252e37d03e928da2e8e2
Shivam Tripathi <shivamtripathi0108@gmail.com>
2018-08-18 23:23:06 +0530

    feat(server/routes/import-entity): Add entity routes

--- src/server/routes/import-entity/index.js
+++ src/server/routes/import-entity/index.js
@@ -16,10 +16,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-// import ImportCreatorRouter from './creator';
-// import ImportEditionRouter from './edition';
-// import ImportPublicationRouter from './publication';
-// import ImportPublisherRouter from './publisher';
+import ImportCreatorRouter from './creator';
+import ImportEditionRouter from './edition';
+import ImportPublicationRouter from './publication';
+import ImportPublisherRouter from './publisher';
 import ImportRecentRouter from './recent';
 import ImportWorkRouter from './work';
 import express from 'express';
@@ -27,10 +27,10 @@ import express from 'express';
 
 const importRouter = express.Router();
 
-// importRouter.use('/creator', ImportCreatorRouter);
-// importRouter.use('/edition', ImportEditionRouter);
-// importRouter.use('/publisher', ImportPublisherRouter);
-// importRouter.use('/publication', ImportPublicationRouter);
+importRouter.use('/creator', ImportCreatorRouter);
+importRouter.use('/edition', ImportEditionRouter);
+importRouter.use('/publisher', ImportPublisherRouter);
+importRouter.use('/publication', ImportPublicationRouter);
 importRouter.use('/work', ImportWorkRouter);
 importRouter.use('/recent', ImportRecentRouter);
 
