metabrainz/bookbrainz-site

commit 57deb63e03d07b1252931780e148267e99bb9669

refactor(entity-editor): use new Entity component in EntitySearch

src/client/entity-editor/common/entity-search-field.js
@@ -20,10 +20,11 @@
 // @flow
 
 import CustomInput from '../../input';
-import Icon from 'react-fontawesome';
+import Entity from './entity';
 import React from 'react';
 import {Async as SelectAsync} from 'react-select';
 import ValidationLabel from '../common/validation-label';
+import _ from 'lodash';
 import makeImmutable from './make-immutable';
 import request from 'superagent-bluebird-promise';
 
@@ -59,46 +60,13 @@ function entityToOption(entity) {
 	const id = isArea(entity) ? entity.id : entity.bbid;
 
 	return {
-		disambiguation: entity.disambiguation ?
-			entity.disambiguation.comment : null,
+		disambiguation: _.get(entity, ['disambiguation', 'comment']),
 		id,
-		text: entity.defaultAlias ?
-			entity.defaultAlias.name : '(unnamed)',
-		type: entity.type
+		text: _.get(entity, ['defaultAlias', 'name']),
+		type: entity.type,
+		value: id
 	};
 }
-
-type OptionProps = {
-	disambiguation: string,
-	text: string,
-	type: string
-};
-
-function Option({disambiguation, text, type}: OptionProps) {
-	const ENTITY_TYPE_ICONS = {
-		Area: 'globe',
-		Creator: 'user',
-		Edition: 'book',
-		Publication: 'th-list',
-		Publisher: 'university',
-		Work: 'file-text-o'
-	};
-
-	return (
-		<div>
-			{type && <Icon name={ENTITY_TYPE_ICONS[type]}/>}
-			{' '}
-			{text}
-			{
-				disambiguation &&
-				<span className="disambig">
-					({disambiguation})
-				</span>
-			}
-		</div>
-	);
-}
-Option.displayName = 'Option';
 
 type EntitySearchFieldProps = {
 	label: string,
@@ -148,10 +116,9 @@ function EntitySearchField(
 	return (
 		<CustomInput label={labelElement}>
 			<ImmutableAsyncSelect
-				labelKey="text"
 				loadOptions={fetchOptions}
-				optionRenderer={Option}
-				valueKey="id"
+				optionRenderer={Entity}
+				valueRenderer={Entity}
 				{...rest}
 			/>
 		</CustomInput>

commit 8d1da29f11f75d98245ceb7e891f2fb1d148286c

fix(EntitySearchField): fix issue with filtering introduced in 57deb63

src/client/entity-editor/common/entity-search-field.js
@@ -116,6 +116,7 @@ function EntitySearchField(
 	return (
 		<CustomInput label={labelElement}>
 			<ImmutableAsyncSelect
+				labelKey="text"
 				loadOptions={fetchOptions}
 				optionRenderer={Entity}
 				valueRenderer={Entity}