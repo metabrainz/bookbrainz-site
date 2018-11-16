ce015f50066440c2ac19e2be586280bf1238366e
Monkey Do <contact@monkeydo.digital>
2018-11-16 12:39:43 +0100

    fix(search): Small layout corrections on search page

--- src/client/components/pages/search.js
+++ src/client/components/pages/search.js
@@ -116,43 +116,44 @@ class SearchPage extends React.Component {
 				/>
 				<SearchResults results={this.state.results}/>
 				{
-					this.state.results && this.state.results.length &&
-					<div>
-						<hr className="thin"/>
-						<Pager>
-							<Pager.Item
-								previous disabled={this.state.from <= 0}
-								href="#" onClick={this.handleClickPrevious}
-							>
-								&larr; Previous Page
-							</Pager.Item>
-							<ButtonGroup>
-								<Button disabled link>Results {this.state.from}—
-									{this.state.results.length < this.state.size ?
-										this.state.results.length :
-										this.state.from + this.state.size
-									}
-								</Button>
-								<DropdownButton
-									dropup bsStyle="info" id="bg-nested-dropdown"
-									title={`${this.state.size} per page`}
-									onSelect={this.handleResultsPerPageChange}
+					this.state.results && this.state.results.length ?
+						<div>
+							<hr className="thin"/>
+							<Pager>
+								<Pager.Item
+									previous disabled={this.state.from <= 0}
+									href="#" onClick={this.handleClickPrevious}
 								>
-									<MenuItem eventKey="10">10 per page</MenuItem>
-									<MenuItem eventKey="20">20 per page</MenuItem>
-									<MenuItem eventKey="35">35 per page</MenuItem>
-									<MenuItem eventKey="50">50 per page</MenuItem>
-									<MenuItem eventKey="100">100 per page</MenuItem>
-								</DropdownButton>
-							</ButtonGroup>
-							<Pager.Item
-								next disabled={!this.state.nextEnabled}
-								href="#" onClick={this.handleClickNext}
-							>
-								Next Page &rarr;
-							</Pager.Item>
-						</Pager>
-					</div>
+									&larr; Previous Page
+								</Pager.Item>
+								<ButtonGroup>
+									<Button disabled>Results {this.state.from}—
+										{this.state.results.length < this.state.size ?
+											this.state.results.length :
+											this.state.from + this.state.size
+										}
+									</Button>
+									<DropdownButton
+										dropup bsStyle="info" id="bg-nested-dropdown"
+										title={`${this.state.size} per page`}
+										onSelect={this.handleResultsPerPageChange}
+									>
+										<MenuItem eventKey="10">10 per page</MenuItem>
+										<MenuItem eventKey="20">20 per page</MenuItem>
+										<MenuItem eventKey="35">35 per page</MenuItem>
+										<MenuItem eventKey="50">50 per page</MenuItem>
+										<MenuItem eventKey="100">100 per page</MenuItem>
+									</DropdownButton>
+								</ButtonGroup>
+								<Pager.Item
+									next disabled={!this.state.nextEnabled}
+									href="#" onClick={this.handleClickNext}
+								>
+									Next Page &rarr;
+								</Pager.Item>
+							</Pager>
+						</div> :
+						null
 				}
 			</div>
 		);
