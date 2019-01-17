c08796368ab4fd7d3d652ce59dec9720d2129498
Akhilesh Kumar <akhileshnithcse@gmail.com>
2019-01-17 18:53:27 +0530

    refactor: remove self-invoking functions and convert to async await.

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -41,18 +41,17 @@ router.get('/cb', (req, res, next) => {
 			return res.redirect('/register/details');
 		}
 
-		return req.logIn(user, (loginErr) => {
+		return req.logIn(user, async (loginErr) => {
 			if (loginErr) {
 				return next(loginErr);
 			}
 
 			const {Editor} = req.app.locals.orm;
+			//  lastLoginDate is current login date with time in ISO foramt
 			const lastLoginDate = new Date().toISOString();
-			(() => {
-			  Editor
-			    .where({id: req.user.id})
-			    .save({activeAt: lastLoginDate}, {patch: true});
-			})();
+			//  Query for update activeAt with current login timestamp
+			await Editor.where({id: req.user.id}).save({activeAt: lastLoginDate}, {patch: true});
+
 			const redirectTo =
 				req.session.redirectTo ? req.session.redirectTo : '/';
 			req.session.redirectTo = null;
