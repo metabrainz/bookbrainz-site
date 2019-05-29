d1b5a4c25534d9eb0b4c145566ad05289088e0a1
Akhilesh Kumar <akhileshnith@gmail.com>
2019-05-29 21:20:21 +0530

    refactor: fetch work data from database

--- test/src/api/endpoints/test-work.js
+++ test/src/api/endpoints/test-work.js
@@ -16,15 +16,15 @@ describe("Work", () => {
 		// Test to get basic information of a work
 		it("should get basic information of work", (done) => {
 			 chai.request(app)
-				 .get('/v1/work/bbid')
+				 .get('/v1/work/13ab71b4-908f-4c72-88a9-4b94a56c7d3e')
 				 .end((err, res) => {
 					 res.should.have.status(200);
 					 res.body.should.be.a('object');
 					 res.body.should.have.property('bbid');
-					 res.body.should.have.property('default-alias');
+					 res.body.should.have.property('defaultAlias');
 					 res.body.should.have.property('languages');
 					 res.body.should.have.property('disambiguation');
-					 res.body.should.have.property('type');
+					 res.body.should.have.property('entityType');
 					 done();
 				  });
 		 });
