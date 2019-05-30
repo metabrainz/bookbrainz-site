9af83063d9f58a0c0e895ecb03887dc16322b67d
Monkey Do <contact@monkeydo.digital>
2019-05-30 12:16:57 +0200

    fix: Cleanup and pass ESLint, remove old date field

--- src/client/entity-editor/common/new-date-field.js
+++ src/client/entity-editor/common/new-date-field.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
 import {Button, FormControl, InputGroup} from 'react-bootstrap';
+import {dateObjectToString, getTodayDate} from '../../helpers/utils';
+
 import CustomInput from '../../input';
-import DatePicker from 'react-datepicker'
+import DatePicker from 'react-datepicker';
 import FontAwesome from 'react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
 import ValidationLabel from './validation-label';
 import classNames from 'classnames';
-import {getSeparatedDate} from '../validators/date';
-import moment from 'moment';
-import {getTodayDate, dateObjectToString} from '../../helpers/utils';
 import {dateIsBefore} from '../validators/base';
+import moment from 'moment';
 
 
 class DateField extends React.Component {
@@ -20,68 +19,67 @@ class DateField extends React.Component {
 		const {day, month, year} = this.props.defaultValue;
 		this.state = {
 			day: !day ? '' : day,
-			month: !month ? '': month,
-			year: !year ? '' : year,
-			warn: dateIsBefore(getTodayDate(),{day, month, year})
+			month: !month ? '' : month,
+			warn: dateIsBefore(getTodayDate(), {day, month, year}),
+			year: !year ? '' : year
 		};
 	}
 
 	updateDate = (day, month, year) => {
 		this.props.onChangeDate({
-			year: !year ? '' : year,
+			day: !day ? '' : day,
 			month: !month ? '' : month,
-			day: !day ? '' : day
+			year: !year ? '' : year
 		});
 
-		this.setState({warn: dateIsBefore(getTodayDate(),{day, month, year})});
+		this.setState({warn: dateIsBefore(getTodayDate(), {day, month, year})});
 	};
 
 	handleYearChange = (event) => {
-		let year = event.target.value;
+		const year = event.target.value;
 		this.setState({year});
 		this.updateDate(this.state.day, this.state.month, year);
 	};
 
 	handleMonthChange = (event) => {
-		let month = event.target.value;
+		const month = event.target.value;
 		this.setState({month});
 		this.updateDate(this.state.day, month, this.state.year);
 	};
 
 	handleDayChange = (event) => {
-		let day = event.target.value;
-		this.setState({day})
+		const day = event.target.value;
+		this.setState({day});
 		this.updateDate(day, this.state.month, this.state.year);
 	};
 
 	handleChangeOfDatePicker = (value) => {
 		const date = new Date(value);
-		const year = (date.getFullYear()).toString();
-		const month = (date.getMonth()+1).toString();
-		const day = (date.getDate()).toString();
+		const year = date.getFullYear().toString();
+		const month = (date.getMonth() + 1).toString();
+		const day = date.getDate().toString();
 		this.setState({year});
 		this.setState({month});
-		this.setState({day})
+		this.setState({day});
 		this.updateDate(day, month, year);
-	}
+	};
 
 	render() {
-		
 		const labelElement = (
 			<ValidationLabel
 				empty={this.props.empty}
 				error={this.props.error}
-				warn={this.state.warn}
 				errorMessage={this.props.errorMessage}
+				warn={this.state.warn}
 			>
 				{this.props.label}
 			</ValidationLabel>
 		);
 
 		const selectedDate = dateObjectToString({
-			year: this.state.year,
+			day: this.state.day,
 			month: this.state.month,
-			day: this.state.day
+			year: this.state.year
 		});
 		const momentDate = moment(selectedDate);
 		const groupClassName = classNames({hidden: !this.props.show});
@@ -99,7 +97,7 @@ class DateField extends React.Component {
 							value={this.state.year}
 							onChange={this.handleYearChange}
 						/>
-					<InputGroup.Addon style={{padding: "0 0.5em"}}>-</InputGroup.Addon>
+						<InputGroup.Addon style={{padding: '0 0.5em'}}>-</InputGroup.Addon>
 						<FormControl
 							maxLength="2"
 							placeholder="MM"
@@ -108,7 +106,7 @@ class DateField extends React.Component {
 							value={this.state.month}
 							onChange={this.handleMonthChange}
 						/>
-					<InputGroup.Addon style={{padding: "0 0.5em"}}>-</InputGroup.Addon>
+						<InputGroup.Addon style={{padding: '0 0.5em'}}>-</InputGroup.Addon>
 						<FormControl
 							maxLength="2"
 							placeholder="DD"
@@ -117,20 +115,22 @@ class DateField extends React.Component {
 							value={this.state.day}
 							onChange={this.handleDayChange}
 						/>
-					<InputGroup.Button style={{ fontSize: "inherit"}}>
+						<InputGroup.Button style={{fontSize: 'inherit'}}>
 							<DatePicker
 								peekNextMonth
 								showMonthDropdown
 								showYearDropdown
-								selected={momentDate.isValid() ? momentDate : null}
+								customInput={
+									<Button bsStyle="info" style={{lineHeight: '1.75', padding: '0.375em 0.938em'}} title="Date picker">
+										<FontAwesome name="calendar-alt"/>
+									</Button>
+								}
 								dateFormat="YYYY-MM-DD"
 								dropdownMode="select"
+								selected={momentDate.isValid() ? momentDate : null}
 								timeFormat="false"
 								viewMode="years"
 								onChange={this.handleChangeOfDatePicker}
-								customInput={
-									<Button style={{lineHeight: '1.75', padding: '0.375em 0.938em'}} bsStyle="info" title="Date picker"><FontAwesome name="calendar-alt"/></Button>
-								}
 							/>
 						</InputGroup.Button>
 					</InputGroup>
@@ -146,9 +146,13 @@ DateField.propTypes = {
 	defaultValue: PropTypes.object.isRequired,
 	empty: PropTypes.bool.isRequired,
 	error: PropTypes.bool.isRequired,
+	errorMessage: PropTypes.string,
 	label: PropTypes.string.isRequired,
 	onChangeDate: PropTypes.func.isRequired,
 	show: PropTypes.bool.isRequired
+};
+DateField.defaultProps = {
+	errorMessage: null
 };
 
 export default DateField;
--- src/client/entity-editor/validators/date.js
+++ src/client/entity-editor/validators/date.js
@@ -1,12 +1,9 @@
-/* eslint-disable */
-
-export function dateValidator ( day, month, year) {
+/* eslint-disable no-extra-parens,eqeqeq,sort-keys */
+export function dateValidator(day, month, year) {
 	const isPosIntRegx = /^\+?([0-9]\d*)$/;
 	const isYearPosInt = isPosIntRegx.test(year) && year > 0;
 	const isMonthPosInt = isPosIntRegx.test(month) && month > 0;
 	const isDayPosInt = isPosIntRegx.test(day) && day > 0;
-	// console.log({isYearPosInt, isMonthPosInt, isDayPosInt});
-	// console.log({year, month, day});
 
 	if (!year) {
 		if (!month && !day) {
@@ -14,45 +11,42 @@ export function dateValidator ( day, month, year) {
 		}
 		return {isValid: false, errorMessage: 'Year must be entered'};
 	}
-	else {
-		if (!isYearPosInt) {
-			return {isValid: false, errorMessage: 'Year is not valid'};
+
+	if (!isYearPosInt) {
+		return {isValid: false, errorMessage: 'Year is not valid'};
+	}
+	if (!month) {
+		if (!day) {
+			return {isValid: true, errorMessage: ''};
 		}
-		if (!month) {
-			if (!day) {
-				return {isValid: true, errorMessage: ''};
-			}
-			return {isValid: false, errorMessage: 'Month must be entered'}
+		return {isValid: false, errorMessage: 'Month must be entered'};
+	}
+	if (!isMonthPosInt) {
+		return {isValid: false, errorMessage: 'Month is not valid'};
+	}
+	else if (month < 1 || month > 12) {
+		return {isValid: false, errorMessage: 'Month is not valid'};
+	}
+	if (!day) {
+		return {isValid: true, errorMessage: ''};
+	}
+	else if (!isDayPosInt) {
+		return {isValid: false, errorMessage: 'Day is not valid'};
+	}
+	else if (day < 1 || day > 31) {
+		return {isValid: false, errorMessage: 'Day is not valid'};
+	}
+	else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) {
+		return {isValid: false, errorMessage: 'Day is not valid for this month'};
+	}
+	else if (month == 2) {
+		const isleap = (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
+		if (day < 1 || day > 29) {
+			return {isValid: false, errorMessage: 'Day is not valid for this month'};
 		}
-		else {
-			if (!isMonthPosInt) {
-				return {isValid: false, errorMessage: 'Month is not valid'};
-			}
-			else if (month < 1 || month > 12) {
-				return {isValid: false, errorMessage: 'Month is not valid'};
-			}
-			if (!day) {
-				return {isValid: true, errorMessage: ''};
-			}
-			else if (!isDayPosInt) {
-				return {isValid: false, errorMessage: 'Day is not valid'};
-			}
-			else if (day < 1 || day > 31) {
-				return {isValid: false, errorMessage: 'Day is not valid'};
-			}
-			else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31) {
-				return {isValid: false, errorMessage: 'Day is not valid for this month'};
-			}
-			else if (month == 2) {
-				let isleap = (year % 100 == 0) ? (year % 400 == 0) : (year % 4 == 0);
-				if (day < 1 || day > 29) {
-					return {isValid: false, errorMessage: 'Day is not valid for this month'}
-				}
-				else if (day > 29 || (day==29 && !isleap)) {
-					return {isValid: false, errorMessage: 'Year is not leap, invalid day'}
-				}
-			}
-			return {isValid: true, errorMessage: ''}
+		else if (day > 29 || (day == 29 && !isleap)) {
+			return {isValid: false, errorMessage: 'Year is not leap, invalid day'};
 		}
 	}
+	return {isValid: true, errorMessage: ''};
 }
