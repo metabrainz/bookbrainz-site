6a5f38b1cc7e23558ddbd92bf4f3f32a9c6f8291
Akhilesh Kumar <akhileshnith@gmail.com>
2019-06-01 00:12:10 +0530

    refactor: restrucure api section

--- src/api/routes/work.js
+++ src/api/routes/work.js
@@ -9,12 +9,10 @@ const router = express.Router();
 
 
 function middleware(req, res, next) {
-	//console.log('middleware called');
 	next();
 }
 
 router.get('/:bbid', middleware, async (req, res) => {
-	//console.log(req.params.bbid);
 	const work = await getEntityBasicInfo(req); // example params
 	return res.send(work);
 });
