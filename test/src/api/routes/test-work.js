e1b0e47d9ed07033a7f83a4ca8320be02f081ebc
Akhilesh Kumar <akhileshnith@gmail.com>
2019-06-07 00:55:27 +0530

    refactor: use expect instead of should to make consistecy on tests

--- test/src/api/routes/test-work.js
+++ test/src/api/routes/test-work.js
@@ -25,7 +25,7 @@ import chaiHttp from 'chai-http';
 
 
 chai.use(chaiHttp);
-chai.should();
+const {expect} = chai;
 
 
 const aBBID = getRandomUUID();
@@ -38,10 +38,9 @@ describe('GET /work', () => {
 	// Test to get basic information of a work
 	it('should get basic information of work', async function () {
 		const res = await chai.request(app).get(`/work/${aBBID}`);
-
-		res.should.have.status(200);
-		res.body.should.be.a('object');
-		res.body.should.have.all.keys(
+		expect(res.status).to.equal(200);
+		expect(res.body).to.be.an('object');
+		expect(res.body).to.have.all.keys(
 			'bbid',
 			'defaultAlias',
 			'languages',
@@ -53,11 +52,10 @@ describe('GET /work', () => {
 
 	 it('should return list of aliases of work', async function () {
 		const res = await chai.request(app).get(`/work/${aBBID}/aliases`);
-
-		res.should.have.status(200);
-		res.body.should.be.a('object');
-		res.body.aliases.should.be.a('array');
-		res.body.should.all.keys(
+		expect(res.status).to.equal(200);
+		expect(res.body).to.be.an('object');
+		expect(res.body.aliases).to.be.an('array');
+		expect(res.body).to.have.all.keys(
 			'bbid',
 			'aliases'
 		);
@@ -65,11 +63,10 @@ describe('GET /work', () => {
 
 	 it('should return list of identifiers of work', async function () {
 		const res = await chai.request(app).get(`/work/${aBBID}/identifiers`);
-
-		res.should.have.status(200);
-		res.body.should.be.a('object');
-		res.body.identifiers.should.be.a('array');
-		res.body.should.all.keys(
+		expect(res.status).to.equal(200);
+		expect(res.body).to.be.an('object');
+		expect(res.body.identifiers).to.be.an('array');
+		expect(res.body).to.have.all.keys(
 			'bbid',
 			'identifiers'
 		);
@@ -77,35 +74,30 @@ describe('GET /work', () => {
 
 	 it('should throw a 404 error if trying to access a work that does not exist', async function () {
 		const res = await chai.request(app).get(`/work/${bBBID}`);
-
-		res.should.have.status(404);
-		res.body.should.be.a('object');
-		res.body.message.should.equal('Work not found');
+		expect(res.status).to.equal(404);
+		expect(res.body).to.be.an('object');
+		expect(res.body.message).to.equal('Work not found');
 	 });
 
-	it('should throw a 406 error if trying to access a work with ivalid BBID', async function () {
+	it('should throw a 406 error if trying to access a work with invalid BBID', async function () {
 		const res = await chai.request(app).get(`/work/${inValidBBID}`);
-
-		res.should.have.status(406);
-		res.body.should.be.a('object');
-		res.body.message.should.equal('BBID is not valid uuid');
+		expect(res.status).to.equal(406);
+		expect(res.body.message).to.equal('BBID is not valid uuid');
 	 });
 
 	 it('should throw a 404 error if trying to identifiers aliases of a work that does not exist', async function () {
 		const res = await chai.request(app).get(`/work/${bBBID}/identifiers`);
-
-		res.should.have.status(404);
-		res.body.should.be.a('object');
-		res.body.message.should.equal('Work not found');
+		expect(res.status).to.equal(404);
+		expect(res.body).to.be.an('object');
+		expect(res.body.message).to.equal('Work not found');
 	 });
 
 
 	it('should throw a 404 error if trying to access aliases of a work that does not exist', async function () {
 		const res = await chai.request(app).get(`/work/${bBBID}/aliases`);
-
-		res.should.have.status(404);
-		res.body.should.be.a('object');
-		res.body.message.should.equal('Work not found');
+		expect(res.status).to.equal(404);
+		expect(res.body).to.be.an('object');
+		expect(res.body.message).to.equal('Work not found');
 	 });
 });
 
