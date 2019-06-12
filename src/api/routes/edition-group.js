c72d8897433e84abea1ff4549a3d3b6bdfb8afb1
Akhilesh Kumar <akhileshnith@gmail.com>
2019-06-12 22:56:03 +0530

    feat: add lookup endpoints for edition-group and author with basic tests

---
+++ src/api/routes/edition-group.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019  Akhilesh Kumar
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import {getEditionGroupBasicInfo, getEntityAliases, getEntityIdentifiers} from '../helpers/formatEntityData';
+import _ from 'lodash';
+import express from 'express';
+import {makeEntityLoader} from '../helpers/entityLoader';
+
+
+const router = express.Router();
+
+const editionGroupBasicRelations = [
+	'defaultAlias.language',
+	'disambiguation',
+	'editionGroupType'
+];
+
+const editionGroupError = 'Edition-Group not found';
+
+router.get('/:bbid',
+	makeEntityLoader('EditionGroup', editionGroupBasicRelations, editionGroupError),
+	async (req, res, next) => {
+		const editionGroupBasicInfo = await getEditionGroupBasicInfo(res.locals.entity);
+		return res.status(200).send(editionGroupBasicInfo);
+	});
+
+const editionGroupAliasRelation = ['aliasSet.aliases.language'];
+
+router.get('/:bbid/aliases',
+	makeEntityLoader('EditionGroup', editionGroupAliasRelation, editionGroupError),
+	async (req, res, next) => {
+		const editionAliasesList = await getEntityAliases(res.locals.entity);
+		return res.status(200).send(editionAliasesList);
+	});
+
+const editionGroupIdentifierRelation = ['identifierSet.identifiers.type'];
+
+router.get('/:bbid/identifiers',
+	makeEntityLoader('EditionGroup', editionGroupIdentifierRelation, editionGroupError),
+	async (req, res, next) => {
+		const editionGroupIdentifiersList = await getEntityIdentifiers(res.locals.entity);
+		return res.status(200).send(editionGroupIdentifiersList);
+	});
+
+export default router;
