776b9b8bd89becb171cc17139fe7b4fa33e5d725
Akhilesh Kumar <akhileshnith@gmail.com>
2019-07-27 00:49:03 +0530

    feat: add swagger-jsdoc for author endpoints

--- src/api/routes/author.js
+++ src/api/routes/author.js
@@ -37,17 +37,155 @@ const authorBasicRelations = [
 const authorError = 'Author not found';
 
 /**
- * @swagger
- *
- * /author:
- *   get:
- *     description: Retrive the basic information of an author
+ *@swagger
+ *definitions:
+ *  defaultAlias:
+ *    type: object
+ *    properties:
+ *      name:
+ *        type: string
+ *        example: 'Robert A. Heinlein'
+ *      sortName:
+ *        type: string
+ *        example: 'Heinlein, Robert A.'
+ *      aliasLanguage:
+ *        type: string
+ *        example: 'English'
+ *  AuthorDetail:
+ *   type: object
+ *   properties:
+ *     bbid:
+ *       type: string
+ *       format: uuid
+ *       example: '2e5f49a8-6a38-4cc7-97c7-8e624e1fc2c1'
+ *     beginArea:
+ *       type: string
+ *       example: 'United States'
+ *     beginDate:
+ *       type: string
+ *       example: '1907-07-07'
+ *     defaultAlias:
+ *         $ref: '#/definitions/defaultAlias'
+ *     disambiguation:
+ *       type: string
+ *       example: 'Robert A. Heinlein'
+ *     endArea:
+ *       type: string
+ *       example: 'United States'
+ *     endDate:
+ *       type: string
+ *       example: '1988-05-08'
+ *     ended:
+ *       type: boolean
+ *       example: true
+ *     gender:
+ *       type: string
+ *       example: 'Male'
+ *     type:
+ *       type: string
+ *       example: 'Person'
+ *  AuthorAliases:
+ *    type: object
+ *    properties:
+ *      bbid:
+ *        type: string
+ *        format: uuid
+ *        example: '2e5f49a8-6a38-4cc7-97c7-8e624e1fc2c1'
+ *      aliases:
+ *        type: array
+ *        items:
+ *          type: object
+ *          properties:
+ *            name:
+ *              type: string
+ *              example: 'Robert A. Heinlein'
+ *            sortName:
+ *              type: string
+ *              example: 'Heinlein, Robert A.'
+ *            aliasLanguage:
+ *              type: string
+ *              example: 'English'
+ *            primary:
+ *              type: boolean
+ *              example: true
+ *  AuthorIdentifiers:
+ *    type: object
+ *    properties:
+ *      bbid:
+ *        type: string
+ *        format: uuid
+ *        example: '2e5f49a8-6a38-4cc7-97c7-8e624e1fc2c1'
+ *      identifiers:
+ *        type: array
+ *        items:
+ *          type: object
+ *          properties:
+ *            type:
+ *              type: string
+ *              example: 'Wikidata ID'
+ *            value:
+ *              type: string
+ *              example: 'Q123078'
+ *  AuthorRelationships:
+ *    type: object
+ *    properties:
+ *      bbid:
+ *        type: string
+ *        format: uuid
+ *        example: '2e5f49a8-6a38-4cc7-97c7-8e624e1fc2c1'
+ *      relationships:
+ *        type: array
+ *        items:
+ *          type: object
+ *          properties:
+ *            direction:
+ *              type: string
+ *              example: 'forward'
+ *            id:
+ *              type: number
+ *              example: 804
+ *            linkPhrase:
+ *              type: string
+ *              example: 'wrote'
+ *            relationshipTypeId:
+ *              type: number
+ *              example: 8
+ *            relationshipTypeName:
+ *              type: string
+ *              example: 'Author'
+ *            targetBbid:
+ *              type: string
+ *							format: uuid
+ *							example: '4682cf09-66fb-4542-b457-b889117e0279'
+ *						targetEntityType:
+ *              type: string
+ *              example: 'work'
+ */
+
+/**
+ *@swagger
+ *'/author/{bbid}':
+ *  get:
  *     tags:
- *       - author
+ *       - Lookup Requests
+ *     summary: Lookup Author by BBID
+ *     description: Returns the basic details of an Author
+ *     operationId: getAuthorByBbid
  *     produces:
  *       - application/json
+ *     parameters:
+ *       - name: bbid
+ *         in: path
+ *         description: BBID of Author to get the data
+ *         required: true
+ *         type: string
  *     responses:
  *       200:
+ *         description: Basic information of an Author entity
+ *         schema:
+ *             $ref: '#/definitions/AuthorDetail'
+ *       404:
+ *         description: Author not found
  */
 
 router.get('/:bbid',
@@ -58,6 +196,32 @@ router.get('/:bbid',
 	});
 
 
+/**
+ *	@swagger
+ * '/author/{bbid}/aliases':
+ *   get:
+ *     tags:
+ *       - Lookup Requests
+ *     summary: Get list of aliases of an author by BBID
+ *     description: Returns the list of aliases of an author
+ *     operationId: getAliasesOfAuthorByBbid
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: bbid
+ *         in: path
+ *         description: BBID of author to return that data
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: List of aliases with BBID of an author entiity
+ *         schema:
+ *             $ref: '#/definitions/AuthorAliases'
+ *       404:
+ *         description: Author not found
+ */
+
 router.get('/:bbid/aliases',
 	makeEntityLoader('Author', aliasesRelation, authorError),
 	async (req, res, next) => {
@@ -65,13 +229,63 @@ router.get('/:bbid/aliases',
 		return res.status(200).send(authorAliasesList);
 	});
 
-
+/**
+ *	@swagger
+ * '/author/{bbid}/identifiers':
+ *   get:
+ *     tags:
+ *       - Lookup Requests
+ *     summary: Get list of identifiers of an author by BBID
+ *     description: Returns the list of identifiers of an author
+ *     operationId: getIdentifiersOfAuthorByBbid
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: bbid
+ *         in: path
+ *         description: BBID of author to return that data
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: List of identifiers with BBID of an author entiity
+ *         schema:
+ *             $ref: '#/definitions/AuthorIdentifiers'
+ *       404:
+ *         description: Author not found
+ */
 router.get('/:bbid/identifiers',
 	makeEntityLoader('Author', identifiersRelation, authorError),
 	async (req, res, next) => {
 		const authorIdentifiersList = await getEntityIdentifiers(res.locals.entity);
 		return res.status(200).send(authorIdentifiersList);
 	});
+
+/**
+ *	@swagger
+ * '/author/{bbid}/relationships':
+ *   get:
+ *     tags:
+ *       - Lookup Requests
+ *     summary: Get list of relationships of an author by BBID
+ *     description: Returns the list of relationships of an author
+ *     operationId: getRelationshipsOfAuthorByBbid
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: bbid
+ *         in: path
+ *         description: BBID of author to return that data
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: List of relationships with BBID of an author entiity
+ *         schema:
+ *             $ref: '#/definitions/AuthorRelationships'
+ *       404:
+ *         description: Author not found
+ */
 
 router.get('/:bbid/relationships',
 	makeEntityLoader('Author', relationshipsRelation, authorError),
