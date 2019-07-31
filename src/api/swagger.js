48f872312f2db5e786114f2e1551f780a8f07376
Monkey Do <contact@monkeydo.digital>
2019-07-31 14:20:37 +0200

    Tweaked SwaggerUI options

--- src/api/swagger.js
+++ src/api/swagger.js
@@ -3,12 +3,12 @@
 const express = require('express')
 const router = express.Router()
 
-const options = {
+const swaggerOptions = {
   swaggerDefinition: {
     info: {
       title: 'BookBrainz API Documentation',
-      version: '1.0.0',
-      description: 'REST API with Swagger doc',
+      version: '0.1.0',
+      description: 'Swagger 2.0 documentation for the BookBrainz REST API.',
       contact: {
         email: 'akhileshithcse@gmail.com'
       }
@@ -20,16 +20,25 @@ const options = {
   apis: ['src/api/routes/*.js', 'src/api/*.js']
 }
 
+const swaggerUIOptions = {
+  swaggerOptions: {
+    deepLinking: true,
+    defaultModelsExpandDepth: 3,
+    defaultModelExpandDepth: 3,
+    operationsSorter: 'alpha'
+  }
+};
+
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-const swaggerSpec = swaggerJSDoc(options)
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 router.get('/json', function (req, res) {
   res.setHeader('Content-Type', 'application/json')
   res.send(swaggerSpec)
-})
+});
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUIOptions));
 
 
 export default router;
