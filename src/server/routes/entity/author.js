metabrainz/bookbrainz-site

commit 08096c3383b9398871d10cf7a1e98971c2ea65ce

fix(entity-editor): Fix BB-353: 500 error when Editing some author entities. (#308)
* fix(profile):(Updated)Fixed the 500 error when Editing some author entities.

* fix: Linting issues fixed for author.js.

src/server/routes/entity/author.js
@@ -86,7 +86,7 @@ router.get('/:bbid/revisions', (req, res, next) => {
 // Creation
 router.get(
 	'/create', auth.isAuthenticated, middleware.loadIdentifierTypes,
-	middleware.loadGenders,	middleware.loadLanguages,
+	middleware.loadGenders, middleware.loadLanguages,
 	middleware.loadAuthorTypes, middleware.loadRelationshipTypes,
 	(req, res) => {
 		const {markup, props} = entityEditorMarkup(generateEntityProps(
@@ -106,8 +106,8 @@ router.get(
 
 function authorToFormState(author) {
 	const aliases = author.aliasSet ?
-		author.aliasSet.aliases.map(({language, ...rest}) => ({
-			language: language.id,
+		author.aliasSet.aliases.map(({languageId, ...rest}) => ({
+			language: languageId,
 			...rest
 		})) : [];
 

commit a7f873b26629ec7df9ed5478660b91bd62cf010d

fix: Reassign alias language id safely
Fix issue with language being reassigned by object destructuring (introduced in PR #308), and refactor other entities to use the same protection

src/server/routes/entity/author.js
@@ -105,10 +105,11 @@ router.get(
 );
 
 function authorToFormState(author) {
+	/** The front-end expects a language id rather than the language object. */
 	const aliases = author.aliasSet ?
 		author.aliasSet.aliases.map(({languageId, ...rest}) => ({
-			language: languageId,
-			...rest
+			...rest,
+			language: languageId
 		})) : [];
 
 	const defaultAliasIndex = entityRoutes.getDefaultAliasIndex(aliases);

src/server/routes/entity/edition-group.js
@@ -33,6 +33,7 @@ import {escapeProps} from '../../helpers/props';
 import express from 'express';
 import target from '../../templates/target';
 
+
 const router = express.Router();
 
 /* If the route specifies a BBID, load the EditionGroup for it. */
@@ -114,10 +115,11 @@ function getDefaultAliasIndex(aliases) {
 }
 
 function editionGroupToFormState(editionGroup) {
+	/** The front-end expects a language id rather than the language object. */
 	const aliases = editionGroup.aliasSet ?
-		editionGroup.aliasSet.aliases.map(({language, ...rest}) => ({
-			language: language.id,
-			...rest
+		editionGroup.aliasSet.aliases.map(({languageId, ...rest}) => ({
+			...rest,
+			language: languageId
 		})) : [];
 
 	const defaultAliasIndex = getDefaultAliasIndex(aliases);

src/server/routes/entity/edition.js
@@ -206,10 +206,11 @@ function getDefaultAliasIndex(aliases) {
 }
 
 function editionToFormState(edition) {
+	/** The front-end expects a language id rather than the language object. */
 	const aliases = edition.aliasSet ?
-		edition.aliasSet.aliases.map(({language, ...rest}) => ({
-			language: language.id,
-			...rest
+		edition.aliasSet.aliases.map(({languageId, ...rest}) => ({
+			...rest,
+			language: languageId
 		})) : [];
 
 	const defaultAliasIndex = getDefaultAliasIndex(aliases);

src/server/routes/entity/publisher.js
@@ -122,10 +122,11 @@ router.get(
 );
 
 function publisherToFormState(publisher) {
+	/** The front-end expects a language id rather than the language object. */
 	const aliases = publisher.aliasSet ?
-		publisher.aliasSet.aliases.map(({language, ...rest}) => ({
-			language: language.id,
-			...rest
+		publisher.aliasSet.aliases.map(({languageId, ...rest}) => ({
+			...rest,
+			language: languageId
 		})) : [];
 
 	const defaultAliasIndex = entityRoutes.getDefaultAliasIndex(aliases);

src/server/routes/entity/work.js
@@ -35,6 +35,7 @@ import {escapeProps} from '../../helpers/props';
 import express from 'express';
 import target from '../../templates/target';
 
+
 const router = express.Router();
 
 /* If the route specifies a BBID, load the Work for it. */
@@ -158,10 +159,11 @@ function getDefaultAliasIndex(aliases) {
 }
 
 function workToFormState(work) {
+	/** The front-end expects a language id rather than the language object. */
 	const aliases = work.aliasSet ?
-		work.aliasSet.aliases.map(({language, ...rest}) => ({
-			language: language.id,
-			...rest
+		work.aliasSet.aliases.map(({languageId, ...rest}) => ({
+			...rest,
+			language: languageId
 		})) : [];
 
 	const defaultAliasIndex = getDefaultAliasIndex(aliases);