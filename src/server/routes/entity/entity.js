42ce0473e2b0cb4dda34fc2902e16c958ea609b3
Monkey Do <contact@monkeydo.digital>
2019-11-20 19:13:13 +0100

    feat(merge-tool): Don't abort entity update if merge operation

--- src/server/routes/entity/entity.js
+++ src/server/routes/entity/entity.js
@@ -794,7 +794,7 @@ export function handleCreateOrEditEntity(
 				]);
 
 			// If there are no differences, bail
-			if (_.isEmpty(changedProps) && _.isEmpty(relationshipSets)) {
+			if (_.isEmpty(changedProps) && _.isEmpty(relationshipSets) && !isMergeOperation) {
 				throw new error.FormSubmissionError('No Updated Field');
 			}
 
@@ -852,7 +852,7 @@ export function handleCreateOrEditEntity(
 					// We only want to set isMerge to true on the entities we're merging
 					newEntityRevision
 						.query(qb => qb.whereIn('bbid', mergingEntititesBBIDs))
-						.save({isMerge: true}, {debug: true, patch: true, transacting});
+						.save({isMerge: true}, {patch: true, transacting});
 				});
 			}
 
