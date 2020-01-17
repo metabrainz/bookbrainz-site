8571c8086965a1669f11772ef677d01ca60c14ce
Prabal Singh <praba170107045@iitg.ac.in>
2020-01-17 18:47:10 +0530

    change comment

--- src/server/routes/revisions.js
+++ src/server/routes/revisions.js
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2015       Ben Ockmore
- *               2015-2016  Sean Burke
- *               2015       Leo Verto
+ * Copyright (C) 2019 Prabal Singh
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
