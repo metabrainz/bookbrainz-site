2599c7047b36d83e29213327f9d31d1789bbffa4
PrabalSingh <praba170107045@iitg.ac.in>
2020-02-04 21:14:52 +0530

    refactor(search) : use the pager element for search result table

--- src/client/components/pages/search.js
+++ src/client/components/pages/search.js
@@ -18,17 +18,13 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import * as bootstrap from 'react-bootstrap';
 
+import PagerElement from './parts/pager';
 import PropTypes from 'prop-types';
 import React from 'react';
 import SearchField from './parts/search-field';
 import SearchResults from './parts/search-results';
-import {isNil} from 'lodash';
-import request from 'superagent-bluebird-promise';
 
-
-const {Button, ButtonGroup, DropdownButton, MenuItem, Pager} = bootstrap;
 
 class SearchPage extends React.Component {
 	/**
@@ -43,7 +39,6 @@ class SearchPage extends React.Component {
 
 		this.state = {
 			from: 0,
-			nextEnabled: this.props.initialResults.length === this.props.resultsPerPage,
 			query: this.props.query,
 			results: this.props.initialResults,
 			size: this.props.resultsPerPage
@@ -51,10 +46,9 @@ class SearchPage extends React.Component {
 
 		// React does not autobind non-React class methods
 		this.handleSearch = this.handleSearch.bind(this);
-		this.triggerSearch = this.triggerSearch.bind(this);
-		this.handleClickPrevious = this.handleClickPrevious.bind(this);
-		this.handleClickNext = this.handleClickNext.bind(this);
-		this.handleResultsPerPageChange = this.handleResultsPerPageChange.bind(this);
+		this.redirectUrl = this.redirectUrl.bind(this);
+		this.changeDataInTable = this.changeDataInTable.bind(this);
+		this.paginationUrl = './search/search?q=';
 	}
 
 	/**
@@ -73,32 +67,19 @@ class SearchPage extends React.Component {
 			return;
 		}
 
-		this.setState({collection, from: 0, query}, this.triggerSearch);
+		this.setState({collection, from: 0, query}, this.redirectUrl);
 	}
 
-	triggerSearch() {
+	redirectUrl() {
 		const collectionString = this.state.collection ? `&collection=${this.state.collection}` : '';
 		const pagination = `&size=${this.state.size}&from=${this.state.from}`;
-		request.get(`./search/search?q=${this.state.query}${collectionString}${pagination}`)
-			.then((res) => JSON.parse(res.text))
-			.then((data) => {
-				this.setState(prevState => ({
-					nextEnabled: data.length >= prevState.size,
-					results: data.filter(result => !isNil(result))
-				}));
-			});
+
+		const url = `./search/search?q=${this.state.query}${collectionString}${pagination}`;
+		// TODO redirect to url;
 	}
 
-	handleClickPrevious(event) {
-		this.setState(prevState => ({from: Math.max(prevState.from - prevState.size, 0)}), this.triggerSearch);
-	}
-
-	handleClickNext(event) {
-		this.setState(prevState => ({from: prevState.from + prevState.size}), this.triggerSearch);
-	}
-
-	handleResultsPerPageChange(value) {
-		this.setState({size: parseInt(value, 10)}, this.triggerSearch);
+	changeDataInTable(newResults) {
+		this.setState({results: newResults});
 	}
 
 	/**
@@ -116,46 +97,14 @@ class SearchPage extends React.Component {
 					onSearch={this.handleSearch}
 				/>
 				<SearchResults results={this.state.results}/>
-				{
-					this.state.results && this.state.results.length ?
-						<div>
-							<hr className="thin"/>
-							<Pager>
-								<Pager.Item
-									previous disabled={this.state.from <= 0}
-									href="#" onClick={this.handleClickPrevious}
-								>
-									&larr; Previous Page
-								</Pager.Item>
-								<ButtonGroup>
-									<Button disabled>Results {this.state.from + 1} —
-										{this.state.results.length < this.state.size ?
-											this.state.from + this.state.results.length :
-											this.state.from + this.state.size
-										}
-									</Button>
-									<DropdownButton
-										dropup bsStyle="info" id="bg-nested-dropdown"
-										title={`${this.state.size} per page`}
-										onSelect={this.handleResultsPerPageChange}
-									>
-										<MenuItem eventKey="10">10 per page</MenuItem>
-										<MenuItem eventKey="20">20 per page</MenuItem>
-										<MenuItem eventKey="35">35 per page</MenuItem>
-										<MenuItem eventKey="50">50 per page</MenuItem>
-										<MenuItem eventKey="100">100 per page</MenuItem>
-									</DropdownButton>
-								</ButtonGroup>
-								<Pager.Item
-									next disabled={!this.state.nextEnabled}
-									href="#" onClick={this.handleClickNext}
-								>
-									Next Page &rarr;
-								</Pager.Item>
-							</Pager>
-						</div> :
-						null
-				}
+				<PagerElement
+					changeDataInTable={this.changeDataInTable}
+					from={this.state.from}
+					paginationUrl={this.paginationUrl}
+					query={this.state.query}
+					results={this.state.results}
+					size={this.state.size}
+				/>
 			</div>
 		);
 	}
