45e3b9c713db21e906160f40f8ea0e9670684e2b
Divyanshu Raj <divyanshuraj5050@gmail.com>
2020-02-23 18:59:29 +0530

    fixed text overflow on add edition page

--- src/client/entity-editor/edition-section/edition-section.js
+++ src/client/entity-editor/edition-section/edition-section.js
@@ -271,6 +271,7 @@ function EditionSection({
 								<Button
 									block
 									bsStyle="primary"
+									id="existing-edition-group-search-button"
 									onClick={onEditionGroupButtonClick}
 								>
 									Click here to search for an existing one instead
