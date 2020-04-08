5b8db1a2cb2139eebfd0f96f6b6c358198bf0123
Monkey Do <contact@monkeydo.digital>
2020-04-08 14:31:12 +0200

    feature(navbar): Added menu item pointing to revisions page

--- src/client/helpers/setupIconLibrary.js
+++ src/client/helpers/setupIconLibrary.js
@@ -20,7 +20,7 @@ import {config, library} from '@fortawesome/fontawesome-svg-core';
 import {
 	faBook, faCalendarAlt, faChartLine, faCheck, faCircle, faCircleNotch,
 	faComment, faEnvelope, faExclamationTriangle, faExternalLinkAlt,
-	faGlobe, faHistory, faInfo, faPenNib, faPencilAlt, faPencilRuler,
+	faGlobe, faHistory, faInfo, faListUl, faPenNib, faPencilAlt, faPencilRuler,
 	faPlus, faQuestionCircle, faRemoveFormat, faSearch, faSignInAlt,
 	faSignOutAlt, faSlash, faTimes, faTimesCircle, faTrashAlt, faUndo,
 	faUniversity, faUser, faUserCircle, faWindowRestore
@@ -38,7 +38,7 @@ config.autoAddCss = true;
 library.add(
 	fab, faBook, faCalendarAlt, faChartLine, faCheck, faCircle, faCircleNotch,
 	faComment, faEnvelope, faExclamationTriangle, faExternalLinkAlt,
-	faGlobe, faHistory, faInfo, faPenNib, faPencilAlt, faPencilRuler,
+	faGlobe, faHistory, faInfo, faListUl, faPenNib, faPencilAlt, faPencilRuler,
 	faPlus, faQuestionCircle, faRemoveFormat, faSearch, faSignInAlt,
 	faSignOutAlt, faSlash, faTimes, faTimesCircle, faTrashAlt, faUndo,
 	faUniversity, faUser, faUserCircle, faWindowRestore
