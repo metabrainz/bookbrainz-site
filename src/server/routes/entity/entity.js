52a6762284e4ac495555a302db353ee95f51c9b1
Anirudh Jain <anirudh.jain@outlook.com>
2020-04-14 21:29:37 +0530

    convert all promises to async await calls

--- src/server/routes/entity/entity.js
+++ src/server/routes/entity/entity.js
@@ -408,43 +408,68 @@ export function handleDelete(
 		}).save(null, {transacting});
 
 		// Get the parents of the new revision
-		const revisionParentsPromise = newRevisionPromise
-			.then((revision) =>
-				revision.related('parents').fetch({require: false, transacting}));
+		const RevisionPromiseRevision = await newRevisionPromise;
+		const revisionParentsPromise = await RevisionPromiseRevision.related('parents').fetch({require: false, transacting});
+		// const revisionParentsPromise = newRevisionPromise
+		// 	.then((revision) =>
+		// 		revision.related('parents').fetch({require: false, transacting}));
 
 		// Add the previous revision as a parent of this revision.
-		const parentAddedPromise =
-			revisionParentsPromise.then(
-				(parents) => parents && parents.attach(
-					entity.revisionId, {transacting}
-				)
-			);
 
-		const notePromise = newRevisionPromise
-			.then((revision) => _createNote(
-				orm, body.note, editorJSON.id, revision, transacting
-			));
+		const parents = await revisionParentsPromise;
+
+		const parentAddedPromise = await parents && parents.attach(
+			entity.revisionId, {transacting}
+		);
+
+		const newRevisionPromiseRevision = await newRevisionPromise;
+
+		const notePromise = _createNote(
+			orm, body.note, editorJSON.id, newRevisionPromiseRevision, transacting
+		);
+
+
+		// const notePromise = newRevisionPromise
+		// 	.then((revision) => _createNote(
+		// 		orm, body.note, editorJSON.id, revision, transacting
+		// 	));
 
 		/*
 		 * No trigger for deletions, so manually create the <Entity>Revision
 		 * and update the entity header
 		 */
 
-		const newEntityRevisionPromise = newRevisionPromise
-			.then((revision) => new RevisionModel({
-				bbid: entity.bbid,
-				dataId: null,
-				id: revision.get('id')
-			}).save(null, {
-				method: 'insert',
-				transacting
-			}));
+		const newEntityRevisionPromise = await new RevisionModel({
+			bbid: entity.bbid,
+			dataId: null,
+			id: await newRevisionPromiseRevision.get('id')
+		}).save(null, {
+			method: 'insert',
+			transacting
+		});
 
-		const entityHeaderPromise = newEntityRevisionPromise
-			.then((entityRevision) => new HeaderModel({
-				bbid: entity.bbid,
-				masterRevisionId: entityRevision.get('id')
-			}).save(null, {transacting}));
+		// const newEntityRevisionPromise = newRevisionPromise
+		// 	.then((revision) => new RevisionModel({
+		// 		bbid: entity.bbid,
+		// 		dataId: null,
+		// 		id: revision.get('id')
+		// 	}).save(null, {
+		// 		method: 'insert',
+		// 		transacting
+		// 	}));
+
+		const entityRevision = await newEntityRevisionPromise;
+
+		const entityHeaderPromise = await new HeaderModel({
+			bbid: entity.bbid,
+			masterRevisionId: await entityRevision.get('id')
+		}).save(null, {transacting});
+
+		// const entityHeaderPromise = newEntityRevisionPromise
+		// 	.then((entityRevision) => new HeaderModel({
+		// 		bbid: entity.bbid,
+		// 		masterRevisionId: entityRevision.get('id')
+		// 	}).save(null, {transacting}));
 
 		const newRevision = await Promise.all(newRevisionPromise);
 
