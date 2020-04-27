1a915d4ac6fca819cf762a96438bdb6f57d21a1d
Chinmay Kothari <chinmay3199@gmail.com>
2020-04-27 21:50:07 +0530

    Update GSoC achievement

--- sql/migrations/2020-04-19/up.sql
+++ sql/migrations/2020-04-19/up.sql
@@ -1,22 +1,12 @@
 BEGIN;
 --(BB-445)
 
--- Delete achievement where GSoC-2016 was unlocked
-DELETE FROM "bookbrainz"."achievement_unlock"
-WHERE id = 13;
+-- Updating the GSoC achievement
+UPDATE bookbrainz.achievement_type
+SET name='GSoC',
+	description='Successfully participated in Google Summer of Code',
+	badge_url='/images/GSoC_logo.png'
 
--- Delete GSoC 2016 as an achievement
-DELETE FROM "bookbrainz"."achievement_type"
-WHERE id = 27;
-
--- Insert GSoC as an achievement(not year specific)
-INSERT INTO "bookbrainz"."achievement_type" ("id", "name", "description", "badge_url")
-VALUES
-	('27', 'GSoC', 'Successfully participated in Google Summer of Code', '/images/GSoC_logo.png');
-
--- Inserting GSoC achievement to previous user
-INSERT INTO "bookbrainz"."achievement_unlock" ("id", "editor_id", "achievement_id", "unlocked_at", "profile_rank")
-VALUES
-	('13', '292', '27', '2016-09-07 20:43:21', '1');
+WHERE id=27;
 
 COMMIT;
