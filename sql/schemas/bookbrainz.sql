1219c03a470b023fad1b5ba3a0108a60e9e03c7e
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-12 14:35:25 +0530

    sql: add the script in schema/bookbrainz.sql sql: add down.sql

--- sql/schemas/bookbrainz.sql
+++ sql/schemas/bookbrainz.sql
@@ -868,4 +868,31 @@ CREATE VIEW bookbrainz.work_import AS
 	LEFT JOIN bookbrainz.alias_set alias_set ON work_data.alias_set_id = alias_set.id
 	WHERE import.type = 'Work';
 
+CREATE TABLE bookbrainz.user_collection (
+	id UUID PRIMARY KEY DEFAULT public.uuid_generate_v4(),
+	editor_id INT,
+	name VARCHAR(80) NOT NULL CHECK (name <> ''),
+	description TEXT NOT NULL DEFAULT '',
+	type bookbrainz.entity_type NOT NULL,
+	public BOOLEAN NOT NULL DEFAULT FALSE,
+	created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now()),
+   	last_modified TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now())
+);
+ALTER TABLE bookbrainz.user_collection ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+
+CREATE TABLE bookbrainz.user_collection_item (
+	collection_id UUID,
+	bbid UUID,
+    added_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now())
+);
+ALTER TABLE bookbrainz.user_collection_item ADD FOREIGN KEY (collection_id) REFERENCES bookbrainz.user_collection (id);
+ALTER TABLE bookbrainz.user_collection_item ADD FOREIGN KEY (bbid) REFERENCES bookbrainz.entity (bbid);
+
+CREATE TABLE bookbrainz.user_collection_collaborator (
+	collection_id UUID ,
+	editor_id INT
+);
+ALTER TABLE bookbrainz.user_collection_collaborator ADD FOREIGN KEY (collection_id) REFERENCES bookbrainz.user_collection (id);
+ALTER TABLE bookbrainz.user_collection_collaborator ADD FOREIGN KEY (editor_id) REFERENCES bookbrainz.editor (id);
+
 COMMIT;
