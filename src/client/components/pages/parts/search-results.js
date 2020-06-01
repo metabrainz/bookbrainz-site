f3c377de0dcdf6f214275b0d2042ea34375e6bec
PrabalSingh <praba170107045@iitg.ac.in>
2020-06-01 14:48:05 +0530

    convert search table to react class; and add checkboxes

--- src/client/components/pages/parts/search-results.js
+++ src/client/components/pages/parts/search-results.js
@@ -34,103 +34,136 @@ const {Row, Table} = bootstrap;
  * @returns {ReactElement} a HTML document which displays the SearchResults.
  * @param {object} props - Properties passed to the component.
  */
+class SearchResults extends React.Component {
+	constructor(props) {
+		super(props);
 
-function SearchResults(props) {
-	const noResults = !props.results || props.results.length === 0;
-	if (noResults) {
+		this.state = {
+			selected: []
+		};
+		this.toggleRow = this.toggleRow.bind(this);
+	}
+
+	toggleRow(entity) {
+		// eslint-disable-next-line react/no-access-state-in-setstate
+		const oldSelected = this.state.selected;
+		let newSelected;
+		if (oldSelected.find(selected => selected.bbid === entity.bbid)) {
+			newSelected = oldSelected.filter(selected => selected.bbid !== entity.bbid);
+		}
+		else {
+			newSelected = oldSelected.push(entity) && oldSelected;
+		}
+		this.setState({
+			selected: newSelected
+		});
+	}
+
+	render() {
+		const noResults = !this.props.results || this.props.results.length === 0;
+		if (noResults) {
+			return (
+				<div className="text-center">
+					<hr className="thin"/>
+					<h2 style={{color: '#754e37'}}>
+						No results found
+					</h2>
+					{
+						!this.props.condensed &&
+						<Row>
+							<small>Make sure the spelling is correct, and that you have selected the correct type in the search bar.</small>
+							<hr className="wide"/>
+							<h3>Are we missing an entry?</h3>
+							<CallToAction/>
+						</Row>
+					}
+				</div>
+			);
+		}
+
+		const results = this.props.results.map((result) => {
+			if (!result) {
+				return null;
+			}
+			const name = result.defaultAlias ? result.defaultAlias.name :
+				'(unnamed)';
+
+			const aliases = !this.props.condensed && result.aliasSet &&
+				Array.isArray(result.aliasSet.aliases) && result.aliasSet.aliases;
+
+			const secondaryAliases = !this.props.condensed && aliases &&
+				_differenceBy(aliases, [result.defaultAlias], 'id').map(alias => alias.name).join(', ');
+
+			const disambiguation = result.disambiguation ? <small>({result.disambiguation.comment})</small> : '';
+			// No redirect link for Area entity results
+			const link = result.type === 'Area' ?
+				`//musicbrainz.org/area/${result.bbid}` :
+				`/${_kebabCase(result.type)}/${result.bbid}`;
+
+			/* eslint-disable react/jsx-no-bind */
+			return (
+				<tr key={result.bbid}>
+					<input
+						checked={this.state.selected.find(selected => selected.bbid === result.bbid)}
+						className="checkbox"
+						type="checkbox"
+						onChange={() => this.toggleRow(result)}
+					/>
+					{
+						!this.props.condensed &&
+						<td>
+							{genEntityIconHTMLElement(result.type)}{_startCase(result.type)}
+						</td>
+					}
+					<td>
+						<a href={link}>
+							{name} {disambiguation}
+						</a>
+					</td>
+					{
+						!this.props.condensed &&
+						<td>
+							{secondaryAliases}
+						</td>
+					}
+				</tr>
+			);
+		});
+		let tableCssClasses = 'table table-striped';
+		if (this.props.condensed) {
+			tableCssClasses += ' table-condensed';
+		}
 		return (
-			<div className="text-center">
-				<hr className="thin"/>
-				<h2 style={{color: '#754e37'}}>
-					No results found
-				</h2>
+			<div>
 				{
-					!props.condensed &&
-					<Row>
-						<small>Make sure the spelling is correct, and that you have selected the correct type in the search bar.</small>
-						<hr className="wide"/>
-						<h3>Are we missing an entry?</h3>
-						<CallToAction/>
-					</Row>
+					!this.props.condensed &&
+					<h3 style={{color: '#754e37'}}>
+						Search Results
+					</h3>
 				}
+				<hr className="thin"/>
+				<Table
+					responsive
+					className={tableCssClasses}
+				>
+					{
+						!this.props.condensed &&
+						<thead>
+							<tr>
+								<th className="col-sm-1">Selcted</th>
+								<th className="col-sm-2">Type</th>
+								<th className="col-sm-5">Name</th>
+								<th className="col-sm-4">Aliases</th>
+							</tr>
+						</thead>
+					}
+					<tbody>
+						{results}
+					</tbody>
+				</Table>
 			</div>
 		);
 	}
-
-	const results = props.results.map((result) => {
-		if (!result) {
-			return null;
-		}
-		const name = result.defaultAlias ? result.defaultAlias.name :
-			'(unnamed)';
-
-		const aliases = !props.condensed && result.aliasSet &&
-		Array.isArray(result.aliasSet.aliases) && result.aliasSet.aliases;
-
-		const secondaryAliases = !props.condensed && aliases &&
-			_differenceBy(aliases, [result.defaultAlias], 'id').map(alias => alias.name).join(', ');
-
-		const disambiguation = result.disambiguation ? <small>({result.disambiguation.comment})</small> : '';
-		// No redirect link for Area entity results
-		const link = result.type === 'Area' ?
-			`//musicbrainz.org/area/${result.bbid}` :
-			`/${_kebabCase(result.type)}/${result.bbid}`;
-
-		return (
-			<tr key={result.bbid}>
-				{
-					!props.condensed &&
-					<td>
-						{genEntityIconHTMLElement(result.type)}{_startCase(result.type)}
-					</td>
-				}
-				<td>
-					<a href={link}>
-						{name} {disambiguation}
-					</a>
-				</td>
-				{
-					!props.condensed &&
-					<td>
-						{secondaryAliases}
-					</td>
-				}
-			</tr>
-		);
-	});
-	let tableCssClasses = 'table table-striped';
-	if (props.condensed) {
-		tableCssClasses += ' table-condensed';
-	}
-	return (
-		<div>
-			{
-				!props.condensed &&
-				<h3 style={{color: '#754e37'}}>
-					Search Results
-				</h3>
-			}
-			<hr className="thin"/>
-			<Table
-				responsive
-				className={tableCssClasses}
-			>
-				{
-					!props.condensed &&
-					<thead>
-						<tr>
-							<th style={{width: '150px'}}>Type</th>
-							<th>Name</th>
-							<th>Aliases</th>
-						</tr>
-					</thead>
-				}
-				<tbody>
-					{results}
-				</tbody>
-			</Table>
-		</div>
-	);
 }
 
 SearchResults.displayName = 'SearchResults';
