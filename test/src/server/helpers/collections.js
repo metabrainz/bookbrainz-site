f82c0c4f08b6f536a4c42cb8cf2a3ef5706abb82
PrabalSingh <praba170107045@iitg.ac.in>
2020-06-13 23:01:22 +0530

    chore: add tests for 'getCollectectionForEditorPage

--- test/src/server/helpers/collections.js
+++ test/src/server/helpers/collections.js
@@ -118,3 +118,220 @@ describe('getOrderedPublicCollections', () => {
 		expect(orderedCollections).to.be.descendingBy('lastModified');
 	});
 });
+
+describe('getOrderedCollectionsForEditorPage', () => {
+	// eslint-disable-next-line one-var
+	let editor, editor2;
+	before(async () => {
+		editor = await createEditor();
+		editor2 = await createEditor();
+		const collectionAttrib = {
+			description: 'description',
+			entityType: 'Author',
+			name: 'name',
+			ownerId: editor.get('id'),
+			public: true
+		};
+		// create a public collections owned by editor1
+		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		// create a public collections owned by editor1 and editor2 as collaborator
+		const collection = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		await new UserCollectionCollaborator({
+			collaboratorId: editor2.get('id'),
+			collectionId: collection.get('id')
+		}).save(null, {method: 'insert'});
+		// create a private collection owned by editor1
+		collectionAttrib.public = false;
+		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		// create a private collection owned by editor1 and editor2 as collaborator
+		const collection2 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		await new UserCollectionCollaborator({
+			collaboratorId: editor2.get('id'),
+			collectionId: collection2.get('id')
+		}).save(null, {method: 'insert'});
+		// create one Edition collection with editor1 as owner
+		collectionAttrib.public = true;
+		collectionAttrib.entityType = 'Edition';
+		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+	});
+	after(truncateEntities);
+
+	it('should return all collections when editor1 is logged in (fetching collections for editor1)', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor.get('id')
+			},
+			user: {
+				id: editor.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+
+		expect(orderedCollections.length).to.equal(5);
+		orderedCollections.forEach((collection) => {
+			expect(collection).to.have.keys(
+				'id',
+				'ownerId',
+				'name',
+				'description',
+				'entityType',
+				'public',
+				'createdAt',
+				'lastModified',
+				'isOwner'
+			);
+		});
+	});
+
+	it('should return only public collections when no one is logged in (fetching collections for editor1)', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+
+		expect(orderedCollections.length).to.equal(3);
+		orderedCollections.forEach((collection) => {
+			expect(collection).to.have.keys(
+				'id',
+				'ownerId',
+				'name',
+				'description',
+				'entityType',
+				'public',
+				'createdAt',
+				'lastModified',
+				'isOwner'
+			);
+		});
+	});
+
+	it('should return all collections when editor2 is logged in (fetching collections for editor2)', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor2.get('id')
+			},
+			user: {
+				id: editor2.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+
+		expect(orderedCollections.length).to.equal(2);
+		orderedCollections.forEach((collection) => {
+			expect(collection).to.have.keys(
+				'id',
+				'ownerId',
+				'name',
+				'description',
+				'entityType',
+				'public',
+				'createdAt',
+				'lastModified',
+				'isOwner'
+			);
+		});
+	});
+
+	it('should return only public collections when no one is logged in (fetching collections for editor2)', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor2.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+
+		expect(orderedCollections.length).to.equal(1);
+		expect(orderedCollections[0]).to.have.keys(
+			'id',
+			'ownerId',
+			'name',
+			'description',
+			'entityType',
+			'public',
+			'createdAt',
+			'lastModified',
+			'isOwner'
+		);
+	});
+
+	it('should return 1 collection when editor1 is logged in (fetching "Edition-Collection" for editor1)', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor.get('id')
+			},
+			user: {
+				id: editor.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, 'Edition', req);
+
+		expect(orderedCollections.length).to.equal(1);
+		expect(orderedCollections[0]).to.have.keys(
+			'id',
+			'ownerId',
+			'name',
+			'description',
+			'entityType',
+			'public',
+			'createdAt',
+			'lastModified',
+			'isOwner'
+		);
+	});
+
+	it('should return correct collections when passed with an offset (fetching for editor1)', async () => {
+		const from = 2;
+		const size = 2;
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor.get('id')
+			},
+			user: {
+				id: editor.get('id')
+			}
+		};
+
+		const allCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+		const orderedCollections = await getOrderedCollectionsForEditorPage(from, size, null, req);
+		const allCollectionsSubset = allCollections.slice(from, from + size);
+		expect(orderedCollections).to.deep.equal(allCollectionsSubset);
+		expect(orderedCollections.length).to.equal(size);
+	});
+});
