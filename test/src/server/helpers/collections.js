0ea2ecafcd12f42331b6df2582004b8433da09b5
PrabalSingh <praba170107045@iitg.ac.in>
2020-06-20 15:41:45 +0530

    chore: make changes in test from last review install: update bookbrainz-data package and install chai-array

--- test/src/server/helpers/collections.js
+++ test/src/server/helpers/collections.js
@@ -19,18 +19,19 @@ import {
 	createEditor,
 	truncateEntities
 } from '../../../test-helpers/create-entities';
-import {date, random} from 'faker';
 import {
 	getOrderedCollectionsForEditorPage,
 	getOrderedPublicCollections
 } from '../../../../src/server/helpers/collections';
-
 import chai from 'chai';
+import {date} from 'faker';
 import orm from '../../../bookbrainz-data';
 
 
 const {expect} = chai;
 chai.use(require('chai-sorted'));
+chai.use(require('chai-arrays'));
+
 
 const {UserCollection, UserCollectionCollaborator} = orm;
 
@@ -102,6 +103,7 @@ describe('getOrderedPublicCollections', () => {
 				'lastModified',
 				'owner'
 			);
+			expect(collection.entityType).to.equal('Author');
 		});
 		expect(orderedCollections.length).to.equal(5);
 		expect(orderedCollections).to.be.descendingBy('lastModified');
@@ -122,6 +124,8 @@ describe('getOrderedPublicCollections', () => {
 describe('getOrderedCollectionsForEditorPage', () => {
 	// eslint-disable-next-line one-var
 	let editor, editor2;
+	// eslint-disable-next-line one-var
+	let collectionID, collectionID2, collectionID3, collectionID4, collectionID5;
 	before(async () => {
 		editor = await createEditor();
 		editor2 = await createEditor();
@@ -132,29 +136,71 @@ describe('getOrderedCollectionsForEditorPage', () => {
 			ownerId: editor.get('id'),
 			public: true
 		};
+
 		// create a public collections owned by editor1
-		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
-		// create a public collections owned by editor1 and editor2 as collaborator
 		const collection = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		collectionID = collection.get('id');
+
+		// create a public collections owned by editor1 and editor2 as collaborator
+		const collection2 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		collectionID2 = collection2.get('id');
 		await new UserCollectionCollaborator({
 			collaboratorId: editor2.get('id'),
-			collectionId: collection.get('id')
+			collectionId: collectionID2
 		}).save(null, {method: 'insert'});
+
 		// create a private collection owned by editor1
 		collectionAttrib.public = false;
-		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		const collection3 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		collectionID3 = collection3.get('id');
+
 		// create a private collection owned by editor1 and editor2 as collaborator
-		const collection2 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		const collection4 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		collectionID4 = collection4.get('id');
 		await new UserCollectionCollaborator({
 			collaboratorId: editor2.get('id'),
-			collectionId: collection2.get('id')
+			collectionId: collectionID4
 		}).save(null, {method: 'insert'});
+
 		// create one Edition collection with editor1 as owner
 		collectionAttrib.public = true;
 		collectionAttrib.entityType = 'Edition';
-		await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		const collection5 = await new UserCollection(collectionAttrib).save(null, {method: 'insert'});
+		collectionID5 = collection5.get('id');
 	});
 	after(truncateEntities);
+
+	it('should return collections with expected keys', async () => {
+		const req = {
+			app: {
+				locals: {
+					orm
+				}
+			},
+			params: {
+				id: editor.get('id')
+			},
+			user: {
+				id: editor.get('id')
+			}
+		};
+
+		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+
+		orderedCollections.forEach((collection) => {
+			expect(collection).to.have.keys(
+				'id',
+				'ownerId',
+				'name',
+				'description',
+				'entityType',
+				'public',
+				'createdAt',
+				'lastModified',
+				'isOwner'
+			);
+		});
+	});
 
 	it('should return all collections when editor1 is logged in (fetching collections for editor1)', async () => {
 		const req = {
@@ -173,20 +219,10 @@ describe('getOrderedCollectionsForEditorPage', () => {
 
 		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
 
+		const collectionIDs = orderedCollections.map(collection => collection.id);
+
 		expect(orderedCollections.length).to.equal(5);
-		orderedCollections.forEach((collection) => {
-			expect(collection).to.have.keys(
-				'id',
-				'ownerId',
-				'name',
-				'description',
-				'entityType',
-				'public',
-				'createdAt',
-				'lastModified',
-				'isOwner'
-			);
-		});
+		expect(collectionIDs).to.be.containingAllOf([collectionID, collectionID2, collectionID3, collectionID4, collectionID5]);
 	});
 
 	it('should return only public collections when no one is logged in (fetching collections for editor1)', async () => {
@@ -203,20 +239,11 @@ describe('getOrderedCollectionsForEditorPage', () => {
 
 		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
 
+		const collectionIDs = orderedCollections.map(collection => collection.id);
+
+		// collection 1, 2, and 5 are public collections
 		expect(orderedCollections.length).to.equal(3);
-		orderedCollections.forEach((collection) => {
-			expect(collection).to.have.keys(
-				'id',
-				'ownerId',
-				'name',
-				'description',
-				'entityType',
-				'public',
-				'createdAt',
-				'lastModified',
-				'isOwner'
-			);
-		});
+		expect(collectionIDs).to.be.containingAllOf([collectionID, collectionID2, collectionID5]);
 	});
 
 	it('should return all collections when editor2 is logged in (fetching collections for editor2)', async () => {
@@ -235,21 +262,11 @@ describe('getOrderedCollectionsForEditorPage', () => {
 		};
 
 		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
+		const collectionIDs = orderedCollections.map(collection => collection.id);
 
+		// editor2 is collaborator for collection 2 and 4
 		expect(orderedCollections.length).to.equal(2);
-		orderedCollections.forEach((collection) => {
-			expect(collection).to.have.keys(
-				'id',
-				'ownerId',
-				'name',
-				'description',
-				'entityType',
-				'public',
-				'createdAt',
-				'lastModified',
-				'isOwner'
-			);
-		});
+		expect(collectionIDs).to.be.containingAllOf([collectionID2, collectionID4]);
 	});
 
 	it('should return only public collections when no one is logged in (fetching collections for editor2)', async () => {
@@ -266,18 +283,9 @@ describe('getOrderedCollectionsForEditorPage', () => {
 
 		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, null, req);
 
+		// collection2 is public and editor2 is it's collaborator
 		expect(orderedCollections.length).to.equal(1);
-		expect(orderedCollections[0]).to.have.keys(
-			'id',
-			'ownerId',
-			'name',
-			'description',
-			'entityType',
-			'public',
-			'createdAt',
-			'lastModified',
-			'isOwner'
-		);
+		expect(orderedCollections[0].id).to.equal(collectionID2);
 	});
 
 	it('should return 1 collection when editor1 is logged in (fetching "Edition-Collection" for editor1)', async () => {
@@ -298,17 +306,7 @@ describe('getOrderedCollectionsForEditorPage', () => {
 		const orderedCollections = await getOrderedCollectionsForEditorPage(0, 10, 'Edition', req);
 
 		expect(orderedCollections.length).to.equal(1);
-		expect(orderedCollections[0]).to.have.keys(
-			'id',
-			'ownerId',
-			'name',
-			'description',
-			'entityType',
-			'public',
-			'createdAt',
-			'lastModified',
-			'isOwner'
-		);
+		expect(orderedCollections[0].entityType).to.equal('Edition');
 	});
 
 	it('should return correct collections when passed with an offset (fetching for editor1)', async () => {
