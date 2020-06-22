65e58a352870922a4a1fabb880e4f0d1fdb7b317
PrabalSingh <praba170107045@iitg.ac.in>
2020-06-22 10:20:15 +0530

    feat(collectio-delete): add delete form and controller

--- webpack.client.js
+++ webpack.client.js
@@ -13,6 +13,7 @@ const clientConfig = {
 	context: path.resolve(__dirname, 'src', 'client'),
 	entry: {
 		'collection/create': ['./controllers/collection/userCollectionForm'],
+		'collection/delete': ['./controllers/collection/userCollectionDeletionForm'],
 		deletion: ['./controllers/deletion.js'],
 		error: ['./controllers/error.js'],
 		index: ['./controllers/index.js'],
