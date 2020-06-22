65035b1700bc522da14e4d7bfb158134cddcc855
PrabalSingh <praba170107045@iitg.ac.in>
2020-06-22 10:19:45 +0530

    feat(collection-delete): add collection/delete route

--- src/server/routes/collection.js
+++ src/server/routes/collection.js
@@ -23,6 +23,7 @@ import {escapeProps, generateProps} from '../helpers/props';
 import Layout from '../../client/containers/layout';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import UserCollectionDeletionForm from '../../client/components/forms/userCollection-delete';
 import UserCollectionForm from '../../client/components/forms/userCollection';
 import {collectionCreateOrEditHandler} from '../helpers/collectionRouteUtils';
 import express from 'express';
@@ -54,6 +55,38 @@ router.param(
 	'collectionId',
 	middleware.makeCollectionLoader()
 );
+
+router.get('/:collectionId/delete', auth.isAuthenticated, auth.isCollectionOwner, (req, res) => {
+	const {collection} = res.locals;
+	const props = generateProps(req, res, {
+		collection
+	});
+	const script = '/js/collection/delete.js';
+	const markup = ReactDOMServer.renderToString(
+		<Layout {...propHelpers.extractLayoutProps(props)}>
+			<UserCollectionDeletionForm
+				collection={props.collection}
+			/>
+		</Layout>
+	);
+	res.send(target({
+		markup,
+		props: escapeProps(props),
+		script
+	}));
+});
+
+router.post('/:collectionId/delete/handler', auth.isAuthenticatedForHandler, auth.isCollectionOwner, async (req, res) => {
+	try {
+		const {UserCollection} = req.app.locals.orm;
+		const {collectionId} = req.params;
+		await new UserCollection({id: collectionId}).destroy();
+		return res.status(200).send({});
+	}
+	catch (err) {
+		return res.status(500).send({});
+	}
+});
 
 router.get('/:collectionId/edit', auth.isAuthenticated, auth.isCollectionOwner, (req, res) => {
 	const {collection} = res.locals;
