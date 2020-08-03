1875e003c5cf19068cf9c34558c9b94c21074290
PrabalSingh <praba170107045@iitg.ac.in>
2020-08-03 12:26:59 +0530

    test: add more tests for create/edit route

--- test/src/server/routes/collection.js
+++ test/src/server/routes/collection.js
@@ -10,13 +10,13 @@ import orm from '../../../bookbrainz-data';
 chai.use(chaiHttp);
 chai.use(assertArrays);
 const {expect} = chai;
-const {UserCollection, UserCollectionItem} = orm;
+const {UserCollection, UserCollectionCollaborator, UserCollectionItem} = orm;
 
 
 describe('POST /collection/create', () => {
 	let agent;
 	let collectionOwner;
-	before(async () => {
+	beforeEach(async () => {
 		try {
 			collectionOwner = await createEditor(123456);
 		}
@@ -27,8 +27,9 @@ describe('POST /collection/create', () => {
 		// back to the server in the next request:
 		agent = await chai.request.agent(app);
 		await agent.get('/cb');
+		// await agent.get('/search/reindex');
 	});
-	after((done) => {
+	afterEach((done) => {
 		// Clear DB tables then close superagent server
 		truncateEntities().then(() => {
 			agent.close(done);
@@ -46,22 +47,23 @@ describe('POST /collection/create', () => {
 		const collection = await new UserCollection({id: res.body.id}).fetch();
 		expect(collection.get('id')).to.equal(res.body.id);
 		expect(collection.get('ownerId')).to.equal(collectionOwner.get('id'));
+		expect(collection.get('name')).to.equal(data.name);
+		expect(collection.get('entityType')).to.equal(data.entityType);
+		expect(collection.get('description')).to.equal(data.description);
+		expect(collection.get('public')).to.equal(true);
 		expect(res.status).to.equal(200);
 	});
 
-	it('should add the collection in the ES index', async () => {
+	xit('should add the collection in the ES index', async () => {
 		const data = {
-			description: 'some description',
+			description: 'some description1234',
 			entityType: 'Author',
 			name: 'collectionName',
 			privacy: 'public'
 		};
 		const res = await agent.post('/collection/create/handler').send(data);
-		const collection = await new UserCollection({id: res.body.id}).fetch();
-
 		const res2 = await agent.get(`/search/search?q=${data.name}&type=collection`);
-		// TODO
-		expect(1).to.equal(1);
+		expect(res2.body[0].id).to.equal(res.body.id);
 	});
 
 	it('should return status code 500 for incorrect entityType', async () => {
@@ -106,10 +108,10 @@ describe('POST /collection/create', () => {
 
 describe('POST collection/edit', () => {
 	// eslint-disable-next-line one-var
-	let agent, collaborator, collectionJSON;
+	let agent, collectionJSON, loggedInUser, oldCollaborator;
 	beforeEach(async () => {
 		try {
-			await createEditor(123456);
+			loggedInUser = await createEditor(123456);
 		}
 		catch (error) {
 			// console.log(error);
@@ -118,9 +120,9 @@ describe('POST collection/edit', () => {
 		// back to the server in the next request:
 		agent = await chai.request.agent(app);
 		await agent.get('/cb');
-		collaborator = await createEditor();
+		oldCollaborator = await createEditor();
 		const data = {
-			collaborators: [collaborator.toJSON()],
+			collaborators: [oldCollaborator.toJSON()],
 			description: 'some description',
 			entityType: 'Author',
 			name: 'collectionName',
@@ -137,7 +139,7 @@ describe('POST collection/edit', () => {
 		});
 	});
 
-	it('should update the collection and return 200 status code', async () => {
+	it('should correctly update the collection and return 200 status code', async () => {
 		const newCollaborator = await createEditor();
 		const newData = {
 			collaborators: [newCollaborator.toJSON()],
@@ -159,9 +161,58 @@ describe('POST collection/edit', () => {
 		expect(updatedCollectionJSON.collaborators[0].collaboratorId).to.equal(newCollaborator.get('id'));
 	});
 
+	it('should correctly add a new collaborator and return 200 status code', async () => {
+		const newCollaborator = await createEditor();
+		const newData = {
+			collaborators: [oldCollaborator.toJSON(), newCollaborator.toJSON()],
+			description: 'some description',
+			entityType: 'Author',
+			name: 'collectionName',
+			privacy: 'public'
+		};
+
+		const res = await agent.post(`/collection/${collectionJSON.id}/edit/handler`).send(newData);
+		const updatedCollection = await new UserCollection({id: collectionJSON.id}).fetch({withRelated: ['collaborators']});
+		const updatedCollectionJSON = updatedCollection.toJSON();
+
+		expect(res.status).to.equal(200);
+		expect(updatedCollectionJSON.collaborators.length).to.equal(2);
+		const collaboratorIds = updatedCollectionJSON.collaborators.map(collaborator => collaborator.collaboratorId);
+		expect(collaboratorIds).to.be.containingAllOf([oldCollaborator.get('id'), newCollaborator.get('id')]);
+	});
+
+	it('should correctly remove a collaborator and return 200 status code', async () => {
+		const newData = {
+			collaborators: [],
+			description: 'some description',
+			entityType: 'Author',
+			name: 'collectionName',
+			privacy: 'public'
+		};
+
+		const res = await agent.post(`/collection/${collectionJSON.id}/edit/handler`).send(newData);
+		const updatedCollection = await new UserCollection({id: collectionJSON.id}).fetch({withRelated: ['collaborators']});
+		const updatedCollectionJSON = updatedCollection.toJSON();
+
+		expect(res.status).to.equal(200);
+		expect(updatedCollectionJSON.collaborators.length).to.equal(0);
+	});
+
+	xit('should update the collection in the ES index', async () => {
+		const newData = {
+			description: 'new description',
+			entityType: 'Author',
+			name: 'newName',
+			privacy: 'public'
+		};
+		const res = await agent.post('/collection/create/handler').send(newData);
+		const res2 = await agent.get(`/search/search?q=${newData.name}&type=collection`);
+		expect(res2.body[0].id).to.equal(res.body.id);
+	});
+
 	it('should return status code 500 for incorrect entityType', async () => {
 		const data = {
-			collaborators: [collaborator.toJSON()],
+			collaborators: [oldCollaborator.toJSON()],
 			description: 'some description',
 			entityType: 'incorrect',
 			name: 'collectionName',
@@ -173,7 +224,7 @@ describe('POST collection/edit', () => {
 
 	it('should return status code 500 for empty collection name', async () => {
 		const data = {
-			collaborators: [collaborator.toJSON()],
+			collaborators: [oldCollaborator.toJSON()],
 			description: 'some description',
 			entityType: 'Author',
 			name: '',
@@ -191,7 +242,7 @@ describe('POST collection/edit', () => {
 		}).save(null, {method: 'insert'});
 
 		const data = {
-			collaborators: [collaborator.toJSON()],
+			collaborators: [oldCollaborator.toJSON()],
 			description: 'some description',
 			entityType: 'Edition',
 			name: 'collectionName',
@@ -199,23 +250,50 @@ describe('POST collection/edit', () => {
 		};
 		const res = await agent.post(`/collection/${collectionJSON.id}/edit/handler`).send(data);
 		expect(res.status).to.equal(500);
+		// expect(res.body.message).to.equal('Trying to change entityType of a non empty collection');
 	});
 
 	it('should throw error when unauthorized user tries to edit the collection', async () => {
-		await createEditor();
-		// The `agent` now has the sessionid cookie saved, and will send it
-		// back to the server in the next request:
-		const newAgent = await chai.request.agent(app);
-		await newAgent.get('/cb');
-		const newData = {
-			description: 'new description',
+		const editor = await createEditor();
+		const data = {
+			description: 'description',
 			entityType: 'Edition',
-			name: 'new collection name',
-			privacy: 'private'
+			name: 'collection name',
+			ownerId: editor.get('id'),
+			public: true
 		};
+		const collection = await new UserCollection(data).save(null, {method: 'insert'});
+		const newData = {
+			...data,
+			name: 'new collection name'
+		};
+		const res = await agent.post(`/collection/${collection.get('id')}/edit/handler`).send(newData);
+		expect(res.status).to.equal(403);
+	});
 
-		const res = await newAgent.post(`/collection/${collectionJSON.id}/edit/handler`).send(newData);
-		// TODO expect(res.status).to.equal(500);
+	it('should throw error when collaborator tries to edit the collection', async () => {
+		const editor = await createEditor();
+		const data = {
+			description: 'description',
+			entityType: 'Edition',
+			name: 'collection name',
+			ownerId: editor.get('id'),
+			public: true
+		};
+		const collection = await new UserCollection(data).save(null, {method: 'insert'});
+		// making loggedInUser as collaborator
+		await new UserCollectionCollaborator({
+			collaboratorId: loggedInUser.get('id'),
+			collectionId: collection.get('id')
+		}).save(null, {method: 'insert'});
+
+		const newData = {
+			...data,
+			name: 'new collection name'
+		};
+		const res = await agent.post(`/collection/${collection.get('id')}/edit/handler`).send(newData);
+		expect(res.status).to.equal(403);
+		// expect(res.body.message).to.equal('You do not have permission to edit this collection');
 	});
 });
 
