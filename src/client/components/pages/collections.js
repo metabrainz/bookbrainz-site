056350b49427fd7008979ab8cb26c63b423dc955
PrabalSingh <praba170107045@iitg.ac.in>
2020-09-08 22:45:32 +0530

    fix pagination url for collection table

--- src/client/components/pages/collections.js
+++ src/client/components/pages/collections.js
@@ -31,7 +31,7 @@ class CollectionsPage extends React.Component {
 
 		this.handleTypeChange = this.handleTypeChange.bind(this);
 		this.searchResultsCallback = this.searchResultsCallback.bind(this);
-		this.paginationUrl = './collections/collections?q=';
+		this.paginationUrl = './collections/collections';
 	}
 
 	searchResultsCallback(newResults) {
@@ -60,7 +60,7 @@ class CollectionsPage extends React.Component {
 					from={this.props.from}
 					nextEnabled={this.props.nextEnabled}
 					paginationUrl={this.paginationUrl}
-					query={this.state.query}
+					querySearchParams={this.state.query}
 					results={this.state.results}
 					searchResultsCallback={this.searchResultsCallback}
 					size={this.props.size}
