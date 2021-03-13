e3a0dbe213403db5fdcfad89ddb1a9b4b7632930
Akash Gupta <akashgp9@gmail.com>
2021-03-13 16:40:20 +0530

    fix(footer): removed buttongroup

--- src/client/components/pages/entities/footer.js
+++ src/client/components/pages/entities/footer.js
@@ -16,19 +16,22 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import * as bootstrap from 'react-bootstrap';
-import * as utilsHelper from '../../../helpers/utils';
-import {faCodeBranch, faGripVertical, faHistory, faPencilAlt, faTimes} from '@fortawesome/free-solid-svg-icons';
-import AddToCollectionModal from '../parts/add-to-collection-modal';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
-import React from 'react';
+import * as bootstrap from "react-bootstrap";
+import * as utilsHelper from "../../../helpers/utils";
+import {
+	faCodeBranch,
+	faGripVertical,
+	faHistory,
+	faPencilAlt,
+	faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import AddToCollectionModal from "../parts/add-to-collection-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import React from "react";
 
-
-const {formatDate} = utilsHelper;
-const {
-	Alert, Button, ButtonGroup, Col, Row
-} = bootstrap;
+const { formatDate } = utilsHelper;
+const { Alert, Button, ButtonGroup, Col, Row } = bootstrap;
 
 class EntityFooter extends React.Component {
 	constructor(props) {
@@ -36,31 +39,32 @@ class EntityFooter extends React.Component {
 		this.state = {
 			message: {
 				text: null,
-				type: null
+				type: null,
 			},
-			showModal: false
+			showModal: false,
 		};
 
 		this.onCloseModal = this.onCloseModal.bind(this);
 		this.handleShowModal = this.handleShowModal.bind(this);
 		this.handleAlertDismiss = this.handleAlertDismiss.bind(this);
-		this.closeModalAndShowMessage = this.closeModalAndShowMessage.bind(this);
+		this.closeModalAndShowMessage = this.closeModalAndShowMessage.bind(
+			this
+		);
 	}
 
 	onCloseModal() {
-		this.setState({showModal: false});
+		this.setState({ showModal: false });
 	}
 
 	handleShowModal() {
 		if (this.props.user) {
-			this.setState({showModal: true});
-		}
-		else {
+			this.setState({ showModal: true });
+		} else {
 			this.setState({
 				message: {
-					text: 'You need to be logged in',
-					type: 'danger'
-				}
+					text: "You need to be logged in",
+					type: "danger",
+				},
 			});
 		}
 	}
@@ -68,79 +72,92 @@ class EntityFooter extends React.Component {
 	closeModalAndShowMessage(message) {
 		this.setState({
 			message,
-			showModal: false
+			showModal: false,
 		});
 	}
 
 	handleAlertDismiss() {
-		this.setState({message: {}});
+		this.setState({ message: {} });
 	}
 
 	render() {
 		return (
 			<div>
-				{
-					this.props.user ?
-						<div>
-							<AddToCollectionModal
-								bbids={[this.props.bbid]}
-								closeModalAndShowMessage={this.closeModalAndShowMessage}
-								entityType={this.props.entityType}
-								handleCloseModal={this.onCloseModal}
-								show={this.state.showModal}
-								userId={this.props.user.id}
-							/>
-						</div> : null
-				}
-				{
-					this.state.message.text ?
-						<Alert bsStyle={this.state.message.type} onDismiss={this.handleAlertDismiss}>{this.state.message.text}</Alert> : null
-
-				}
+				{this.props.user ? (
+					<div>
+						<AddToCollectionModal
+							bbids={[this.props.bbid]}
+							closeModalAndShowMessage={
+								this.closeModalAndShowMessage
+							}
+							entityType={this.props.entityType}
+							handleCloseModal={this.onCloseModal}
+							show={this.state.showModal}
+							userId={this.props.user.id}
+						/>
+					</div>
+				) : null}
+				{this.state.message.text ? (
+					<Alert
+						bsStyle={this.state.message.type}
+						onDismiss={this.handleAlertDismiss}
+					>
+						{this.state.message.text}
+					</Alert>
+				) : null}
 				<Row>
-					<Col md={10} mdOffset={1}>
-						<ButtonGroup justified>
-							<Button
-								bsStyle="warning"
-								disabled={this.props.deleted}
-								href={`${this.props.entityUrl}/edit`}
-								title="Edit Entity"
-							>
-								<FontAwesomeIcon icon={faPencilAlt}/>&nbsp;Edit
-							</Button>
-							<Button
-								bsStyle="primary"
-								href={`${this.props.entityUrl}/revisions`}
-								title="Revision History"
-							>
-								<FontAwesomeIcon icon={faHistory}/>&nbsp;History
-							</Button>
-							<Button
-								bsStyle="danger"
-								disabled={this.props.deleted}
-								href={`${this.props.entityUrl}/delete`}
-								title="Delete Entity"
-							>
-								<FontAwesomeIcon icon={faTimes}/>&nbsp;Delete
-							</Button>
-							<Button
-								bsStyle="default"
-								href={`/merge/add/${this.props.bbid}`}
-								title="Select entity for merging"
-							>
-								<FontAwesomeIcon flip="vertical" icon={faCodeBranch}/>
-								&nbsp;Merge
-							</Button>
-							<Button
-								bsStyle="primary"
-								href="#"
-								title="Add To Collection"
-								onClick={this.handleShowModal}
-							>
-								<FontAwesomeIcon icon={faGripVertical}/>
-								&nbsp;Add to collection
-							</Button>
-						</ButtonGroup>
+					<Col md={10} mdOffset={1} className="text-center">
+						<Button
+							className="custom-width"
+							bsStyle="warning"
+							disabled={this.props.deleted}
+							href={`${this.props.entityUrl}/edit`}
+							title="Edit Entity"
+						>
+							<FontAwesomeIcon icon={faPencilAlt} />
+							&nbsp;Edit
+						</Button>
+						<Button
+							className="custom-width"
+							bsStyle="primary"
+							href={`${this.props.entityUrl}/revisions`}
+							title="Revision History"
+						>
+							<FontAwesomeIcon icon={faHistory} />
+							&nbsp;History
+						</Button>
+						<Button
+							className="custom-width"
+							bsStyle="danger"
+							disabled={this.props.deleted}
+							href={`${this.props.entityUrl}/delete`}
+							title="Delete Entity"
+						>
+							<FontAwesomeIcon icon={faTimes} />
+							&nbsp;Delete
+						</Button>
+						<Button
+							className="custom-width"
+							bsStyle="default"
+							href={`/merge/add/${this.props.bbid}`}
+							title="Select entity for merging"
+						>
+							<FontAwesomeIcon
+								flip="vertical"
+								icon={faCodeBranch}
+							/>
+							&nbsp;Merge
+						</Button>
+						<Button
+							className="custom-width"
+							bsStyle="primary"
+							href="#"
+							title="Add To Collection"
+							onClick={this.handleShowModal}
+						>
+							<FontAwesomeIcon icon={faGripVertical} />
+							&nbsp;Add to collection
+						</Button>
 					</Col>
 				</Row>
 				<div className="text-center margin-top-d10">
@@ -153,17 +170,20 @@ class EntityFooter extends React.Component {
 		);
 	}
 }
-EntityFooter.displayName = 'EntityFooter';
+EntityFooter.displayName = "EntityFooter";
 EntityFooter.propTypes = {
 	bbid: PropTypes.string.isRequired,
 	deleted: PropTypes.bool,
 	entityType: PropTypes.string.isRequired,
 	entityUrl: PropTypes.string.isRequired,
-	lastModified: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]).isRequired,
-	user: PropTypes.object.isRequired
+	lastModified: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.instanceOf(Date),
+	]).isRequired,
+	user: PropTypes.object.isRequired,
 };
 EntityFooter.defaultProps = {
-	deleted: false
+	deleted: false,
 };
 
 export default EntityFooter;
