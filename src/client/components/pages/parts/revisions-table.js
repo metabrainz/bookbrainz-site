bfaafe5f719aa21cc2927918e4549bb96000b19a
Akash Gupta <akashgp9@gmail.com>
2021-03-14 20:31:52 +0530

    fix: added traget blank to anchor tag

--- src/client/components/pages/parts/revisions-table.js
+++ src/client/components/pages/parts/revisions-table.js
@@ -129,7 +129,7 @@ function RevisionsTable(props) {
 														dangerouslySetInnerHTML={{
 															__html: note.content.replace(
 																/((http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?)/g,
-																'<a href="$1">$1</a>'
+																'<a target="_blank" href="$1">$1</a>'
 															),
 														}}
 													></span>
