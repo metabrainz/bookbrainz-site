2389c0adfaebdf7f6e04e8bda0fd14ff086a1e40
Lint Action <lint-action@samuelmeuli.com>
2021-03-17 03:00:05 +0000

    Fix code style issues with ESLint

--- src/server/helpers/mailer.js
+++ src/server/helpers/mailer.js
@@ -1,13 +1,13 @@
-import nodemailer from 'nodemailer'
-import config from "../../../config/config.json"
+import nodemailer from 'nodemailer';
+import config from '../../../config/config.json';
 
-const {mailConfig} = config ;
 
-async function sendEmail({from, to, subject, body}){
-    const transporter = nodemailer.createTransport(mailConfig);
-    //returns a promise 
-    return transporter.sendMail({from, to, subject, body})
+const {mailConfig} = config;
 
+async function sendEmail({from, to, subject, body}) {
+	const transporter = nodemailer.createTransport(mailConfig);
+	// returns a promise
+	return transporter.sendMail({from, to, subject, body});
 }
 
 
