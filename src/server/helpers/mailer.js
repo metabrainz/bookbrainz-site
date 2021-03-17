9df9a390418fd4870296218159b8958c6632edfb
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-03-17 08:34:31 +0530

    corrected wrong transporter options

--- src/server/helpers/mailer.js
+++ src/server/helpers/mailer.js
@@ -3,10 +3,10 @@ import config from "../../../config/config.json"
 
 const {mailConfig} = config ;
 
-async function sendEmail({from, to, subject, body}){
+async function sendEmail({from, to, subject, html}){
     const transporter = nodemailer.createTransport(mailConfig);
     //returns a promise 
-    return transporter.sendMail({from, to, subject, body})
+    return transporter.sendMail({from, to, subject, html})
 
 }
 
