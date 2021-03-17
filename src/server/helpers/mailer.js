9b6b14968838ce76a48e9ebe90271c15ee7dc35c
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-03-17 08:40:04 +0530

    improved config object destructuring

--- src/server/helpers/mailer.js
+++ src/server/helpers/mailer.js
@@ -1,13 +1,11 @@
 import nodemailer from 'nodemailer';
-import config from '../../../config/config.json';
+import {mailConfig} '../../../config/config.json';
 
 
-const {mailConfig} = config;
-
-async function sendEmail({from, to, subject, body}) {
+async function sendEmail({from, to, subject, html}) {
 	const transporter = nodemailer.createTransport(mailConfig);
 	// returns a promise
-	return transporter.sendMail({from, to, subject, body});
+	return transporter.sendMail({from, to, subject, html});
 }
 
 
