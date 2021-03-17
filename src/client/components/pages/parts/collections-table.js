9b3da4c9dc2f272bdbe34a48eea13d6bfa56903b
Akash Gupta <akashgp9@gmail.com>
2021-03-17 08:47:02 +0530

    fix: renaming class

--- src/client/components/pages/parts/collections-table.js
+++ src/client/components/pages/parts/collections-table.js
@@ -89,7 +89,7 @@ class CollectionsTable extends React.Component {
 					<h1 className="text-center">
 						{tableHeading}
 					</h1>
-					<div className="text-collection-entity">
+					<div className="collection-page-buttons">
 						{newCollectionButton}
 						{entityTypeSelect}
 					</div>
