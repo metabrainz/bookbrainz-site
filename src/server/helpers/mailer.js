7647b7315cf26154e4a9d666b1e17517c7d5ea6d
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-03-17 08:10:27 +0530

    added basic nodemailer setup

---
+++ src/server/helpers/mailer.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer'
+import config from "../../../config/config.json"
+
+const {mailConfig} = config ;
+
+async function sendEmail({from, to, subject, bsody}){
+    const transporter = nodemailer.createTransport(mailConfig);
+    //returns a promise 
+    return transporter.sendMail({from, to, subject, body})
+
+}
+
+
+export default sendEmail;
