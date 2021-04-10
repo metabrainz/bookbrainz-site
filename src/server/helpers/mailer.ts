f2aa07a4f4c2d85712ffda6df440467ac687a268
Divyanshu Raj <divyanshuraj5050@gmail.com>
2021-04-11 02:13:18 +0530

    fixed linting errors

--- src/server/helpers/mailer.ts
+++ src/server/helpers/mailer.ts
@@ -21,29 +21,29 @@ import nodemailer from 'nodemailer';
 const {mailConfig} = config;
 
 /**
- * This is a dedicated mailing interface for bookbrainz mailing services
+ * This is a dedicated mailing interface for bookbrainz mailing services.
  *
  * @param {string} from - Email address of sender
- * @param {string} to - Email address of receiver 
+ * @param {string} to - Email address of receiver
  * @param {string} subject- Subject of the E-mail
- * @param {string} html - Body of E-mail in form of HTML 
+ * @param {string} html - Body of E-mail in form of HTML
  * @returns {Promise<object>} Returns a promise that resolves with an error object if message failed or an info object if the message succeeded (see https://nodemailer.com/usage/ for more details)
  * If entity is found successfully in the database this function set the entity data
  * at res.locals.entity and return to next function.
  * @example
  * Promise way :
- * 
+ *
  * sendEmail("abc@gmail.com", "def@gmail.com", "Revision added to Entity", "some html string")
  * .then((response)=>{
-     // the response object contains details of a successful response (see https://nodemailer.com/usage)
-* 	  console.log("Email has been sent successfully")
+ *  // the response object contains details of a successful response (see https://nodemailer.com/usage)
+ * 	  console.log("Email has been sent successfully")
  * 	 })
  * .catch((error)=>{
  * 	  //handle error
  * 	})
- * 
+ *
  * Async/Await way :
- * 
+ *
  *  try{
  * 		const response = await sendEmail("abc@gmail.com", "def@gmail.com", "Revision added to Entity", "some html string")
  * 		console.log("Email has been sent successfully")
@@ -52,16 +52,20 @@ const {mailConfig} = config;
  * 		//handle the error
  * 	}
  * @description
- * This helper function  sends E-mail with attributes collected from argument in given order. 
+ * This helper function  sends E-mail with attributes collected from argument in given order.
  * It uses nodemailer's transporter to send the E-mail under the hood.
  * It returns a promise that should be handled using .then()/.catch() or async/await syntax wrapped in a try…catch block.
  */
 
-function sendEmail(from:string, to:string, subject:string, html:string|JSX.Element) {
+function sendEmail(
+	from: string,
+	to: string,
+	subject: string,
+	html: string | JSX.Element
+) {
 	const transporter = nodemailer.createTransport(mailConfig);
 	// returns a promise
 	return transporter.sendMail({from, html, subject, to});
 }
-
 
 export default sendEmail;
