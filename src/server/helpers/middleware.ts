6e8dfdc4148f44c37c97533a4ce631ed9929d310
Akash Gupta <akashgp9@gmail.com>
2021-04-28 15:56:44 +0530

    added collections.owner to relations

--- src/server/helpers/middleware.ts
+++ src/server/helpers/middleware.ts
@@ -151,7 +151,8 @@ export function makeEntityLoader(modelName: string, additionalRels: Array<string
 		'identifierSet.identifiers.type',
 		'relationshipSet.relationships.type',
 		'revision.revision',
-		'collections'
+		'collections',
+		'collections.owner'
 	].concat(additionalRels);
 
 	return async (req: $Request, res: $Response, next: NextFunction, bbid: string) => {
