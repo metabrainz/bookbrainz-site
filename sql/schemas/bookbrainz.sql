8e3322d05dedbf9536014c3edecf082bf3beee7c
Akash Gupta <akashgp9@gmail.com>
2021-06-01 16:11:49 +0530

    sql: added relationship attribute tables to bookbrainz schema

--- sql/schemas/bookbrainz.sql
+++ sql/schemas/bookbrainz.sql
@@ -557,40 +557,50 @@ ALTER TABLE bookbrainz.relationship ADD FOREIGN KEY (type_id) REFERENCES bookbra
 ALTER TABLE bookbrainz.relationship ADD FOREIGN KEY (source_bbid) REFERENCES bookbrainz.entity (bbid);
 ALTER TABLE bookbrainz.relationship ADD FOREIGN KEY (target_bbid) REFERENCES bookbrainz.entity (bbid);
 
-CREATE TABLE bookbrainz.relationship_attribute_ordinal (
-    id SERIAL,
-    rel_id INT REFERENCES bookbrainz.relationship(id),
-    position INTEGER DEFAULT NULL,
-	number INTEGER DEFAULT NULL,
-	PRIMARY KEY(id, rel_id)
+-- Relationship Attributes
+
+CREATE TABLE bookbrainz.relationship_attribute_set (
+	id SERIAL PRIMARY KEY 
 );
 
-CREATE TABLE bookbrainz.relationship_attribute_date (
-   id SERIAL,
-   rel_id INT REFERENCES bookbrainz.relationship(id),
-   begin_year SMALLINT,
-   begin_month SMALLINT,
-   begin_day SMALLINT,
-   end_year SMALLINT,
-   end_month SMALLINT,
-   end_day SMALLINT,
-   ended BOOLEAN NOT NULL DEFAULT FALSE,
-   PRIMARY KEY(id, rel_id),
-   CHECK (
-		(
-			(
-				end_year IS NOT NULL OR
-				end_month IS NOT NULL OR
-				end_day IS NOT NULL
-			) AND ended = TRUE
-		) OR (
-			(
-				end_year IS NULL AND
-				end_month IS NULL AND
-				end_day IS NULL
-			)
-		)
-	)
+ALTER TABLE bookbrainz.relationship ADD attribute_set_id INTEGER NOT NULL;
+ALTER TABLE bookbrainz.relationship ADD FOREIGN KEY (attribute_set_id) REFERENCES bookbrainz.relationship_attribute_set (id);
+
+CREATE TABLE bookbrainz.relationship_attribute_type (
+  id serial NOT NULL PRIMARY KEY,
+  parent INT DEFAULT NULL,
+  root INT NOT NULL,
+  child_order INT NOT NULL DEFAULT 0,
+  name varchar(255) NOT NULL,
+  description TEXT DEFAULT NULL,
+  last_updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now())
+);
+
+CREATE TABLE bookbrainz.relationship_type__attribute_type (
+  relationship_type INT NOT NULL REFERENCES bookbrainz.relationship_type(id),
+  attribute_type INT NOT NULL REFERENCES bookbrainz.relationship_attribute_type(id),
+  min SMALLINT DEFAULT NULL,
+  max SMALLINT DEFAULT NULL,
+  last_updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT timezone('UTC'::TEXT, now())
+);
+
+CREATE TABLE bookbrainz.relationship_attribute (
+  id SERIAL PRIMARY KEY, 
+  attribute_type INT NOT NULL REFERENCES bookbrainz.relationship_attribute_type(id)
+);
+
+CREATE TABLE bookbrainz.relationship_attribute_text_value (
+  attribute_id INT NOT NULL REFERENCES bookbrainz.relationship_attribute (id),
+  text_value TEXT DEFAULT NULL
+);
+
+CREATE TABLE bookbrainz.relationship_attribute_set__relationship_attribute (
+	set_id INTEGER REFERENCES bookbrainz.relationship_attribute_set (id),
+	attribute_id INTEGER REFERENCES bookbrainz.relationship_attribute (id),
+  PRIMARY KEY (
+    set_id, 
+    attribute_id
+  )
 );
 
 CREATE TABLE bookbrainz.language_set (
