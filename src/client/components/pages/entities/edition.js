73e67ff2fdafe27d461bc94f30efa9f99a43a375
the-good-boy <shivam.awasthi702@gmail.com>
2021-12-07 22:20:20 +0530

    Remove dimensions if format is eBook

--- src/client/components/pages/entities/edition.js
+++ src/client/components/pages/entities/edition.js
@@ -78,10 +78,11 @@ function EditionAttributes({edition}) {
 				</Col>
 				<Col md={3}>
 					<dl>
+						{format !== "eBook" && <>
 						<dt>Dimensions (WxHxD)</dt>
 						<dd>{width}&times;{height}&times;{depth} mm</dd>
 						<dt>Weight</dt>
-						<dd>{weight} g</dd>
+						<dd>{weight} g</dd></>}
 						<dt>Page Count</dt>
 						<dd>{pageCount}</dd>
 					</dl>
