3ba0e3f1839dc268a7200a14aad1ea1bb1ed9b68
tri10 <shubhi.blog@gmail.com>
2022-01-12 17:02:27 +0530

    comparing height for hiding the show more button

--- src/client/components/pages/entities/annotation.js
+++ src/client/components/pages/entities/annotation.js
@@ -25,10 +25,24 @@ import React from 'react';
 class EntityAnnotation extends React.Component {
 	constructor(props) {
 	  super(props);
-
+	  this.preRef = React.createRef();
 	  this.state = {
-			open: false
+			open: false,
+			show: true
 	  };
+	}
+
+
+	componentDidMount() {
+		const {annotation} = this.props.entity;
+		if (!annotation || !annotation.content) {
+			return;
+		}
+		const spanElement = document.querySelector('pre span');
+		if (spanElement.offsetHeight < this.preRef.current.offsetHeight) {
+			// eslint-disable-next-line react/no-did-mount-set-state
+			this.setState({open: false, show: false});
+		}
 	}
 
 	handleToggleCollapse = () => {
@@ -46,11 +60,12 @@ class EntityAnnotation extends React.Component {
 				<Col md={12}>
 					<h2>Annotation</h2>
 					<Collapse in={this.state.open}>
-						<pre className="annotation-content">{stringToHTMLWithLinks(annotation.content)}</pre>
+						<pre className="annotation-content" ref={this.preRef} >{stringToHTMLWithLinks(annotation.content)}</pre>
 					</Collapse>
+					{this.state.show &&
 					<Button bsStyle="link" onClick={this.handleToggleCollapse}>
 						Show {this.state.open ? 'less' : 'more…'}
-					</Button>
+					</Button>}
 					<p className="text-muted">Last modified: <span title={formatDate(lastModifiedDate, true)}>{formatDate(lastModifiedDate)}</span>
 						<span className="small"> (revision <a href={`/revision/${annotation.lastRevisionId}`}>#{annotation.lastRevisionId}</a>)</span>
 					</p>
