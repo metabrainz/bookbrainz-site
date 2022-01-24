98626ef8e1c9b0e3e4ea501b9328c6489fb52c3c
Monkey Do <MonkeyDo@users.noreply.github.com>
2022-01-24 16:08:20 +0100

    Dummy test change
    Testing Github Actions and linting of PRs from forked repos (specifically using the master branch)

--- src/server/app.js
+++ src/server/app.js
@@ -41,6 +41,7 @@ import routes from './routes';
 import serveStatic from 'serve-static';
 import session from 'express-session';
 
+console.log("Testing linting in PRs from forked repos");
 
 // Initialize log-to-stdout  writer
 logNode();
