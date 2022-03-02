717e8031cbc363f0f2bb37b70f5a75efa2616ba3
akshaaatt <tiwariakshat03@gmail.com>
2022-03-02 11:31:23 +0530

    FIx lint issues

--- src/client/components/footer.js
+++ src/client/components/footer.js
@@ -18,17 +18,10 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import * as bootstrap from 'react-bootstrap';
-
-import PropTypes from 'prop-types';
 import React from 'react';
 
 
-const {Col, Container, Row} = bootstrap;
-
-function Footer(props) {
-	const {repositoryUrl, siteRevision} = props;
-
+function Footer() {
 	return (
 		 <section className="section-footer">
 			<div className="container">
@@ -122,11 +115,5 @@ function Footer(props) {
 		 </section>
 	);
 }
-
-Footer.displayName = 'Footer';
-Footer.propTypes = {
-	repositoryUrl: PropTypes.string.isRequired,
-	siteRevision: PropTypes.string.isRequired
-};
 
 export default Footer;
