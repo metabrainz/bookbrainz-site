a364c467609fecb0c24ebe3c9b5e263d734871f0
Monkey Do <contact@monkeydo.digital>
2022-05-25 13:48:24 +0200

    chore: Create DB with env variables
    Instead of having to run a separate script we can just use the postgres docker image env variables to create the DB and user on first start. Thanks alastairp !

--- scripts/database-init-docker.sh
+++ scripts/database-init-docker.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose build &&
-docker-compose run --rm bookbrainz-site scripts/wait-for-postgres.sh scripts/create-db.sh &&
 docker-compose run --rm bookbrainz-site scripts/wait-for-postgres.sh scripts/download-import-dump.sh
