9e772130fdd17cc1b20e6e715c2243e1f75735d8
tri10 <shubhi.blog@gmail.com>
2022-07-03 18:26:54 +0530

    add js-doc for all uf components

--- src/client/unified-form/action.ts
+++ src/client/unified-form/action.ts
@@ -1,8 +1,18 @@
+import {Action} from './interface/type';
+
+
 export const DUMP_EDITION = 'DUMP_EDITION';
 export const LOAD_EDITION = 'LOAD_EDITION';
 
 const nextEditionId = 0;
-export function dumpEdition(type?:string) {
+
+/**
+ * Produces an action indicating that current edition state should be saved in `Editions`.
+ *
+ * @param {string} type - type of new entity which caused dump
+ * @returns {Action} The resulting DUMP_EDITION action.
+ */
+export function dumpEdition(type?:string):Action {
 	return {
 		payload: {
 			id: `e${nextEditionId}`,
@@ -13,7 +23,14 @@ export function dumpEdition(type?:string) {
 	};
 }
 
-export function loadEdition(editionId = 'e0') {
+/**
+ * Produces an action indicating that particular edition state having that id
+ * should be loaded from `Editions`.
+ *
+ * @param {string} editionId - id of edition to load
+ * @returns {Action} The resulting LOAD_EDITION action.
+ */
+export function loadEdition(editionId = 'e0'):Action {
 	return {
 		payload: {
 			id: editionId
