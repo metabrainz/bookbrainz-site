ddcdb2160b62332b12a6bd4a3b9968d7e304476e
tri10 <shubhi.blog@gmail.com>
2022-07-03 12:52:25 +0530

    feat(uf): improve detail-tab accordion

--- src/client/unified-form/detail-tab/detail-tab.tsx
+++ src/client/unified-form/detail-tab/detail-tab.tsx
@@ -1,7 +1,9 @@
 import * as Bootstrap from 'react-bootstrap';
 import AnnotationSection from '../../entity-editor/annotation-section/annotation-section';
 import EditionSection from '../../entity-editor/edition-section/edition-section';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 
 const {Card, Accordion} = Bootstrap;
@@ -9,14 +11,15 @@ export function DetailTab(props) {
 	return (
 		<div>
 			<Accordion >
-				<Card>
-					<Accordion.Toggle as={Card.Header} eventKey="0">Extra info
-					</Accordion.Toggle>
+				<Card className="icon-card">
 					<Accordion.Collapse eventKey="0">
 						<Card.Body>
 							<EditionSection {...props}/>
 						</Card.Body>
 					</Accordion.Collapse>
+					<Accordion.Toggle as={Card.Header} eventKey="0">Extra info
+						<FontAwesomeIcon className="accordion-arrow" icon={faChevronRight}/>
+					</Accordion.Toggle>
 				</Card>
 			</Accordion>
 			<AnnotationSection {...props}/>
