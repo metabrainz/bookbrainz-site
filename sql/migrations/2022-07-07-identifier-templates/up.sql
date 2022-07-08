c49501a2dab300b8a93a15b69a862542f8e69cec
Monkey Do <contact@monkeydo.digital>
2022-07-08 11:30:03 +0200

    chore(sql): Add transactions
    + cleanup

--- sql/migrations/2022-07-07-identifier-templates/up.sql
+++ sql/migrations/2022-07-07-identifier-templates/up.sql
@@ -1,3 +1,4 @@
+BEGIN TRANSACTION;
 UPDATE bookbrainz.identifier_type
 SET display_template =
 	CASE
@@ -29,4 +30,6 @@ SET display_template =
 		WHEN id in (12, 29, 31) THEN 'https://viaf.org/viaf/{value}'
 		WHEN id in (18, 19, 20, 21, 30) THEN 'https://www.wikidata.org/wiki/{value}'
 	END
-WHERE id IN (1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34)
+WHERE id IN (1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34);
+
+COMMIT;
