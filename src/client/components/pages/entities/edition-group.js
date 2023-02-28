3c2361e060d7b1c02afd13781ff4dad572fe7267
Monkey Do <contact@monkeydo.digital>
2023-02-28 12:35:14 +0100

    fix(BB-707): Don't create CBReviewModal for deleted entities

--- src/client/components/pages/entities/edition-group.js
+++ src/client/components/pages/entities/edition-group.js
@@ -112,15 +112,6 @@ function EditionGroupDisplayPage({entity, identifierTypes, user}) {
 
 	return (
 		<div>
-			<CBReviewModal
-				entityBBID={entity.bbid}
-				entityName={entity.defaultAlias.name}
-				entityType={entity.type}
-				handleModalToggle={handleModalToggle}
-				handleUpdateReviews={handleUpdateReviews}
-				showModal={showCBReviewModal}
-				userId={user?.id}
-			/>
 			<Row className="entity-display-background">
 				<Col className="entity-display-image-box text-center" lg={2}>
 					<EntityImage
@@ -174,6 +165,15 @@ function EditionGroupDisplayPage({entity, identifierTypes, user}) {
 				lastModified={entity.revision.revision.createdAt}
 				user={user}
 			/>
+			{!entity.deleted && <CBReviewModal
+				entityBBID={entity.bbid}
+				entityName={entity.defaultAlias.name}
+				entityType={entity.type}
+				handleModalToggle={handleModalToggle}
+				handleUpdateReviews={handleUpdateReviews}
+				showModal={showCBReviewModal}
+				userId={user?.id}
+			                    />}
 		</div>
 	);
 }
