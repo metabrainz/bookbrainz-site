808f7c8ceccc5978ae7a287e093b4da63d0bc7e5
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-03-06 15:49:18 +0100

    feat(core): parse 'Accept-Language' HTTP request header
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
    
    Intentionally ignoring the wildcard case '*' as this is effectively the
    same information as a missing header (return value: empty array).

---
+++ src/server/helpers/i18n.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2023  David Kellner
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+import type {Request} from 'express';
+
+
+/**
+ * Parses an Accept-Language header to obtain the language codes, optional subtags and weights.
+ * @param {string} acceptLanguage - Accept-Language header value.
+ * @returns Parsed languages, sorted by weight in descending order.
+ */
+export function parseAcceptLanguage(acceptLanguage: string) {
+	return acceptLanguage
+		.split(',')
+		.map((value) => {
+			const match = value.match(/(?<tag>[a-zA-Z]{2,3})(?:-(?<subtag>[\w-]+))?(?:;q=(?<weight>[01](?:\.[0-9]+)?))?/);
+			return match ? {
+				code: match.groups.tag,
+				subtags: match.groups.subtag?.split('-') ?? [],
+				weight: parseFloat(match.groups.weight ?? '1')
+			} : null;
+		})
+		.filter((value) => value !== null)
+		.sort((a, b) => b.weight - a.weight);
+}
+
