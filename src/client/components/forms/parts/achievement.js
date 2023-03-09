552ff35e052ad7ed11c12b8f7756eab66c620737
meziyum <siddhant24feb@gmail.com>
2023-03-10 02:27:08 +0530

    Update achievement.js

--- src/client/components/forms/parts/achievement.js
+++ src/client/components/forms/parts/achievement.js
@@ -58,7 +58,7 @@ const maxAchievementProgress = {
 };
 
 function Achievement(props) {
-	const { achievement, counter, unlocked } = props;
+	const {achievement, counter, unlocked} = props;
 	const imgElement = unlocked ? (
 		<DragAndDropImage
 			achievementId={achievement.id}
