2724501ccc4b387a7df605b3ff22dd5cedf8a4bb
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-03-14 16:29:13 +0530

    BB-715 and BB-718

--- src/client/components/pages/contribute.js
+++ src/client/components/pages/contribute.js
@@ -16,8 +16,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-import React from 'react';
-
+import React from "react";
 
 /**
  * Renders the the document and displays the 'Contribute' page.
@@ -25,10 +24,9 @@ import React from 'react';
  * Contribute page
  */
 function ContributePage() {
-	const IRCLink = 'https://kiwiirc.com/nextclient/irc.libera.chat/?#bookbrainz';
-	const styleLink =
-		'https://bookbrainz-user-guide.readthedocs.io/' +
-		'en/latest/style/introduction/';
+	const IRCLink =
+		"https://kiwiirc.com/nextclient/irc.libera.chat/?#bookbrainz";
+	const styleLink = "https://bookbrainz-user-guide.readthedocs.io/";
 
 	return (
 		<div>
@@ -36,34 +34,34 @@ function ContributePage() {
 				<h1>Contributing to BookBrainz</h1>
 			</div>
 			<p className="lead">
-				There are many ways that you can contribute to the
-				BookBrainz online encyclopedia, depending on where your
-				particular skills lie.
+				There are many ways that you can contribute to the BookBrainz
+				online encyclopedia, depending on where your particular skills
+				lie.
 			</p>
 
 			<h2>Editing</h2>
 			<p>
 				If you come to BookBrainz with new information about a
-				particular book, you&rsquo;ll want to edit the database.<br/>
-				To do that, you&rsquo;ll first need an account, which you
-				can get by&nbsp;
-				<a href="/register">
-					registering
-				</a>. Then, you&rsquo;ll be able to add your new
-				information by editing an entity.<br/>
+				particular book, you&rsquo;ll want to edit the database.
+				<br />
+				To do that, you&rsquo;ll first need an account, which you can
+				get by&nbsp;
+				<a href="/register">registering</a>. Then, you&rsquo;ll be able
+				to add your new information by editing an entity.
+				<br />
 			</p>
 			<p>
 				Please follow the&nbsp;
-				<a href={styleLink}>
-					BookBrainz style guidelines
-				</a> when adding data.<br/> They are still a work in progress
-				and should they not cover some cases, please refer to the&nbsp;
-				<a href="http://musicbrainz.org/doc/Style">
-					MusicBrainz ones
-				</a>, adapting as necessary.
+				<a href={styleLink}>BookBrainz style guidelines</a> when adding
+				data.
+				<br /> They are still a work in progress and should they not
+				cover some cases, please refer to the&nbsp;
+				<a href="http://musicbrainz.org/doc/Style">MusicBrainz ones</a>,
+				adapting as necessary.
 			</p>
 			<p>
-				If you find duplicates, instead of deleting them, please follow&nbsp;
+				If you find duplicates, instead of deleting them, please
+				follow&nbsp;
 				<a href="https://bookbrainz-user-guide.readthedocs.io/en/latest/tutorials/merge/">
 					this tutorial
 				</a>
@@ -73,15 +71,14 @@ function ContributePage() {
 
 			<h2>Programming</h2>
 			<p>
-				If you&rsquo;re someone who&rsquo;s good at using
-				JavaScript, NodeJS, SQL or LESS/SASS, we&rsquo;d love to
-				have your help developing BookBrainz. Take a look at our&nbsp;
+				If you&rsquo;re someone who&rsquo;s good at using JavaScript,
+				NodeJS, SQL or LESS/SASS, we&rsquo;d love to have your help
+				developing BookBrainz. Take a look at our&nbsp;
 				<a href="https://wiki.musicbrainz.org/Development/Priorities#BookBrainz">
-					 roadmap
+					roadmap
 				</a>
-				&nbsp;to see if there are any issues
-				or project that interests you.
-				Our code is all open source and hosted&nbsp;
+				&nbsp;to see if there are any issues or project that interests
+				you. Our code is all open source and hosted&nbsp;
 				<a href="https://github.com/metabrainz?q=bookbrainz">
 					on GitHub
 				</a>
@@ -92,13 +89,14 @@ function ContributePage() {
 				categories, think you can contribute in a different area, or
 				just want to chat, feel free to drop in and meet us on the
 				MetaBrainz&nbsp;
-				<a href={IRCLink}>
-					IRC channel
-				</a>!
+				<a href={IRCLink}>IRC channel</a>!
 			</p>
 			<p>
 				You can also get in touch and discuss with the community
-				<a href="https://community.metabrainz.org/c/bookbrainz"> on our forums,</a>
+				<a href="https://community.metabrainz.org/c/bookbrainz">
+					{" "}
+					on our forums,
+				</a>
 				&nbsp;or send us
 				<a href="mailto:bookbrainz@metabrainz.org"> an email.</a>
 			</p>
@@ -106,6 +104,6 @@ function ContributePage() {
 	);
 }
 
-ContributePage.displayName = 'ContributePage';
+ContributePage.displayName = "ContributePage";
 
 export default ContributePage;
