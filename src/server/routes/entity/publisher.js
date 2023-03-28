6f4630a65c728e5e2c8e0367fb2698f9b88b74b2
Rohan Sasne <rohansasne30@gmail.com>
2023-03-28 23:17:49 +0530

    Update publisher.js

--- src/server/routes/entity/publisher.js
+++ src/server/routes/entity/publisher.js
@@ -191,25 +191,24 @@ function _setPublisherTitle(res) {
 
 
 router.get('/:bbid', middleware.loadEntityRelationships, async (req, res, next) => {
- try {
- // Fetch editions
-	 const {Publisher} = req.app.locals.orm;
-	 const editionRelationsToFetch = [
-		 'defaultAlias',
-		 'disambiguation',
-		 'releaseEventSet.releaseEvents',
-		 'identifierSet.identifiers.type',
-		 'editionFormat', 'authorCredit.names'
-	 ];
-  const editions = await Publisher.forge({bbid: res.locals.entity.bbid})
-			.editions({withRelated: editionRelationsToFetch});
-		
+	try {
+		// Fetch editions
+		const {Publisher} = req.app.locals.orm;
+		const editionRelationsToFetch = [
+			'defaultAlias',
+			'disambiguation',
+			'releaseEventSet.releaseEvents',
+			'identifierSet.identifiers.type',
+			'editionFormat', 'authorCredit.names'
+		];
+		const editions = await Publisher.forge({bbid: res.locals.entity.bbid})
+		.editions({withRelated: editionRelationsToFetch});
 		res.locals.entity.editions = editions.toJSON();
 		_setPublisherTitle(res);
 		res.locals.entity.editions.sort(entityRoutes.compareEntitiesByDate);
 		await entityRoutes.displayEntity(req, res);
     	}
-	catch(err){
+	catch (err) {
 		next(err);
 	}
 });
