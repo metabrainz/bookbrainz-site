7ad537fb245ce15690ea140129f3c7b26f25a294
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-04-14 18:34:53 +0200

    fix(core): fallback to 'dev' if the version can not be determined
    'latest' is not always true, it could be any commit.

--- src/server/info.ts
+++ src/server/info.ts
@@ -8,7 +8,8 @@ export const siteRevision = process.env.GIT_COMMIT_SHA;
 
 // In production, a git tag (e.g. `v0.15.2` or `v-2023-02-20.0`) might be passed instead of a commit SHA.
 // Try to extract the plain version number (e.g. `0.15.2` or `2023-02-20.0`) from the git specifier.
-export const version = siteRevision?.match(/^v-?([\d.-]+)$/)?.[1] ?? 'latest';
+// Otherwise we assume that a development server is running and use `dev` as the version.
+export const version = siteRevision?.match(/^v-?([\d.-]+)$/)?.[1] ?? 'dev';
 
 export const domain = 'bookbrainz.org';
 
