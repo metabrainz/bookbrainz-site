f2af26563ed6445f85286f41b95d26893c658296
Rohan Sasne <rohansasne30@gmail.com>
2023-05-18 15:38:38 +0530

    fix: Solve ESLint errors

--- src/server/routes/entity/publisher.js
+++ src/server/routes/entity/publisher.js
@@ -191,26 +191,24 @@ function _setPublisherTitle(res) {
 
 
 router.get('/:bbid', middleware.loadEntityRelationships, middleware.loadWikipediaExtract, async (req, res, next) => {
-  // Fetch editions
-  const {Publisher} = req.app.locals.orm;
-  const editionRelationsToFetch = [
-    'defaultAlias',
-    'disambiguation',
-    'releaseEventSet.releaseEvents',
-    'identifierSet.identifiers.type',
-    'editionFormat',
-    'authorCredit.names'
-  ];
-  const editions = await Publisher.forge({bbid: res.locals.entity.bbid})
-    .editions({withRelated: editionRelationsToFetch});
+ // Fetch editions
+ const {Publisher} = req.app.locals.orm;
+ const editionRelationsToFetch = [
+  'defaultAlias',
+  'disambiguation',
+  'releaseEventSet.releaseEvents',
+  'identifierSet.identifiers.type',
+  'editionFormat',
+  'authorCredit.names'
+ ];
+ const editions = await Publisher.forge({bbid: res.locals.entity.bbid})
+   .editions({withRelated: editionRelationsToFetch});
 
-  res.locals.entity.editions = editions.toJSON();
-  _setPublisherTitle(res);
-  res.locals.entity.editions.sort(entityRoutes.compareEntitiesByDate);
-  await entityRoutes.displayEntity(req, res);
-})
-
-
+ res.locals.entity.editions = editions.toJSON();
+ _setPublisherTitle(res);
+ res.locals.entity.editions.sort(entityRoutes.compareEntitiesByDate);
+ await entityRoutes.displayEntity(req, res);
+});
 
 router.get('/:bbid/delete', auth.isAuthenticated, (req, res, next) => {
 	if (!res.locals.entity.dataId) {
