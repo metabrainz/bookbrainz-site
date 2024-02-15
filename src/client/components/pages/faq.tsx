15919dc24aad7f2d0dfcb73d7f09087bea897153
meziyum <siddhant24feb@gmail.com>
2024-02-15 22:32:20 +0530

    Documentaion
    Added React component Documentation

--- src/client/components/pages/faq.tsx
+++ src/client/components/pages/faq.tsx
@@ -20,8 +20,11 @@ import {Card, ListGroup} from 'react-bootstrap';
 import React from 'react';
 import {genEntityIconHTMLElement} from '../../helpers/entity';
 
-
-function FAQPage() {
+/**
+ * Renders the page for the Frequently Asked Questions on Bookbrainz
+ * @returns {React.JSX.Element} a React JSX Element
+ */
+function FAQPage(): React.JSX.Element {
 	return (
 		<Card>
 			<Card.Header as="h2">
