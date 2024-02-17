04767ebc135b9cbfb7e4a39bcb1c5c32c45ad704
meziyum <siddhant24feb@gmail.com>
2024-02-17 23:50:34 +0530

    Refactor develop page
    Refactor develop page to typescript

--- src/client/components/pages/develop.tsx
+++ src/client/components/pages/develop.tsx
@@ -21,9 +21,9 @@ import React from 'react';
 
 /**
  * Renders the document and displays the 'Develop' page.
- * @returns {ReactElement} a HTML document to display the Develop page
+ * @returns {React.JSX.Element} a React JSX Element
  */
-function DevelopPage() {
+function DevelopPage(): React.JSX.Element {
 	return (
 		<div>
 			<div className="page-header">
