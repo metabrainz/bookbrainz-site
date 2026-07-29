e3eed0e83a10e48096a78bb7e37d84c4315fe739
garvthakre <garvthakre0@gmail.com>
2026-07-29 19:02:02 +0530

    refactor(i18n): migrate unified form components  and remaining pages

--- src/client/unified-form/common/search-entity-create-select.tsx
+++ src/client/unified-form/common/search-entity-create-select.tsx
@@ -11,6 +11,7 @@ import {addEditionGroup} from '../detail-tab/action';
 import {connect} from 'react-redux';
 import makeImmutable from '../../entity-editor/common/make-immutable';
 import {submitSingleEntity} from '../../entity-editor/submission-section/actions';
+import {useTranslation} from 'react-i18next';
 
 
 const ImmutableCreatableAsync = makeImmutable(AsyncCreatable);
@@ -22,8 +23,10 @@ const addEntityAction = {
 	work: addWork
 };
 function SearchEntityCreate(props:SearchEntityCreateProps) {
+	const {t: translate} = useTranslation('entityEditor');
 	const {type, nextId, onModalOpen, onModalClose, onSubmitEntity, rowId, onOpenCallback, ...rest} = props;
-	const createLabel = React.useCallback((input) => `Create ${type} "${input}"`, [type]);
+	const createLabel = React.useCallback((input) => translate('unifiedForm.createEntity', {entityType: translate(`common:entityType.${camelCase(type)}`),name: input}),[translate, type]
+);
 	const [showModal, setShowModal] = React.useState(false);
 	const getNewOptionData = React.useCallback((_, label) => ({
 		__isNew__: true,
@@ -97,4 +100,3 @@ function mapDispatchToProps(dispatch, {type, submissionUrl, onAddCallback}):Sear
 }
 
 export default connect<null, SearchEntityCreateDispatchProps>(null, mapDispatchToProps)(SearchEntityCreate);
-
