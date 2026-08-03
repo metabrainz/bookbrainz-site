82106060e594d46fa4a44692514ed712f64aecea
Monkey Do <salicon.petiller@gmail.com>
2026-08-03 12:38:28 +0200

    Export only specified tables from the databse
    Avoid ending up with private tables in the dumps by mistake

--- scripts/create-dumps.sh
+++ scripts/create-dumps.sh
@@ -8,19 +8,110 @@ source /home/bookbrainz/bookbrainz-site/scripts/config.sh
 pushd /home/bookbrainz/data/dumps
 
 
-DUMP_FILE=bookbrainz-dump-`date -I`.sql
+DUMP_FILE=bookbrainz-dump-$(date -I).sql
+
+# Explicit allowlist for public database dumps. Keep this in sync with the
+# tables that are safe to export.
+EXPORTED_TABLES=(
+	musicbrainz.gender
+	musicbrainz.language
+	musicbrainz.area_type
+	musicbrainz.area
+	musicbrainz.l_area_area
+	musicbrainz.country_area
+	bookbrainz.editor_type
+	bookbrainz.editor
+	bookbrainz.editor__language
+	bookbrainz.admin_log
+	bookbrainz.entity
+	bookbrainz.entity_redirect
+	bookbrainz.author_header
+	bookbrainz.edition_group_header
+	bookbrainz.edition_header
+	bookbrainz.publisher_header
+	bookbrainz.work_header
+	bookbrainz.revision_parent
+	bookbrainz.revision
+	bookbrainz.author_revision
+	bookbrainz.edition_group_revision
+	bookbrainz.edition_revision
+	bookbrainz.publisher_revision
+	bookbrainz.work_revision
+	bookbrainz.note
+	bookbrainz.author_type
+	bookbrainz.author_data
+	bookbrainz.release_event
+	bookbrainz.release_event_set
+	bookbrainz.release_event_set__release_event
+	bookbrainz.author_credit
+	bookbrainz.author_credit_name
+	bookbrainz.publisher_set
+	bookbrainz.publisher_set__publisher
+	bookbrainz.edition_format
+	bookbrainz.edition_status
+	bookbrainz.edition_data
+	bookbrainz.edition_group_type
+	bookbrainz.edition_group_data
+	bookbrainz.publisher_type
+	bookbrainz.publisher_data
+	bookbrainz.work_type
+	bookbrainz.work_data
+	bookbrainz.annotation
+	bookbrainz.disambiguation
+	bookbrainz.alias
+	bookbrainz.identifier_type
+	bookbrainz.identifier
+	bookbrainz.relationship_type
+	bookbrainz.alias_set
+	bookbrainz.alias_set__alias
+	bookbrainz.identifier_set
+	bookbrainz.identifier_set__identifier
+	bookbrainz.relationship_set
+	bookbrainz.relationship_set__relationship
+	bookbrainz.relationship
+	bookbrainz.relationship_attribute_set
+	bookbrainz.relationship_attribute_type
+	bookbrainz.relationship_type__attribute_type
+	bookbrainz.relationship_attribute
+	bookbrainz.relationship_attribute_text_value
+	bookbrainz.relationship_attribute_set__relationship_attribute
+	bookbrainz.language_set
+	bookbrainz.language_set__language
+	bookbrainz.series_header
+	bookbrainz.series_ordering_type
+	bookbrainz.series_data
+	bookbrainz.series_revision
+	bookbrainz.title_type
+	bookbrainz.title_unlock
+	bookbrainz.achievement_type
+	bookbrainz.achievement_unlock
+	# Imports - TODO: evaluate whether we wwant those exported in the dumps
+	bookbrainz.import
+	bookbrainz.author_import_header
+	bookbrainz.edition_import_header
+	bookbrainz.edition_group_import_header
+	bookbrainz.publisher_import_header
+	bookbrainz.work_import_header
+	bookbrainz.discard_votes
+	bookbrainz.origin_source
+	bookbrainz.link_import
+)
+
+PG_DUMP_TABLE_ARGS=()
+for table_name in "${EXPORTED_TABLES[@]}"; do
+	PG_DUMP_TABLE_ARGS+=("--table=$table_name")
+done
 
 echo "Creating data dump..."
 
 # Dump new backup to /tmp
-pg_dump\
-	-h $POSTGRES_HOST \
-	-p $POSTGRES_PORT \
+pg_dump \
+	-h "$POSTGRES_HOST" \
+	-p "$POSTGRES_PORT" \
 	-U bookbrainz \
-	-T _editor_entity_visits\
-	-T user_collection*\
-	 --serializable-deferrable\
-	 bookbrainz > /tmp/$DUMP_FILE
+	"${PG_DUMP_TABLE_ARGS[@]}" \
+	--serializable-deferrable \
+	bookbrainz > "/tmp/$DUMP_FILE"
 echo "Dump created!"
 
 # Compress new backup and move to dump dir
