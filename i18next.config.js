d06538f8b7d83580daf8029fce5a0094563ca58a
garvthakre <garvthakre0@gmail.com>
2026-08-05 12:34:48 +0530

    refactor(i18n): update i18n config files and middleware

--- i18next.config.js
+++ i18next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('i18next-cli').I18nextToolkitConfig} */
 export default {
 	extract: {
-		defaultNS: 'common',
+		defaultNS: 'translation',
 		input: ['src/**/*.{js,jsx,ts,tsx}'],
-		keySeparator: false,
-		nsSeparator: ':',
-		output: 'public/locales/{{language}}/{{namespace}}.json',
+		keySeparator: '.',
+		nsSeparator: false,
+		output: 'public/locales/{{language}}/translation.json',
 		removeUnusedKeys: false,
 		sort: true
 	},
