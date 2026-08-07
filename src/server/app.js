02b82d78a470ff6bdc1d9e7389c7f12aaac6f903
Akira <5215000+ApeKattQuest-MonkeyPython@users.noreply.github.com>
2026-08-07 16:23:35 +0200

    GNU Terry Pratchett
    GNU Terry Pratchett - http://www.gnuterrypratchett.com/#nodejs

--- src/server/app.js
+++ src/server/app.js
@@ -64,6 +64,10 @@ if (app.get('env') !== 'testing') {
 	app.use(logger('dev'));
 }
 
+server.use(function (req, res, next) {
+  res.set('X-Clacks-Overhead', 'GNU Terry Pratchet');
+  next();
+});
 app.use(express.json({
 	limit: '10mb',
 	verify: (req, res, buffer) => {
