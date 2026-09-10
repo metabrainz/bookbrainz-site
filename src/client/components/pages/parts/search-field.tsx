c4b67a88a879bd8a350affccc64b48004a2c97f7
Monkey Do <MonkeyDo@users.noreply.github.com>
2026-09-10 15:12:12 +0200

    Increase debounce delay on search field
    Reduce unnecessary calls to the search endpoint

--- src/client/components/pages/parts/search-field.tsx
+++ src/client/components/pages/parts/search-field.tsx
@@ -39,7 +39,7 @@ const SearchButton = (
 	</Button>
 );
 
-const updateDelay = 1000;
+const updateDelay = 2000;
 
 type SearchFieldState = {
 	type: string,
@@ -74,7 +74,7 @@ class SearchField extends React.Component<SearchFieldProps, SearchFieldState> {
 			query: props.query || '',
 			type: props.type || ''
 		};
-		this.debouncedTriggerOnSearch = _.debounce(this.triggerOnSearch, updateDelay, {});
+		this.debouncedTriggerOnSearch = _.debounce(this.triggerOnSearch, updateDelay, {leading:false});
 	}
 
 	// If search term is changed outside this component (for example browser navigation),
